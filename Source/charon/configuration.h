d75f797e25e866344f8154c9545fe154e197a7f8
Jan Hutter <jhutter@hsr.ch>
2005-11-08 09:38:00 +0000

    - created header and c file for this type (without content)

---
+++ Source/charon/configuration.h
@@ -0,0 +1,27 @@
+/**
+ * @file configuration.h
+ * 
+ * @brief Class configuration_t. 
+ * Object of this type represents a configuration for an IKE_SA
+ * 
+ */
+
+/*
+ * Copyright (C) 2005 Jan Hutter, Martin Willi
+ * Hochschule fuer Technik Rapperswil
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+
+#ifndef CONFIGURATION_H_
+#define CONFIGURATION_H_
+
+#endif /*CONFIGURATION_H_*/
