d94fe8b3b93ffced079277ae739ce80b5a67d42b
Jan Hutter <jhutter@hsr.ch>
2005-11-09 08:39:39 +0000

    - added array of payload infos

---
+++ Source/charon/encodings.c
@@ -0,0 +1,44 @@
+/**
+ * @file encodings.c
+ * 
+ * @brief Type definitions for parser and generator
+ * 		  Also, Payload types are defined here.
+ * 
+ * Header is parsed like a payload and gets its one payload_id 
+ * from PRIVATE USE space. Also the substructures get their own
+ * payload_id from PRIVATE_USE space
+ * 
+ * 
+ */
+
+/*
+ * Copyright (C) 2005 Jan Hutter, Martin Willi
+ * Hochschule fuer Technik Rapperswil
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+ 
+ #include "encodings.h"
+ #include "encodings/ike_header.h" 
+
+extern payload_info_t ike_header_info;
+
+/**
+ * Contains all payload informations supported by parser and generator
+ * 
+ * @warning This list must be NULL terminated
+ */
+payload_info_t *payload_infos[] = {
+	&ike_header_info,
+ 	NULL
+};
+ 
+ 
