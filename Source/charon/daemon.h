cf274de285dfd804cfe84d5c518a1c34d9816c99
Jan Hutter <jhutter@hsr.ch>
2005-11-11 11:20:45 +0000

    - daemon name added to this header

---
+++ Source/charon/daemon.h
@@ -0,0 +1,28 @@
+/**
+ * @file daemon.h
+ * 
+ * @brief Main of IKEv2-Daemon
+ * 
+ */
+
+/*
+ * Copyright (C) 2005 Jan Hutter, Martin Willi
+ * Hochschule fuer Technik Rapperswil
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+
+#ifndef DAEMON_H_
+#define DAEMON_H_
+
+#define DEAMON_NAME "charon"
+
+#endif /*DAEMON_H_*/
