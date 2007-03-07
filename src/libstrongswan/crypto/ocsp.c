817fecddc96fec244db7f2cdb2bb3e4d2206d4a6
Andreas Steffen <andreas.steffen@strongswan.org>
2007-03-07 04:22:21 +0000

    nonce in ocsp_t was not properly initialized

--- src/libstrongswan/crypto/ocsp.c
+++ src/libstrongswan/crypto/ocsp.c
@@ -499,6 +499,7 @@ ocsp_t *ocsp_create(x509_t *cacert, linked_list_t *uris)
 	this->cacert = cacert;
 	this->uris = uris;
 	this->certinfos = linked_list_create();
+	this->nonce = chunk_empty;
 	{
 		hasher_t *hasher = hasher_create(HASH_SHA1);
 		identification_t *issuer = cacert->get_subject(cacert);
