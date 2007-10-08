99d7cd20ad8823bc3013100998b4a2fdfa536568
Andreas Steffen <andreas.steffen@strongswan.org>
2007-10-08 20:09:57 +0000

    added RCSID

--- src/openac/build.h
+++ src/openac/build.h
@@ -12,6 +12,8 @@
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
  * for more details.
+ *
+ * RCSID $Id$
  */
 
 #ifndef _BUILD_H
