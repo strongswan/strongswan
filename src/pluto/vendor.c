6859f760d9ef6766f8d90fda85283f5c72dede8e
Andreas Steffen <andreas.steffen@strongswan.org>
2008-02-14 21:26:21 +0000

    release of 4.1.11 bug fix version

--- src/pluto/vendor.c
+++ src/pluto/vendor.c
@@ -206,7 +206,7 @@ static struct vid_struct _vid_tab[] = {
 	/*
 	 * strongSwan
 	 */
-	DEC_MD5_VID(STRONGSWAN,       "strongSwan 4.2.0")
+	DEC_MD5_VID(STRONGSWAN,       "strongSwan 4.1.11")
 	DEC_MD5_VID(STRONGSWAN_4_1_10,"strongSwan 4.1.10")
 	DEC_MD5_VID(STRONGSWAN_4_1_9, "strongSwan 4.1.9")
 	DEC_MD5_VID(STRONGSWAN_4_1_8, "strongSwan 4.1.8")
