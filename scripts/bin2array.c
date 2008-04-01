68356ab12cb1058fde78af312bfae3be0398d4d7
Martin Willi <martin@strongswan.org>
2008-04-01 14:19:22 +0000

    simple converter from binary data to a c array

---
+++ scripts/bin2array.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+
+/**
+ * convert standard input to binary data to a c array
+ */
+int main(int argc, char *argv[])
+{
+	int i, end = 0;
+	char byte;
+
+	printf("char %s[] = {\n", argc > 1 ? argv[1] : "data");
+	while (1)
+	{
+		printf("  ");
+		for (i = 0; i < 16; i++)
+		{
+			if (fread(&byte, 1, 1, stdin) != 1)
+			{
+				end = 1;
+				break;
+			}
+			printf("0x%02x,", byte);
+		}
+		printf("\n");
+		if (end)
+		{
+			break;
+		}
+	}	
+	printf("};\n");
+	return 0;
+}
+
