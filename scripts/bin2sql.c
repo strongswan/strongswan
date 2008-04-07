8ec032fc629ff5284de738f4961d8b41ba410f7d
Martin Willi <martin@strongswan.org>
2008-04-07 06:06:42 +0000

    added helper scripts to create SQL scripts

---
+++ scripts/bin2sql.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+
+/**
+ * convert standard input to SQL hex binary
+ */
+int main(int argc, char *argv[])
+{
+	int i, end = 0;
+	unsigned char byte;
+
+	printf("X'");
+	while (1)
+	{
+		if (fread(&byte, 1, 1, stdin) != 1)
+		{
+			end = 1;
+			break;
+		}
+		printf("%02x", (unsigned int)byte);
+	}	
+	printf("'\n");
+	return 0;
+}
+
---
+++ scripts/id2sql.c
@@ -0,0 +1,36 @@
+
+#include <stdio.h>
+#include <library.h>
+
+/**
+ * convert an identity to type and encoding
+ */
+int main(int argc, char *argv[])
+{
+	identification_t *id;
+	chunk_t enc;
+	int i;
+	
+	if (argc < 2)
+	{
+		return -1;
+	}
+
+	id = identification_create_from_string(argv[1]);
+	if (!id)
+	{
+		return -2;
+	}
+	printf("type\tencoding\n");
+	printf("%d,\t", id->get_type(id));
+	enc = id->get_encoding(id);
+
+	printf("X'");
+	for (i = 0; i < enc.len; i++)
+	{
+		printf("%02x", (unsigned int)enc.ptr[i]);
+	}	
+	printf("'\n");
+	return 0;
+}
+
