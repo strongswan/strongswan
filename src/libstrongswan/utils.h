e822fc576a9c2210aec9fc3d947fa859c03a0f22
Tobias Brunner <tobias@strongswan.org>
2009-08-25 13:11:42 +0200

    Added side effect free min and max macros.

--- src/libstrongswan/utils.h
+++ src/libstrongswan/utils.h
@@ -72,12 +72,19 @@
 /**
  * Macro gives back larger of two values.
  */
-#define max(x,y) ((x) > (y) ? (x):(y))
+#define max(x,y) ({ \
+	typeof(x) _x = (x); \
+	typeof(y) _y = (y); \
+	_x > _y ? _x : _y; })
+
 
 /**
  * Macro gives back smaller of two values.
  */
-#define min(x,y) ((x) < (y) ? (x):(y))
+#define min(x,y) ({ \
+	typeof(x) _x = (x); \
+	typeof(y) _y = (y); \
+	_x < _y ? _x : _y; })
 
 /**
  * Call destructor of an object, if object != NULL
