91cb348cc25418679974d16e8655440bd9752ad1
Martin Willi <martin@strongswan.org>
2009-09-21 18:13:25 +0200

    CA certificates are looked up using the subjectPublicKeyInfo keyid

--- testing/tests/sql/net2net-cert/hosts/moon/etc/ipsec.d/data.sql
+++ testing/tests/sql/net2net-cert/hosts/moon/etc/ipsec.d/data.sql
@@ -9,7 +9,7 @@ INSERT INTO identities (
 INSERT INTO identities (
   type, data
 ) VALUES ( /* keyid of 'C=CH, O=Linux strongSwan, CN=strongSwan Root CA' */
-  11, X'ae096b87b44886d3b820978623dabd0eae22ebbc'
+  11, X'5da7dd700651327ee7b66db3b5e5e060ea2e4def'
  );
 
 INSERT INTO identities (
