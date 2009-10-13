a2b50c5d60fc9d828d1b36243acee391d9b01826
Martin Willi <martin@strongswan.org>
2009-10-13 11:04:15 +0200

    Fixed assignment of get_triplet() dummy implementation

--- src/charon/plugins/eap_aka_3gpp2/eap_aka_3gpp2_card.c
+++ src/charon/plugins/eap_aka_3gpp2/eap_aka_3gpp2_card.c
@@ -151,7 +151,7 @@ eap_aka_3gpp2_card_t *eap_aka_3gpp2_card_create(eap_aka_3gpp2_functions_t *f)
 {
 	private_eap_aka_3gpp2_card_t *this = malloc_thing(private_eap_aka_3gpp2_card_t);
 
-	this->public.card.get_triplet = (bool(*)(sim_card_t*, identification_t *imsi, char rand[SIM_RAND_LEN], char sres[SIM_SRES_LEN], char kc[SIM_KC_LEN]))return_false();
+	this->public.card.get_triplet = (bool(*)(sim_card_t*, identification_t *imsi, char rand[SIM_RAND_LEN], char sres[SIM_SRES_LEN], char kc[SIM_KC_LEN]))return_false;
 	this->public.card.get_quintuplet = (status_t(*)(sim_card_t*, identification_t *imsi, char rand[AKA_RAND_LEN], char autn[AKA_AUTN_LEN], char ck[AKA_CK_LEN], char ik[AKA_IK_LEN], char res[AKA_RES_LEN]))get_quintuplet;
 	this->public.card.resync = (bool(*)(sim_card_t*, identification_t *imsi, char rand[AKA_RAND_LEN], char auts[AKA_AUTS_LEN]))resync;
 	this->public.destroy = (void(*)(eap_aka_3gpp2_card_t*))destroy;
