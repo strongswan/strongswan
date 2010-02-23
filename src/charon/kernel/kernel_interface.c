2d49f74e28a52fe3aeb5ec691b97c2bca61498e5
Martin Willi <martin@revosec.ch>
2010-02-23 16:34:34 +0100

    Migrated kernel_interface wrapper to METHOD/INIT macros

--- src/charon/kernel/kernel_interface.c
+++ src/charon/kernel/kernel_interface.c
@@ -1,6 +1,8 @@
 /*
  * Copyright (C) 2008-2009 Tobias Brunner
  * Hochschule fuer Technik Rapperswil
+ * Copyright (C) 2010 Martin Willi
+ * Copyright (C) 2010 revosec AG
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -40,11 +42,9 @@ struct private_kernel_interface_t {
 	kernel_net_t *net;
 };
 
-/**
- * Implementation of kernel_interface_t.get_spi
- */
-static status_t get_spi(private_kernel_interface_t *this, host_t *src, host_t *dst,
-				 protocol_id_t protocol, u_int32_t reqid, u_int32_t *spi)
+METHOD(kernel_interface_t, get_spi, status_t,
+	private_kernel_interface_t *this, host_t *src, host_t *dst,
+	protocol_id_t protocol, u_int32_t reqid, u_int32_t *spi)
 {
 	if (!this->ipsec)
 	{
@@ -53,11 +53,9 @@ static status_t get_spi(private_kernel_interface_t *this, host_t *src, host_t *d
 	return this->ipsec->get_spi(this->ipsec, src, dst, protocol, reqid, spi);
 }
 
-/**
- * Implementation of kernel_interface_t.get_cpi
- */
-static status_t get_cpi(private_kernel_interface_t *this, host_t *src, host_t *dst,
-					u_int32_t reqid, u_int16_t *cpi)
+METHOD(kernel_interface_t, get_cpi, status_t,
+	private_kernel_interface_t *this, host_t *src, host_t *dst,
+	u_int32_t reqid, u_int16_t *cpi)
 {
 	if (!this->ipsec)
 	{
@@ -66,17 +64,13 @@ static status_t get_cpi(private_kernel_interface_t *this, host_t *src, host_t *d
 	return this->ipsec->get_cpi(this->ipsec, src, dst, reqid, cpi);
 }
 
-/**
- * Implementation of kernel_interface_t.add_sa
- */
-static status_t add_sa(private_kernel_interface_t *this, host_t *src, host_t *dst,
-				u_int32_t spi, protocol_id_t protocol, u_int32_t reqid,
-				lifetime_cfg_t *lifetime,
-				u_int16_t enc_alg, chunk_t enc_key,
-				u_int16_t int_alg, chunk_t int_key,
-				ipsec_mode_t mode, u_int16_t ipcomp, u_int16_t cpi, bool encap,
-				bool inbound, traffic_selector_t *src_ts,
-				traffic_selector_t *dst_ts)
+METHOD(kernel_interface_t, add_sa, status_t,
+	private_kernel_interface_t *this, host_t *src, host_t *dst,
+	u_int32_t spi, protocol_id_t protocol, u_int32_t reqid,
+	lifetime_cfg_t *lifetime, u_int16_t enc_alg, chunk_t enc_key,
+	u_int16_t int_alg, chunk_t int_key, ipsec_mode_t mode, u_int16_t ipcomp,
+	u_int16_t cpi, bool encap, bool inbound, traffic_selector_t *src_ts,
+	traffic_selector_t *dst_ts)
 {
 	if (!this->ipsec)
 	{
@@ -87,26 +81,22 @@ static status_t add_sa(private_kernel_interface_t *this, host_t *src, host_t *ds
 			encap, inbound, src_ts, dst_ts);
 }
 
-/**
- * Implementation of kernel_interface_t.update_sa
- */
-static status_t update_sa(private_kernel_interface_t *this, u_int32_t spi,
-				   protocol_id_t protocol, u_int16_t cpi, host_t *src, host_t *dst,
-				   host_t *new_src, host_t *new_dst, bool encap, bool new_encap)
+METHOD(kernel_interface_t, update_sa, status_t,
+	private_kernel_interface_t *this, u_int32_t spi, protocol_id_t protocol,
+	u_int16_t cpi, host_t *src, host_t *dst, host_t *new_src, host_t *new_dst,
+	bool encap, bool new_encap)
 {
 	if (!this->ipsec)
 	{
 		return NOT_SUPPORTED;
 	}
 	return this->ipsec->update_sa(this->ipsec, spi, protocol, cpi, src, dst,
-			new_src, new_dst, encap, new_encap);
+								  new_src, new_dst, encap, new_encap);
 }
 
-/**
- * Implementation of kernel_interface_t.query_sa
- */
-static status_t query_sa(private_kernel_interface_t *this, host_t *src, host_t *dst,
-						 u_int32_t spi, protocol_id_t protocol, u_int64_t *bytes)
+METHOD(kernel_interface_t, query_sa, status_t,
+	private_kernel_interface_t *this, host_t *src, host_t *dst,
+	u_int32_t spi, protocol_id_t protocol, u_int64_t *bytes)
 {
 	if (!this->ipsec)
 	{
@@ -115,11 +105,9 @@ static status_t query_sa(private_kernel_interface_t *this, host_t *src, host_t *
 	return this->ipsec->query_sa(this->ipsec, src, dst, spi, protocol, bytes);
 }
 
-/**
- * Implementation of kernel_interface_t.del_sa
- */
-static status_t del_sa(private_kernel_interface_t *this, host_t *src, host_t *dst,
-				u_int32_t spi, protocol_id_t protocol, u_int16_t cpi)
+METHOD(kernel_interface_t, del_sa, status_t,
+	private_kernel_interface_t *this, host_t *src, host_t *dst, u_int32_t spi,
+	protocol_id_t protocol, u_int16_t cpi)
 {
 	if (!this->ipsec)
 	{
@@ -128,14 +116,12 @@ static status_t del_sa(private_kernel_interface_t *this, host_t *src, host_t *ds
 	return this->ipsec->del_sa(this->ipsec, src, dst, spi, protocol, cpi);
 }
 
-/**
- * Implementation of kernel_interface_t.add_policy
- */
-static status_t add_policy(private_kernel_interface_t *this, host_t *src, host_t *dst,
-					traffic_selector_t *src_ts, traffic_selector_t *dst_ts,
-					policy_dir_t direction, u_int32_t spi, protocol_id_t protocol,
-					u_int32_t reqid, ipsec_mode_t mode, u_int16_t ipcomp, u_int16_t cpi,
-					bool routed)
+METHOD(kernel_interface_t, add_policy, status_t,
+	private_kernel_interface_t *this, host_t *src, host_t *dst,
+	traffic_selector_t *src_ts, traffic_selector_t *dst_ts,
+	policy_dir_t direction, u_int32_t spi, protocol_id_t protocol,
+	u_int32_t reqid, ipsec_mode_t mode, u_int16_t ipcomp, u_int16_t cpi,
+	bool routed)
 {
 	if (!this->ipsec)
 	{
@@ -145,39 +131,32 @@ static status_t add_policy(private_kernel_interface_t *this, host_t *src, host_t
 			direction, spi, protocol, reqid, mode, ipcomp, cpi, routed);
 }
 
-/**
- * Implementation of kernel_interface_t.query_policy
- */
-static status_t query_policy(private_kernel_interface_t *this,
-					  traffic_selector_t *src_ts, traffic_selector_t *dst_ts,
-					  policy_dir_t direction, u_int32_t *use_time)
+METHOD(kernel_interface_t, query_policy, status_t,
+	private_kernel_interface_t *this, traffic_selector_t *src_ts,
+	traffic_selector_t *dst_ts, policy_dir_t direction, u_int32_t *use_time)
 {
 	if (!this->ipsec)
 	{
 		return NOT_SUPPORTED;
 	}
-	return this->ipsec->query_policy(this->ipsec, src_ts, dst_ts, direction, use_time);
+	return this->ipsec->query_policy(this->ipsec, src_ts, dst_ts,
+									 direction, use_time);
 }
 
-/**
- * Implementation of kernel_interface_t.del_policy
- */
-static status_t del_policy(private_kernel_interface_t *this,
-					traffic_selector_t *src_ts, traffic_selector_t *dst_ts,
-					policy_dir_t direction, bool unrouted)
+METHOD(kernel_interface_t, del_policy, status_t,
+	private_kernel_interface_t *this, traffic_selector_t *src_ts,
+	traffic_selector_t *dst_ts, policy_dir_t direction, bool unrouted)
 {
 	if (!this->ipsec)
 	{
 		return NOT_SUPPORTED;
 	}
-	return this->ipsec->del_policy(this->ipsec, src_ts, dst_ts, direction, unrouted);
+	return this->ipsec->del_policy(this->ipsec, src_ts, dst_ts,
+								   direction, unrouted);
 }
 
-/**
- * Implementation of kernel_interface_t.get_source_addr
- */
-static host_t *get_source_addr(private_kernel_interface_t *this,
-							   host_t *dest, host_t *src)
+METHOD(kernel_interface_t, get_source_addr, host_t*,
+	private_kernel_interface_t *this, host_t *dest, host_t *src)
 {
 	if (!this->net)
 	{
@@ -186,10 +165,8 @@ static host_t *get_source_addr(private_kernel_interface_t *this,
 	return this->net->get_source_addr(this->net, dest, src);
 }
 
-/**
- * Implementation of kernel_interface_t.get_nexthop
- */
-static host_t *get_nexthop(private_kernel_interface_t *this, host_t *dest)
+METHOD(kernel_interface_t, get_nexthop, host_t*,
+	private_kernel_interface_t *this, host_t *dest)
 {
 	if (!this->net)
 	{
@@ -198,10 +175,8 @@ static host_t *get_nexthop(private_kernel_interface_t *this, host_t *dest)
 	return this->net->get_nexthop(this->net, dest);
 }
 
-/**
- * Implementation of kernel_interface_t.get_interface
- */
-static char* get_interface(private_kernel_interface_t *this, host_t *host)
+METHOD(kernel_interface_t, get_interface, char*,
+	private_kernel_interface_t *this, host_t *host)
 {
 	if (!this->net)
 	{
@@ -210,25 +185,20 @@ static char* get_interface(private_kernel_interface_t *this, host_t *host)
 	return this->net->get_interface(this->net, host);
 }
 
-/**
- * Implementation of kernel_interface_t.create_address_enumerator
- */
-static enumerator_t *create_address_enumerator(private_kernel_interface_t *this,
-		bool include_down_ifaces, bool include_virtual_ips)
+METHOD(kernel_interface_t, create_address_enumerator, enumerator_t*,
+	private_kernel_interface_t *this, bool include_down_ifaces,
+	bool include_virtual_ips)
 {
 	if (!this->net)
 	{
 		return enumerator_create_empty();
 	}
 	return this->net->create_address_enumerator(this->net, include_down_ifaces,
-			include_virtual_ips);
+												include_virtual_ips);
 }
 
-/**
- * Implementation of kernel_interface_t.add_ip
- */
-static status_t add_ip(private_kernel_interface_t *this, host_t *virtual_ip,
-				host_t *iface_ip)
+METHOD(kernel_interface_t, add_ip, status_t,
+	private_kernel_interface_t *this, host_t *virtual_ip, host_t *iface_ip)
 {
 	if (!this->net)
 	{
@@ -237,10 +207,8 @@ static status_t add_ip(private_kernel_interface_t *this, host_t *virtual_ip,
 	return this->net->add_ip(this->net, virtual_ip, iface_ip);
 }
 
-/**
- * Implementation of kernel_interface_t.del_ip
- */
-static status_t del_ip(private_kernel_interface_t *this, host_t *virtual_ip)
+METHOD(kernel_interface_t, del_ip, status_t,
+	private_kernel_interface_t *this, host_t *virtual_ip)
 {
 	if (!this->net)
 	{
@@ -249,40 +217,33 @@ static status_t del_ip(private_kernel_interface_t *this, host_t *virtual_ip)
 	return this->net->del_ip(this->net, virtual_ip);
 }
 
-/**
- * Implementation of kernel_interface_t.add_route
- */
-static status_t add_route(private_kernel_interface_t *this, chunk_t dst_net,
-		u_int8_t prefixlen, host_t *gateway, host_t *src_ip, char *if_name)
+METHOD(kernel_interface_t, add_route, status_t,
+	private_kernel_interface_t *this, chunk_t dst_net,
+	u_int8_t prefixlen, host_t *gateway, host_t *src_ip, char *if_name)
 {
 	if (!this->net)
 	{
 		return NOT_SUPPORTED;
 	}
-	return this->net->add_route(this->net, dst_net, prefixlen, gateway, src_ip,
-			if_name);
+	return this->net->add_route(this->net, dst_net, prefixlen, gateway,
+								src_ip, if_name);
 }
 
-/**
- * Implementation of kernel_interface_t.del_route
- */
-static status_t del_route(private_kernel_interface_t *this, chunk_t dst_net,
-		u_int8_t prefixlen, host_t *gateway, host_t *src_ip, char *if_name)
+METHOD(kernel_interface_t, del_route, status_t,
+	private_kernel_interface_t *this, chunk_t dst_net,
+	u_int8_t prefixlen, host_t *gateway, host_t *src_ip, char *if_name)
 {
 	if (!this->net)
 	{
 		return NOT_SUPPORTED;
 	}
-	return this->net->del_route(this->net, dst_net, prefixlen, gateway, src_ip,
-			if_name);
+	return this->net->del_route(this->net, dst_net, prefixlen, gateway,
+								src_ip, if_name);
 }
 
 
-/**
- * Implementation of kernel_interface_t.get_address_by_ts
- */
-static status_t get_address_by_ts(private_kernel_interface_t *this,
-								  traffic_selector_t *ts, host_t **ip)
+METHOD(kernel_interface_t, get_address_by_ts, status_t,
+	private_kernel_interface_t *this, traffic_selector_t *ts, host_t **ip)
 {
 	enumerator_t *addrs;
 	host_t *host;
@@ -336,11 +297,8 @@ static status_t get_address_by_ts(private_kernel_interface_t *this,
 }
 
 
-/**
- * Implementation of kernel_interface_t.add_ipsec_interface.
- */
-static void add_ipsec_interface(private_kernel_interface_t *this,
-		kernel_ipsec_constructor_t constructor)
+METHOD(kernel_interface_t, add_ipsec_interface, void,
+	private_kernel_interface_t *this, kernel_ipsec_constructor_t constructor)
 {
 	if (!this->ipsec)
 	{
@@ -348,20 +306,14 @@ static void add_ipsec_interface(private_kernel_interface_t *this,
 	}
 }
 
-/**
- * Implementation of kernel_interface_t.remove_ipsec_interface.
- */
-static void remove_ipsec_interface(private_kernel_interface_t *this,
-		kernel_ipsec_constructor_t constructor)
+METHOD(kernel_interface_t, remove_ipsec_interface, void,
+	private_kernel_interface_t *this, kernel_ipsec_constructor_t constructor)
 {
 	/* TODO: replace if interface currently in use */
 }
 
-/**
- * Implementation of kernel_interface_t.add_net_interface.
- */
-static void add_net_interface(private_kernel_interface_t *this,
-		kernel_net_constructor_t constructor)
+METHOD(kernel_interface_t, add_net_interface, void,
+	private_kernel_interface_t *this, kernel_net_constructor_t constructor)
 {
 	if (!this->net)
 	{
@@ -369,19 +321,14 @@ static void add_net_interface(private_kernel_interface_t *this,
 	}
 }
 
-/**
- * Implementation of kernel_interface_t.remove_net_interface.
- */
-static void remove_net_interface(private_kernel_interface_t *this,
-		kernel_net_constructor_t constructor)
+METHOD(kernel_interface_t, remove_net_interface, void,
+	private_kernel_interface_t *this, kernel_net_constructor_t constructor)
 {
 	/* TODO: replace if interface currently in use */
 }
 
-/**
- * Implementation of kernel_interface_t.destroy.
- */
-static void destroy(private_kernel_interface_t *this)
+METHOD(kernel_interface_t, destroy, void,
+	private_kernel_interface_t *this)
 {
 	DESTROY_IF(this->ipsec);
 	DESTROY_IF(this->net);
@@ -393,38 +340,37 @@ static void destroy(private_kernel_interface_t *this)
  */
 kernel_interface_t *kernel_interface_create()
 {
-	private_kernel_interface_t *this = malloc_thing(private_kernel_interface_t);
+	private_kernel_interface_t *this;
 
-	this->public.get_spi = (status_t(*)(kernel_interface_t*,host_t*,host_t*,protocol_id_t,u_int32_t,u_int32_t*))get_spi;
-	this->public.get_cpi = (status_t(*)(kernel_interface_t*,host_t*,host_t*,u_int32_t,u_int16_t*))get_cpi;
-	this->public.add_sa  = (status_t(*)(kernel_interface_t *,host_t*,host_t*,u_int32_t,protocol_id_t,u_int32_t,lifetime_cfg_t*,u_int16_t,chunk_t,u_int16_t,chunk_t,ipsec_mode_t,u_int16_t,u_int16_t,bool,bool,traffic_selector_t*,traffic_selector_t*))add_sa;
-	this->public.update_sa = (status_t(*)(kernel_interface_t*,u_int32_t,protocol_id_t,u_int16_t,host_t*,host_t*,host_t*,host_t*,bool,bool))update_sa;
-	this->public.query_sa = (status_t(*)(kernel_interface_t*,host_t*,host_t*,u_int32_t,protocol_id_t,u_int64_t*))query_sa;
-	this->public.del_sa = (status_t(*)(kernel_interface_t*,host_t*,host_t*,u_int32_t,protocol_id_t,u_int16_t))del_sa;
-	this->public.add_policy = (status_t(*)(kernel_interface_t*,host_t*,host_t*,traffic_selector_t*,traffic_selector_t*,policy_dir_t,u_int32_t,protocol_id_t,u_int32_t,ipsec_mode_t,u_int16_t,u_int16_t,bool))add_policy;
-	this->public.query_policy = (status_t(*)(kernel_interface_t*,traffic_selector_t*,traffic_selector_t*,policy_dir_t,u_int32_t*))query_policy;
-	this->public.del_policy = (status_t(*)(kernel_interface_t*,traffic_selector_t*,traffic_selector_t*,policy_dir_t,bool))del_policy;
+	INIT(this,
+		.public = {
+			.get_spi = _get_spi,
+			.get_cpi = _get_cpi,
+			.add_sa = _add_sa,
+			.update_sa = _update_sa,
+			.query_sa = _query_sa,
+			.del_sa = _del_sa,
+			.add_policy = _add_policy,
+			.query_policy = _query_policy,
+			.del_policy = _del_policy,
+			.get_source_addr = _get_source_addr,
+			.get_nexthop = _get_nexthop,
+			.get_interface = _get_interface,
+			.create_address_enumerator = _create_address_enumerator,
+			.add_ip = _add_ip,
+			.del_ip = _del_ip,
+			.add_route = _add_route,
+			.del_route = _del_route,
 
-	this->public.get_source_addr = (host_t*(*)(kernel_interface_t*, host_t *dest, host_t *src))get_source_addr;
-	this->public.get_nexthop = (host_t*(*)(kernel_interface_t*, host_t *dest))get_nexthop;
-	this->public.get_interface = (char*(*)(kernel_interface_t*,host_t*))get_interface;
-	this->public.create_address_enumerator = (enumerator_t*(*)(kernel_interface_t*,bool,bool))create_address_enumerator;
-	this->public.add_ip = (status_t(*)(kernel_interface_t*,host_t*,host_t*)) add_ip;
-	this->public.del_ip = (status_t(*)(kernel_interface_t*,host_t*)) del_ip;
-	this->public.add_route = (status_t(*)(kernel_interface_t*,chunk_t,u_int8_t,host_t*,host_t*,char*)) add_route;
-	this->public.del_route = (status_t(*)(kernel_interface_t*,chunk_t,u_int8_t,host_t*,host_t*,char*)) del_route;
-
-	this->public.get_address_by_ts = (status_t(*)(kernel_interface_t*,traffic_selector_t*,host_t**))get_address_by_ts;
-
-	this->public.add_ipsec_interface = (void(*)(kernel_interface_t*, kernel_ipsec_constructor_t))add_ipsec_interface;
-	this->public.remove_ipsec_interface = (void(*)(kernel_interface_t*, kernel_ipsec_constructor_t))remove_ipsec_interface;
-	this->public.add_net_interface = (void(*)(kernel_interface_t*, kernel_net_constructor_t))add_net_interface;
-	this->public.remove_net_interface = (void(*)(kernel_interface_t*, kernel_net_constructor_t))remove_net_interface;
-
-	this->public.destroy = (void (*)(kernel_interface_t*))destroy;
-
-	this->ipsec = NULL;
-	this->net = NULL;
+			.get_address_by_ts = _get_address_by_ts,
+			.add_ipsec_interface = _add_ipsec_interface,
+			.remove_ipsec_interface = _remove_ipsec_interface,
+			.add_net_interface = _add_net_interface,
+			.remove_net_interface = _remove_net_interface,
+			.destroy = _destroy,
+		},
+	);
 
 	return &this->public;
 }
+
