717d60f13f3b542849fd795aaffc9b1b308d3385
Tobias Brunner <tobias@strongswan.org>
2010-07-29 12:24:18 +0200

    pluto: Added missing return_on in out_sa.

--- src/pluto/spdb.c
+++ src/pluto/spdb.c
@@ -234,7 +234,7 @@ out_attr(int type
 				, val, enum_show(d, val)));
 	return TRUE;
 }
-#define return_on(var, val) do { var=val;goto return_out; } while(0);
+#define return_on(var, val) do { var=val;goto return_out; } while(0)
 /* Output an SA, as described by a db_sa.
  * This has the side-effect of allocating SPIs for us.
  */
@@ -448,7 +448,7 @@ out_sa(pb_stream *outs
 							, &st->st_connection->spd
 							, tunnel_mode);
 						if (*spi_ptr == 0)
-							return FALSE;
+							return_on(ret, FALSE);
 						*spi_generated = TRUE;
 					}
 					if (!out_raw((u_char *)spi_ptr, IPSEC_DOI_SPI_SIZE
