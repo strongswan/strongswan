52e6df0cdc52b4541ac9a37f3ffb66e3fc12b8aa
Martin Willi <martin@revosec.ch>
2010-11-02 15:51:56 +0100

    Implemented a add_notify hook to inject arbitrary Notify payloads

---
+++ src/conftest/hooks/add_notify.c
@@ -0,0 +1,140 @@
+/*
+ * Copyright (C) 2010 Martin Willi
+ * Copyright (C) 2010 revosec AG
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+
+#include "hook.h"
+
+typedef struct private_add_notify_t private_add_notify_t;
+
+/**
+ * Private data of an add_notify_t object.
+ */
+struct private_add_notify_t {
+
+	/**
+	 * Implements the hook_t interface.
+	 */
+	hook_t hook;
+
+	/**
+	 * Alter requests or responses?
+	 */
+	bool req;
+
+	/**
+	 * ID of message to alter.
+	 */
+	int id;
+
+	/**
+	 * Notify type
+	 */
+	char *type;
+
+	/**
+	 * Notify data
+	 */
+	char *data;
+
+	/**
+	 * SPI of notify
+	 */
+	int spi;
+
+	/**
+	 * TRUE for a ESP protocol notify, FALSE for IKE
+	 */
+	bool esp;
+};
+
+METHOD(listener_t, message, bool,
+	private_add_notify_t *this, ike_sa_t *ike_sa, message_t *message,
+	bool incoming)
+{
+	if (!incoming &&
+		message->get_request(message) == this->req &&
+		message->get_message_id(message) == this->id)
+	{
+		notify_type_t type;
+		notify_payload_t *notify;
+		chunk_t data = chunk_empty;
+
+		type = atoi(this->type);
+		if (!type)
+		{
+			type = enum_from_name(notify_type_names, this->type);
+			if (type == -1)
+			{
+				DBG1(DBG_CFG, "unknown notify: '%s', skipped", this->type);
+				return TRUE;
+			}
+		}
+		if (strncasecmp(this->data, "0x", 2) == 0)
+		{
+			data = chunk_skip(chunk_create(this->data, strlen(this->data)), 2);
+			data = chunk_from_hex(data, NULL);
+		}
+		else if (data.len)
+		{
+			data = chunk_clone(chunk_create(this->data, strlen(this->data)));
+		}
+		notify = notify_payload_create_from_protocol_and_type(
+									this->esp ? PROTO_ESP : PROTO_IKE, type);
+		notify->set_spi(notify, this->spi);
+		if (data.len)
+		{
+			notify->set_notification_data(notify, data);
+			free(data.ptr);
+		}
+		message->add_payload(message, &notify->payload_interface);
+	}
+	return TRUE;
+}
+
+METHOD(hook_t, destroy, void,
+	private_add_notify_t *this)
+{
+	free(this);
+}
+
+/**
+ * Create the IKE_AUTH fill hook
+ */
+hook_t *add_notify_hook_create(void)
+{
+	private_add_notify_t *this;
+
+	INIT(this,
+		.hook = {
+			.listener = {
+				.message = _message,
+			},
+			.destroy = _destroy,
+		},
+		.req = conftest->test->get_bool(conftest->test,
+										"hooks.add_notify.request", TRUE),
+		.id = conftest->test->get_int(conftest->test,
+										"hooks.add_notify.id", 0),
+		.type = conftest->test->get_str(conftest->test,
+										"hooks.add_notify.type", ""),
+		.data = conftest->test->get_str(conftest->test,
+										"hooks.add_notify.data", ""),
+		.spi = conftest->test->get_int(conftest->test,
+										"hooks.add_notify.spi", 0),
+		.esp = conftest->test->get_bool(conftest->test,
+										"hooks.add_notify.esp", FALSE),
+	);
+
+	return &this->hook;
+}
