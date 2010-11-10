30419567709c73a97eee5a8ce667d3b0584237fa
Andreas Steffen <andreas.steffen@strongswan.org>
2010-11-10 21:22:27 +0100

    created enum names for action recommendations and evaluation results

---
+++ src/libcharon/tnc/tncifimv_names.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2010 Andreas Steffen
+ * HSR Hochschule fuer Technik Rapperswil
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+
+#include "tncifimv.h"
+#include "tncifimv_names.h"
+
+ENUM(action_recommendation_names, TNC_IMV_ACTION_RECOMMENDATION_ALLOW,
+								  TNC_IMV_ACTION_RECOMMENDATION_NO_RECOMMENDATION,
+	"allow",
+	"no access",
+	"isolate",
+	"no recommendation"
+);
+
+ENUM(evaluation_result_names, TNC_IMV_EVALUATION_RESULT_COMPLIANT,
+							  TNC_IMV_EVALUATION_RESULT_DONT_KNOW,
+	"compliant",
+	"non-compliant minor",
+	"non-compliant major",
+	"error",
+	"don't know"
+);
+
---
+++ src/libcharon/tnc/tncifimv_names.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2010 Andreas Steffen
+ * HSR Hochschule fuer Technik Rapperswil
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+
+/**
+ * @defgroup tncifimv_names tncifimv_names
+ * @{ @ingroup libcharon
+ */
+
+#ifndef TNCIFIMV_NAMES_H_
+#define TNCIFIMV_NAMES_H_
+
+#include <library.h>
+
+extern enum_name_t *action_recommendation_names;
+extern enum_name_t *evaluation_result_names;
+
+#endif /** TNCIFIMV_NAMES_H_ @}*/
