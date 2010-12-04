c7b778450e477f3537f14d3a445c23438f5dcf86
Andreas Steffen <andreas.steffen@strongswan.org>
2010-12-04 20:45:49 +0100

    Migrated md4_plugin_t to INIT/METHOD macros

--- src/libstrongswan/plugins/md4/md4_plugin.c
+++ src/libstrongswan/plugins/md4/md4_plugin.c
@@ -31,10 +31,8 @@ struct private_md4_plugin_t {
 	md4_plugin_t public;
 };
 
-/**
- * Implementation of md4_plugin_t.destroy
- */
-static void destroy(private_md4_plugin_t *this)
+METHOD(plugin_t, destroy, void,
+	private_md4_plugin_t *this)
 {
 	lib->crypto->remove_hasher(lib->crypto,
 							   (hasher_constructor_t)md4_hasher_create);
@@ -46,9 +44,15 @@ static void destroy(private_md4_plugin_t *this)
  */
 plugin_t *md4_plugin_create()
 {
-	private_md4_plugin_t *this = malloc_thing(private_md4_plugin_t);
+	private_md4_plugin_t *this;
 
-	this->public.plugin.destroy = (void(*)(plugin_t*))destroy;
+	INIT(this,
+		.public = {
+			.plugin = {
+				.destroy = _destroy,
+			},
+		},
+	);
 
 	lib->crypto->add_hasher(lib->crypto, HASH_MD4,
 							(hasher_constructor_t)md4_hasher_create);
