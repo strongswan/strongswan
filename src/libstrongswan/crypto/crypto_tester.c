c6a043fad08086ec5b2ff98b335053809e29bc05
Andreas Steffen <andreas.steffen@strongswan.org>
2010-12-25 15:53:15 +0100

    log if an AEAD algorithm does not support a given key size

--- src/libstrongswan/crypto/crypto_tester.c
+++ src/libstrongswan/crypto/crypto_tester.c
@@ -188,7 +188,8 @@ METHOD(crypto_tester_t, test_crypter, bool,
 		}
 		crypter = create(alg, vector->key_size);
 		if (!crypter)
-		{	DBG1(DBG_LIB, "%N[%s]: %u bit key size not supported",
+		{
+			DBG1(DBG_LIB, "%N[%s]: %u bit key size not supported",
 				 encryption_algorithm_names, alg, plugin_name,
 				 BITS_PER_BYTE * vector->key_size);
 			continue;
@@ -337,7 +338,10 @@ METHOD(crypto_tester_t, test_aead, bool,
 		}
 		aead = create(alg, vector->key_size);
 		if (!aead)
-		{	/* key size not supported... */
+		{
+			DBG1(DBG_LIB, "%N[%s]: %u bit key size not supported",
+				 encryption_algorithm_names, alg, plugin_name,
+				 BITS_PER_BYTE * vector->key_size);
 			continue;
 		}
 
