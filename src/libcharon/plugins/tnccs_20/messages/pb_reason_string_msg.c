97613b3b1a171654e3151512ec4a14b7f57ad3aa
Andreas Steffen <andreas.steffen@strongswan.org>
2011-01-05 03:44:57 +0100

    remove private_

--- src/libcharon/plugins/tnccs_20/messages/pb_reason_string_msg.c
+++ src/libcharon/plugins/tnccs_20/messages/pb_reason_string_msg.c
@@ -36,7 +36,7 @@ typedef struct private_pb_reason_string_msg_t private_pb_reason_string_msg_t;
  */
 
 /**
- * Private data of a private_pb_reason_string_msg_t object.
+ * Private data of a pb_reason_string_msg_t object.
  *
  */
 struct private_pb_reason_string_msg_t {
