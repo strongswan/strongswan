64e06d4f8fb72812e860be2bbd1d4d7282b90bc5
Martin Willi <martin@revosec.ch>
2011-01-14 13:22:19 +0100

    Compare ending address in ts->equals, fixes redundant traffic selector elimination

--- src/libstrongswan/selectors/traffic_selector.c
+++ src/libstrongswan/selectors/traffic_selector.c
@@ -393,13 +393,15 @@ static bool equals(private_traffic_selector_t *this, private_traffic_selector_t 
 	switch (this->type)
 	{
 		case TS_IPV4_ADDR_RANGE:
-			if (memeq(this->from4, other->from4, sizeof(this->from4)))
+			if (memeq(this->from4, other->from4, sizeof(this->from4)) &&
+				memeq(this->to4, other->to4, sizeof(this->to4)))
 			{
 				return TRUE;
 			}
 			break;
 		case TS_IPV6_ADDR_RANGE:
-			if (memeq(this->from6, other->from6, sizeof(this->from6)))
+			if (memeq(this->from6, other->from6, sizeof(this->from6)) &&
+				memeq(this->to6, other->to6, sizeof(this->to6)))
 			{
 				return TRUE;
 			}
