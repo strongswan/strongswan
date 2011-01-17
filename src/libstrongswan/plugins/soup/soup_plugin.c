2ca52c8048286675d995b17527f7d54610e1f587
Martin Willi <martin@revosec.ch>
2011-01-17 13:27:18 +0100

    Implemented an alternative HTTP fetcher based on libsoup

---
+++ src/libstrongswan/plugins/soup/soup_plugin.c
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2010 Martin Willi
+ * Copyright (C) 2010 revosec AG
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+
+#include "soup_plugin.h"
+#include "soup_fetcher.h"
+
+#include <glib.h>
+#include <glib-object.h>
+
+#include <library.h>
+
+typedef struct private_soup_plugin_t private_soup_plugin_t;
+
+/**
+ * private data of soup_plugin
+ */
+struct private_soup_plugin_t {
+
+	/**
+	 * public functions
+	 */
+	soup_plugin_t public;
+};
+
+METHOD(plugin_t, destroy, void,
+	private_soup_plugin_t *this)
+{
+	lib->fetcher->remove_fetcher(lib->fetcher,
+								 (fetcher_constructor_t)soup_fetcher_create);
+	free(this);
+}
+
+/*
+ * see header file
+ */
+plugin_t *soup_plugin_create()
+{
+	private_soup_plugin_t *this;
+
+	g_type_init();
+	if (!g_thread_get_initialized())
+	{
+		g_thread_init(NULL);
+	}
+
+	INIT(this,
+		.public = {
+			.plugin = {
+				.destroy = _destroy,
+			},
+		},
+	);
+
+	lib->fetcher->add_fetcher(lib->fetcher,
+					(fetcher_constructor_t)soup_fetcher_create, "http://");
+	lib->fetcher->add_fetcher(lib->fetcher,
+					(fetcher_constructor_t)soup_fetcher_create, "https://");
+
+	return &this->public.plugin;
+}
