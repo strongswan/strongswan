0700c153e736c064e41442b35a7a4aaf0d5b7845
Martin Willi <martin@revosec.ch>
2011-02-08 10:14:56 +0100

    Fixed function parameter description

--- src/libcharon/encoding/payloads/cert_payload.h
+++ src/libcharon/encoding/payloads/cert_payload.h
@@ -138,7 +138,7 @@ cert_payload_t *cert_payload_create_from_hash_and_url(chunk_t hash, char *url);
  * Creates a custom certificate payload using type and associated data.
  *
  * @param type				encoding type of certificate
- * @param url				associated data (gets owned)
+ * @param data				associated data (gets owned)
  * @return					cert_payload_t object
  */
 cert_payload_t *cert_payload_create_custom(cert_encoding_t type, chunk_t data);
