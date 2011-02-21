007c47088cb1b4837eca5aa9a119a4f39df192ec
Martin Willi <martin@revosec.ch>
2011-02-21 13:05:21 +0000

    Implemented permanent certificate coupling plugin

---
+++ src/libcharon/plugins/coupling/coupling_plugin.c
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2011 Martin Willi
+ * Copyright (C) 2011 revosec AG
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+
+#include "coupling_plugin.h"
+
+#include "coupling_validator.h"
+
+#include <daemon.h>
+
+typedef struct private_coupling_plugin_t private_coupling_plugin_t;
+
+/**
+ * private data of coupling plugin
+ */
+struct private_coupling_plugin_t {
+
+	/**
+	 * implements plugin interface
+	 */
+	coupling_plugin_t public;
+
+	/**
+	 * validator controlling couplings
+	 */
+	coupling_validator_t *validator;
+};
+
+METHOD(plugin_t, destroy, void,
+	private_coupling_plugin_t *this)
+{
+	lib->credmgr->remove_validator(lib->credmgr, &this->validator->validator);
+	this->validator->destroy(this->validator);
+	free(this);
+}
+
+/**
+ * Plugin constructor
+ */
+plugin_t *coupling_plugin_create()
+{
+	private_coupling_plugin_t *this;
+
+	INIT(this,
+		.public = {
+			.plugin = {
+				.destroy = _destroy,
+			},
+		},
+		.validator = coupling_validator_create(),
+	);
+
+	if (!this->validator)
+	{
+		free(this);
+		return NULL;
+	}
+
+	lib->credmgr->add_validator(lib->credmgr, &this->validator->validator);
+
+	return &this->public.plugin;
+}
