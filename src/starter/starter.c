67ec2be6653b528bb1a04f5aa3fc0c45276c5ed1
Tobias Brunner <tobias@strongswan.org>
2011-04-27 11:33:06 +0200

    IKEv2 was only partially the default for connections with auto=route and auto=start.
    Connections with auto=route and auto=start that did not have
    keyexchange=ikev2 explicitly specified did get added to charon,
    but did not get routed or started by charon.

--- src/starter/starter.c
+++ src/starter/starter.c
@@ -714,7 +714,7 @@ int main (int argc, char **argv)
 
 					if (conn->startup == STARTUP_START)
 					{
-						if (conn->keyexchange == KEY_EXCHANGE_IKEV2)
+						if (conn->keyexchange != KEY_EXCHANGE_IKEV1)
 						{
 							if (starter_charon_pid())
 							{
@@ -731,7 +731,7 @@ int main (int argc, char **argv)
 					}
 					else if (conn->startup == STARTUP_ROUTE)
 					{
-						if (conn->keyexchange == KEY_EXCHANGE_IKEV2)
+						if (conn->keyexchange != KEY_EXCHANGE_IKEV1)
 						{
 							if (starter_charon_pid())
 							{
