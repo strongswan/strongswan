8a59adad57ca3cd452b661e02232d9bc57413a61
Andreas Steffen <andreas.steffen@strongswan.org>
2011-08-22 22:37:24 +0200

    replaced unreliable tabs by spaces

--- src/libimcv/tcg/pts/pts_error.h
+++ src/libimcv/tcg/pts/pts_error.h
@@ -26,28 +26,27 @@ typedef enum pts_error_code_t pts_error_code_t;
 #include <library.h>
 
 /**
-  * PTS Attestation Error Codes
-  * see section 3.14.2 of PTS Protocol: Binding to TNC IF-M Specification
+ * PTS Attestation Error Codes
+ * see section 3.14.2 of PTS Protocol: Binding to TNC IF-M Specification
  */
 enum pts_error_code_t {
-	TCG_PTS_RESERVED_ERROR =		0,
-	TCG_PTS_HASH_ALG_NOT_SUPPORTED =	1,
-	TCG_PTS_INVALID_PATH =			2,
-	TCG_PTS_FILE_NOT_FOUND =		3,
-	TCG_PTS_REG_NOT_SUPPORTED =		4,
-	TCG_PTS_REG_KEY_NOT_FOUND =		5,
-	TCG_PTS_DH_GRPS_NOT_SUPPORTED =		6,
-	TCG_PTS_BAD_NONCE_LENGTH =		7,
-	TCG_PTS_INVALID_NAME_FAM =		8,
-	TCG_PTS_TPM_VERS_NOT_SUPPORTED =	9,
-	TCG_PTS_INVALID_DELIMITER =		10,
-	TCG_PTS_OPERATION_NOT_SUPPORTED =	11,
-	TCG_PTS_RM_ERROR =			12,
-	TCG_PTS_UNABLE_LOCAL_VAL =		13,
-	TCG_PTS_UNABLE_CUR_EVID =		14,
-	TCG_PTS_UNABLE_DET_TTC =		15,
-	TCG_PTS_UNABLE_DET_PCR =		16,
-	PB_BATCH_ROOF =				16
+	TCG_PTS_RESERVED_ERROR =           0,
+	TCG_PTS_HASH_ALG_NOT_SUPPORTED =   1,
+	TCG_PTS_INVALID_PATH =             2,
+	TCG_PTS_FILE_NOT_FOUND =           3,
+	TCG_PTS_REG_NOT_SUPPORTED =        4,
+	TCG_PTS_REG_KEY_NOT_FOUND =        5,
+	TCG_PTS_DH_GRPS_NOT_SUPPORTED =    6,
+	TCG_PTS_BAD_NONCE_LENGTH =         7,
+	TCG_PTS_INVALID_NAME_FAM =         8,
+	TCG_PTS_TPM_VERS_NOT_SUPPORTED =   9,
+	TCG_PTS_INVALID_DELIMITER =	      10,
+	TCG_PTS_OPERATION_NOT_SUPPORTED = 11,
+	TCG_PTS_RM_ERROR =                12,
+	TCG_PTS_UNABLE_LOCAL_VAL =        13,
+	TCG_PTS_UNABLE_CUR_EVID =         14,
+	TCG_PTS_UNABLE_DET_TTC =          15,
+	TCG_PTS_UNABLE_DET_PCR =          16,
 };
 
 /**
