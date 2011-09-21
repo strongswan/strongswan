8a55b83319c353f8af5f77fb5e7de2655b0bcfd5
Sansar Choinyambuu <schoinya@hsr.ch>
2011-09-21 10:36:50 +0200

    Implemented Diffie Hellman Nonce attributes

--- src/libimcv/plugins/imc_attestation/imc_attestation.c
+++ src/libimcv/plugins/imc_attestation/imc_attestation.c
@@ -27,6 +27,9 @@
 #include <pts/pts_error.h>
 
 #include <tcg/tcg_pts_attr_proto_caps.h>
+#include <tcg/tcg_pts_attr_dh_nonce_params_req.h>
+#include <tcg/tcg_pts_attr_dh_nonce_params_resp.h>
+#include <tcg/tcg_pts_attr_dh_nonce_finish.h>
 #include <tcg/tcg_pts_attr_meas_algo.h>
 #include <tncif_pa_subtypes.h>
 
