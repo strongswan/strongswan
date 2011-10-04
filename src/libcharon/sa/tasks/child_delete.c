4fb6b7a12cd9c8d1ea4ba4e72e3577eca7553c9f
Andreas Steffen <andreas.steffen@strongswan.org>
2011-10-04 08:05:27 +0200

    Migrated child_delete to INIT/METHOD macros

--- src/libcharon/sa/tasks/child_delete.c
+++ src/libcharon/sa/tasks/child_delete.c
@@ -261,10 +261,8 @@ static void log_children(private_child_delete_t *this)
 	enumerator->destroy(enumerator);
 }
 
-/**
- * Implementation of task_t.build for initiator
- */
-static status_t build_i(private_child_delete_t *this, message_t *message)
+METHOD(task_t, build_i, status_t,
+	private_child_delete_t *this, message_t *message)
 {
 	child_sa_t *child_sa;
 
@@ -291,10 +289,8 @@ static status_t build_i(private_child_delete_t *this, message_t *message)
 	return NEED_MORE;
 }
 
-/**
- * Implementation of task_t.process for initiator
- */
-static status_t process_i(private_child_delete_t *this, message_t *message)
+METHOD(task_t, process_i, status_t,
+	private_child_delete_t *this, message_t *message)
 {
 	/* flush the list before adding new SAs */
 	this->child_sas->destroy(this->child_sas);
@@ -305,20 +301,16 @@ static status_t process_i(private_child_delete_t *this, message_t *message)
 	return destroy_and_reestablish(this);
 }
 
-/**
- * Implementation of task_t.process for initiator
- */
-static status_t process_r(private_child_delete_t *this, message_t *message)
+METHOD(task_t, process_r, status_t,
+	private_child_delete_t *this, message_t *message)
 {
 	process_payloads(this, message);
 	log_children(this);
 	return NEED_MORE;
 }
 
-/**
- * Implementation of task_t.build for responder
- */
-static status_t build_r(private_child_delete_t *this, message_t *message)
+METHOD(task_t, build_r, status_t,
+	private_child_delete_t *this, message_t *message)
 {
 	/* if we are rekeying, we send an empty informational */
 	if (this->ike_sa->get_state(this->ike_sa) != IKE_REKEYING)
@@ -329,28 +321,22 @@ static status_t build_r(private_child_delete_t *this, message_t *message)
 	return destroy_and_reestablish(this);
 }
 
-/**
- * Implementation of task_t.get_type
- */
-static task_type_t get_type(private_child_delete_t *this)
+METHOD(task_t, get_type, task_type_t,
+	private_child_delete_t *this)
 {
 	return CHILD_DELETE;
 }
 
-/**
- * Implementation of child_delete_t.get_child
- */
-static child_sa_t* get_child(private_child_delete_t *this)
+METHOD(child_delete_t , get_child, child_sa_t*,
+	private_child_delete_t *this)
 {
 	child_sa_t *child_sa = NULL;
 	this->child_sas->get_first(this->child_sas, (void**)&child_sa);
 	return child_sa;
 }
 
-/**
- * Implementation of task_t.migrate
- */
-static void migrate(private_child_delete_t *this, ike_sa_t *ike_sa)
+METHOD(task_t, migrate, void,
+	private_child_delete_t *this, ike_sa_t *ike_sa)
 {
 	this->check_delete_action = FALSE;
 	this->ike_sa = ike_sa;
@@ -359,10 +345,8 @@ static void migrate(private_child_delete_t *this, ike_sa_t *ike_sa)
 	this->child_sas = linked_list_create();
 }
 
-/**
- * Implementation of task_t.destroy
- */
-static void destroy(private_child_delete_t *this)
+METHOD(task_t, destroy, void,
+	private_child_delete_t *this)
 {
 	this->child_sas->destroy(this->child_sas);
 	free(this);
@@ -374,30 +358,33 @@ static void destroy(private_child_delete_t *this)
 child_delete_t *child_delete_create(ike_sa_t *ike_sa, protocol_id_t protocol,
 									u_int32_t spi)
 {
-	private_child_delete_t *this = malloc_thing(private_child_delete_t);
+	private_child_delete_t *this;
 
-	this->public.get_child = (child_sa_t*(*)(child_delete_t*))get_child;
-	this->public.task.get_type = (task_type_t(*)(task_t*))get_type;
-	this->public.task.migrate = (void(*)(task_t*,ike_sa_t*))migrate;
-	this->public.task.destroy = (void(*)(task_t*))destroy;
-
-	this->ike_sa = ike_sa;
-	this->check_delete_action = FALSE;
-	this->child_sas = linked_list_create();
-	this->protocol = protocol;
-	this->spi = spi;
-	this->rekeyed = FALSE;
+	INIT(this,
+		.public = {
+			.task = {
+				.get_type = _get_type,
+				.migrate = _migrate,
+				.destroy = _destroy,
+			},
+			.get_child = _get_child,
+		},
+		.ike_sa = ike_sa,
+		.child_sas = linked_list_create(),
+		.protocol = protocol,
+		.spi = spi,
+	);
 
 	if (protocol != PROTO_NONE)
 	{
-		this->public.task.build = (status_t(*)(task_t*,message_t*))build_i;
-		this->public.task.process = (status_t(*)(task_t*,message_t*))process_i;
+		this->public.task.build = _build_i;
+		this->public.task.process = _process_i;
 		this->initiator = TRUE;
 	}
 	else
 	{
-		this->public.task.build = (status_t(*)(task_t*,message_t*))build_r;
-		this->public.task.process = (status_t(*)(task_t*,message_t*))process_r;
+		this->public.task.build = _build_r;
+		this->public.task.process = _process_r;
 		this->initiator = FALSE;
 	}
 	return &this->public;
