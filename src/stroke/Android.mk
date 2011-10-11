b3b1419e0687ab019ac1ea880ed4f1aee6d56b68
Tobias Brunner <tobias@strongswan.org>
2011-10-11 16:17:55 +0200

    Build stroke on Android.

---
+++ src/stroke/Android.mk
@@ -0,0 +1,25 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+# copy-n-paste from Makefile.am
+LOCAL_SRC_FILES := \
+stroke.c stroke_msg.h stroke_keywords.c stroke_keywords.h
+
+# build stroke -----------------------------------------------------------------
+
+LOCAL_C_INCLUDES += \
+	$(libvstr_PATH) \
+	$(strongswan_PATH)/src/libstrongswan
+
+LOCAL_CFLAGS := $(strongswan_CFLAGS)
+
+LOCAL_MODULE := stroke
+
+LOCAL_ARM_MODE := arm
+
+LOCAL_PRELINK_MODULE := false
+
+LOCAL_SHARED_LIBRARIES += libstrongswan
+
+include $(BUILD_EXECUTABLE)
+
