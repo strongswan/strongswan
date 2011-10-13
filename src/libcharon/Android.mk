0ad58f013a608e1292c32d72ec09b241258aabd7
Tobias Brunner <tobias@strongswan.org>
2011-10-13 19:25:06 +0200

    Enable socket-raw on Android if pluto is enabled.

--- src/libcharon/Android.mk
+++ src/libcharon/Android.mk
@@ -147,6 +147,8 @@ LOCAL_SRC_FILES += $(call add_plugin, socket-default)
 
 LOCAL_SRC_FILES += $(call add_plugin, socket-dynamic)
 
+LOCAL_SRC_FILES += $(call add_plugin, socket-raw)
+
 LOCAL_SRC_FILES += $(call add_plugin, stroke)
 ifneq ($(call plugin_enabled, stroke),)
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../stroke/
