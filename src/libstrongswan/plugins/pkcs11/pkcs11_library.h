6a5020fc6758280e1f34bf76c4f2c40695a8ab89
Tobias Brunner <tobias@strongswan.org>
2011-10-28 18:07:02 +0200

    pkcs11: Added names for CKA_* constants.

--- src/libstrongswan/plugins/pkcs11/pkcs11_library.h
+++ src/libstrongswan/plugins/pkcs11/pkcs11_library.h
@@ -72,7 +72,7 @@ struct pkcs11_library_t {
 	 *
 	 * @param session	session to use
 	 * @param tmpl		search template
-	 * @param tcount 	number of attributes in the search template
+	 * @param tcount	number of attributes in the search template
 	 * @param attr		attributes to read from object
 	 * @param acount	number of attributes to read
 	 */
@@ -107,6 +107,11 @@ extern enum_name_t *ck_rv_names;
  * Enum names for CK_MECHANISM_TYPE values
  */
 extern enum_name_t *ck_mech_names;
+
+/**
+ * Enum names for CK_ATTRIBUTE_TYPE values
+ */
+extern enum_name_t *ck_attr_names;
 
 /**
  * Trim/null terminate a string returned by the varius PKCS#11 functions.
