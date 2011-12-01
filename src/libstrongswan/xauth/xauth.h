781f4c88988f23f5a6f155a59076b37164c954c9
Clavister OpenSource <opensource@clavister.com>
2011-12-01 13:11:36 +0100

    IKEv1 XAuth: Add XAuth defines for plugin types.

---
+++ src/libstrongswan/xauth/xauth.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2010 Martin Willi
+ * Copyright (C) 2010 revosec AG
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+
+/**
+ * @defgroup xauth xauth
+ * @{ @ingroup libstrongswan
+ */
+
+#ifndef XAUTH_H_
+#define XAUTH_H_
+
+typedef enum xauth_type_t xauth_type_t;
+
+#include <library.h>
+
+/**
+ * XAuth types, defines the XAuth method implementation
+ */
+enum xauth_type_t {
+	XAUTH_RADIUS = 253,
+};
+
+/**
+ * enum names for xauth_type_t.
+ */
+extern enum_name_t *xauth_method_type_names;
+
+/**
+ * short string enum names for xauth_type_t.
+ */
+extern enum_name_t *xauth_method_type_short_names;
+
+/**
+ * Lookup the XAuth method type from a string.
+ *
+ * @param name		XAuth method name (such as "md5", "aka")
+ * @return			method type, 0 if unknown
+ */
+xauth_type_t xauth_type_from_string(char *name);
+
+#endif /** XAUTH_H_ @}*/
