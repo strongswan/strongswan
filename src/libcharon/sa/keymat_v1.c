a846be311627f23e202bee65dae71d5731e53967
Clavister OpenSource <opensource@clavister.com>
2011-12-05 13:54:54 +0100

    keymat: derive_ike_keys updated with XAUTH RSA:s

--- src/libcharon/sa/keymat_v1.c
+++ src/libcharon/sa/keymat_v1.c
@@ -426,8 +426,14 @@ METHOD(keymat_v1_t, derive_ike_keys, bool,
 			this->prf->allocate_bytes(this->prf, nonces, &this->skeyid);
 			break;
 		}
-		case AUTH_RSA:
 		case AUTH_XAUTH_INIT_RSA:
+		case AUTH_XAUTH_RESP_RSA:
+		{
+			this->prf->set_key(this->prf, nonces);
+			this->prf->allocate_bytes(this->prf, g_xy, &this->skeyid);
+			break;
+		}
+		case AUTH_RSA:
 		{
 			/* signatures : SKEYID = prf(Ni_b | Nr_b, g^xy)
 			 * pubkey encr: SKEYID = prf(hash(Ni_b | Nr_b), CKY-I | CKY-R) */
