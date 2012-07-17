strongswan/strongswan

commit bc4748832395a5db4b71cb38d37f75de0d6e9416

Add a return value to prf_t.get_bytes()

src/libtls/tls_prf.c
@@ -56,12 +56,15 @@ static bool p_hash(prf_t *prf, char *label, chunk_t seed, size_t block_size,
 
 	while (TRUE)
 	{
-		/* A(i) = HMAC_hash(secret, A(i-1)) */
-		prf->get_bytes(prf, a, abuf);
 		a = chunk_from_thing(abuf);
-		/* HMAC_hash(secret, A(i) + seed) */
-		prf->get_bytes(prf, a, NULL);
-		prf->get_bytes(prf, seed, buf);
+		/* A(i) = HMAC_hash(secret, A(i-1))
+		 * HMAC_hash(secret, A(i) + seed) */
+		if (!prf->get_bytes(prf, a, abuf) ||
+			!prf->get_bytes(prf, a, NULL) ||
+			!prf->get_bytes(prf, seed, buf))
+		{
+			return FALSE;
+		}
 
 		if (bytes <= block_size)
 		{

src/libcharon/sa/ikev1/keymat_v1.c
@@ -222,31 +222,38 @@ METHOD(aead_t, aead_destroy, void,
  * Expand SKEYID_e according to Appendix B in RFC 2409.
  * TODO-IKEv1: verify keys (e.g. for weak keys, see Appendix B)
  */
-static chunk_t expand_skeyid_e(chunk_t skeyid_e, size_t key_size, prf_t *prf)
+static bool expand_skeyid_e(chunk_t skeyid_e, size_t key_size, prf_t *prf,
+							chunk_t *ka)
 {
 	size_t block_size;
-	chunk_t seed, ka;
+	chunk_t seed;
 	int i;
 
 	if (skeyid_e.len >= key_size)
 	{	/* no expansion required, reduce to key_size */
 		skeyid_e.len = key_size;
-		return skeyid_e;
+		*ka = skeyid_e;
+		return TRUE;
 	}
 	block_size = prf->get_block_size(prf);
-	ka = chunk_alloc((key_size / block_size + 1) * block_size);
-	ka.len = key_size;
+	*ka = chunk_alloc((key_size / block_size + 1) * block_size);
+	ka->len = key_size;
 
 	/* Ka = K1 | K2 | ..., K1 = prf(SKEYID_e, 0), K2 = prf(SKEYID_e, K1) ... */
 	prf->set_key(prf, skeyid_e);
 	seed = octet_0;
 	for (i = 0; i < key_size; i += block_size)
 	{
-		prf->get_bytes(prf, seed, ka.ptr + i);
-		seed = chunk_create(ka.ptr + i, block_size);
+		if (!prf->get_bytes(prf, seed, ka->ptr + i))
+		{
+			chunk_clear(ka);
+			chunk_clear(&skeyid_e);
+			return FALSE;
+		}
+		seed = chunk_create(ka->ptr + i, block_size);
 	}
 	chunk_clear(&skeyid_e);
-	return ka;
+	return TRUE;
 }
 
 /**
@@ -276,7 +283,10 @@ static aead_t *create_aead(proposal_t *proposal, prf_t *prf, chunk_t skeyid_e)
 		return NULL;
 	}
 	key_size = crypter->get_key_size(crypter);
-	ka = expand_skeyid_e(skeyid_e, crypter->get_key_size(crypter), prf);
+	if (!expand_skeyid_e(skeyid_e, crypter->get_key_size(crypter), prf, &ka))
+	{
+		return NULL;
+	}
 	DBG4(DBG_IKE, "encryption key Ka %B", &ka);
 	crypter->set_key(crypter, ka);
 	chunk_clear(&ka);

src/libsimaka/simaka_crypto.c
@@ -133,7 +133,12 @@ METHOD(simaka_crypto_t, derive_keys_full, bool,
 	str = chunk_alloca(this->prf->get_block_size(this->prf) * 3);
 	for (i = 0; i < 3; i++)
 	{
-		this->prf->get_bytes(this->prf, chunk_empty, str.ptr + str.len / 3 * i);
+		if (!this->prf->get_bytes(this->prf, chunk_empty,
+								  str.ptr + str.len / 3 * i))
+		{
+			chunk_clear(mk);
+			return FALSE;
+		}
 	}
 
 	k_encr = chunk_create(str.ptr, KENCR_LEN);
@@ -166,7 +171,11 @@ METHOD(simaka_crypto_t, derive_keys_reauth, bool,
 	str = chunk_alloca(this->prf->get_block_size(this->prf) * 2);
 	for (i = 0; i < 2; i++)
 	{
-		this->prf->get_bytes(this->prf, chunk_empty, str.ptr + str.len / 2 * i);
+		if (!this->prf->get_bytes(this->prf, chunk_empty,
+								  str.ptr + str.len / 2 * i))
+		{
+			return FALSE;
+		}
 	}
 	k_encr = chunk_create(str.ptr, KENCR_LEN);
 	k_auth = chunk_create(str.ptr + KENCR_LEN, KAUTH_LEN);
@@ -202,7 +211,11 @@ METHOD(simaka_crypto_t, derive_keys_reauth_msk, bool,
 	str = chunk_alloca(this->prf->get_block_size(this->prf) * 2);
 	for (i = 0; i < 2; i++)
 	{
-		this->prf->get_bytes(this->prf, chunk_empty, str.ptr + str.len / 2 * i);
+		if (!this->prf->get_bytes(this->prf, chunk_empty,
+								  str.ptr + str.len / 2 * i))
+		{
+			return FALSE;
+		}
 	}
 	*msk = chunk_create(str.ptr, MSK_LEN);
 	DBG3(DBG_LIB, "MSK %B", msk);

src/libstrongswan/crypto/crypto_tester.c
@@ -797,8 +797,10 @@ static u_int bench_prf(private_crypto_tester_t *this,
 		start_timing(&start);
 		while (end_timing(&start) < this->bench_time)
 		{
-			prf->get_bytes(prf, buf, bytes);
-			runs++;
+			if (prf->get_bytes(prf, buf, bytes))
+			{
+				runs++;
+			}
 		}
 		free(buf.ptr);
 		prf->destroy(prf);
@@ -860,7 +862,10 @@ METHOD(crypto_tester_t, test_prf, bool,
 		{
 			prf->set_key(prf, key);
 		}
-		prf->get_bytes(prf, seed, out.ptr);
+		if (!prf->get_bytes(prf, seed, out.ptr))
+		{
+			failed = TRUE;
+		}
 		if (!memeq(vector->out, out.ptr, out.len))
 		{
 			failed = TRUE;
@@ -874,8 +879,11 @@ METHOD(crypto_tester_t, test_prf, bool,
 				prf->set_key(prf, key);
 			}
 			prf->allocate_bytes(prf, chunk_create(seed.ptr, 1), NULL);
-			prf->get_bytes(prf, chunk_create(seed.ptr + 1, 1), NULL);
-			prf->get_bytes(prf, chunk_skip(seed, 2), out.ptr);
+			if (!prf->get_bytes(prf, chunk_create(seed.ptr + 1, 1), NULL) ||
+				!prf->get_bytes(prf, chunk_skip(seed, 2), out.ptr))
+			{
+				failed = TRUE;
+			}
 			if (!memeq(vector->out, out.ptr, out.len))
 			{
 				failed = TRUE;

src/libstrongswan/crypto/prf_plus.c
@@ -66,17 +66,27 @@ METHOD(prf_plus_t, get_bytes, bool,
 	{
 		if (this->buffer.len == this->used)
 		{	/* buffer used, get next round */
-			this->prf->get_bytes(this->prf, this->buffer, NULL);
+			if (!this->prf->get_bytes(this->prf, this->buffer, NULL))
+			{
+				return FALSE;
+			}
 			if (this->counter)
 			{
-				this->prf->get_bytes(this->prf, this->seed, NULL);
-				this->prf->get_bytes(this->prf, chunk_from_thing(this->counter),
-									 this->buffer.ptr);
+				if (!this->prf->get_bytes(this->prf, this->seed, NULL) ||
+					!this->prf->get_bytes(this->prf,
+							chunk_from_thing(this->counter), this->buffer.ptr))
+				{
+					return FALSE;
+				}
 				this->counter++;
 			}
 			else
 			{
-				this->prf->get_bytes(this->prf, this->seed, this->buffer.ptr);
+				if (!this->prf->get_bytes(this->prf, this->seed,
+										  this->buffer.ptr))
+				{
+					return FALSE;
+				}
 			}
 			this->used = 0;
 		}
@@ -131,14 +141,22 @@ prf_plus_t *prf_plus_create(prf_t *prf, bool counter, chunk_t seed)
 	if (counter)
 	{
 		this->counter = 0x01;
-		this->prf->get_bytes(this->prf, this->seed, NULL);
-		this->prf->get_bytes(this->prf, chunk_from_thing(this->counter),
-							 this->buffer.ptr);
+		if (!this->prf->get_bytes(this->prf, this->seed, NULL) ||
+			!this->prf->get_bytes(this->prf, chunk_from_thing(this->counter),
+								  this->buffer.ptr))
+		{
+			destroy(this);
+			return NULL;
+		}
 		this->counter++;
 	}
 	else
 	{
-		this->prf->get_bytes(this->prf, this->seed, this->buffer.ptr);
+		if (!this->prf->get_bytes(this->prf, this->seed, this->buffer.ptr))
+		{
+			destroy(this);
+			return NULL;
+		}
 	}
 
 	return &this->public;

src/libstrongswan/crypto/prfs/mac_prf.c
@@ -35,10 +35,11 @@ struct private_prf_t {
 	mac_t *mac;
 };
 
-METHOD(prf_t, get_bytes, void,
+METHOD(prf_t, get_bytes, bool,
 	private_prf_t *this, chunk_t seed, u_int8_t *buffer)
 {
 	this->mac->get_mac(this->mac, seed, buffer);
+	return TRUE;
 }
 
 METHOD(prf_t, allocate_bytes, void,

src/libstrongswan/crypto/prfs/prf.h
@@ -71,13 +71,16 @@ extern enum_name_t *pseudo_random_function_names;
  * Generic interface for pseudo-random-functions.
  */
 struct prf_t {
+
 	/**
 	 * Generates pseudo random bytes and writes them in the buffer.
 	 *
 	 * @param seed		a chunk containing the seed for the next bytes
 	 * @param buffer	pointer where the generated bytes will be written
+	 * @return			TRUE if bytes generated successfully
 	 */
-	void (*get_bytes) (prf_t *this, chunk_t seed, u_int8_t *buffer);
+	__attribute__((warn_unused_result))
+	bool (*get_bytes) (prf_t *this, chunk_t seed, u_int8_t *buffer);
 
 	/**
 	 * Generates pseudo random bytes and allocate space for them.

src/libstrongswan/plugins/af_alg/af_alg_prf.c
@@ -105,10 +105,11 @@ static size_t lookup_alg(pseudo_random_function_t algo, char **name, bool *xcbc)
 	return 0;
 }
 
-METHOD(prf_t, get_bytes, void,
+METHOD(prf_t, get_bytes, bool,
 	private_af_alg_prf_t *this, chunk_t seed, u_int8_t *buffer)
 {
 	this->ops->hash(this->ops, seed, buffer, this->block_size);
+	return TRUE;
 }
 
 METHOD(prf_t, allocate_bytes, void,

commit 2df12b4c57a97c5c1203401f41f5e11cb95a0153

Fix tls_prf bug introduced with bc474883

src/libtls/tls_prf.c
@@ -55,11 +55,14 @@ static bool p_hash(prf_t *prf, char *label, chunk_t seed, size_t block_size,
 
 	while (TRUE)
 	{
+		/* A(i) = HMAC_hash(secret, A(i-1)) */
+		if (!prf->get_bytes(prf, a, abuf))
+		{
+			return FALSE;
+		}
 		a = chunk_from_thing(abuf);
-		/* A(i) = HMAC_hash(secret, A(i-1))
-		 * HMAC_hash(secret, A(i) + seed) */
-		if (!prf->get_bytes(prf, a, abuf) ||
-			!prf->get_bytes(prf, a, NULL) ||
+		/* HMAC_hash(secret, A(i) + seed) */
+		if (!prf->get_bytes(prf, a, NULL) ||
 			!prf->get_bytes(prf, seed, buf))
 		{
 			return FALSE;