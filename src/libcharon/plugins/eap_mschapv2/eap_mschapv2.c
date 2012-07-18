strongswan/strongswan

commit 87dd205b61ae8c0125b459959fcc7349fa27bb74

Add a return value to hasher_t.allocate_hash()

src/libcharon/plugins/eap_mschapv2/eap_mschapv2.c
@@ -281,7 +281,11 @@ static status_t NtPasswordHash(chunk_t password, chunk_t *password_hash)
 		DBG1(DBG_IKE, "EAP-MS-CHAPv2 failed, no MD4 hasher available");
 		return FAILED;
 	}
-	hasher->allocate_hash(hasher, password, password_hash);
+	if (!hasher->allocate_hash(hasher, password, password_hash))
+	{
+		hasher->destroy(hasher);
+		return FAILED;
+	}
 	hasher->destroy(hasher);
 	return SUCCESS;
 }
@@ -302,7 +306,11 @@ static status_t ChallengeHash(chunk_t peer_challenge, chunk_t server_challenge,
 		return FAILED;
 	}
 	concat = chunk_cata("ccc", peer_challenge, server_challenge, username);
-	hasher->allocate_hash(hasher, concat, challenge_hash);
+	if (!hasher->allocate_hash(hasher, concat, challenge_hash))
+	{
+		hasher->destroy(hasher);
+		return FAILED;
+	}
 	hasher->destroy(hasher);
 	/* we need only the first 8 octets */
 	challenge_hash->len = 8;
@@ -382,10 +390,17 @@ static status_t AuthenticatorResponse(chunk_t password_hash_hash,
 	}
 
 	concat = chunk_cata("ccc", password_hash_hash, nt_response, magic1);
-	hasher->allocate_hash(hasher, concat, &digest);
+	if (!hasher->allocate_hash(hasher, concat, &digest))
+	{
+		hasher->destroy(hasher);
+		return FAILED;
+	}
 	concat = chunk_cata("ccc", digest, challenge_hash, magic2);
-	hasher->allocate_hash(hasher, concat, response);
-
+	if (!hasher->allocate_hash(hasher, concat, response))
+	{
+		hasher->destroy(hasher);
+		return FAILED;
+	}
 	hasher->destroy(hasher);
 	chunk_free(&digest);
 	return SUCCESS;
@@ -434,7 +449,9 @@ static status_t GenerateMSK(chunk_t password_hash_hash,
 	chunk_t keypad = chunk_from_chars(
 		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 		0x00, 0x00, 0x00, 0x00, 0x00, 0x00);
-	chunk_t concat, master_key, master_receive_key, master_send_key;
+	char master_key[HASH_SIZE_SHA1];
+	char master_receive_key[HASH_SIZE_SHA1], master_send_key[HASH_SIZE_SHA1];
+	chunk_t concat, master;
 	hasher_t *hasher;
 
 	hasher = lib->crypto->create_hasher(lib->crypto, HASH_SHA1);
@@ -444,24 +461,22 @@ static status_t GenerateMSK(chunk_t password_hash_hash,
 		return FAILED;
 	}
 
+	master = chunk_create(master_key, 16);
 	concat = chunk_cata("ccc", password_hash_hash, nt_response, magic1);
-	hasher->allocate_hash(hasher, concat, &master_key);
-	master_key.len = 16;
+	concat = chunk_cata("cccc", master, shapad1, magic2, shapad2);
+	concat = chunk_cata("cccc", master, shapad1, magic3, shapad2);
+	if (!hasher->get_hash(hasher, concat, master_key) ||
+		!hasher->get_hash(hasher, concat, master_receive_key) ||
+		!hasher->get_hash(hasher, concat, master_send_key))
+	{
+		hasher->destroy(hasher);
+		return FAILED;
+	}
 
-	concat = chunk_cata("cccc", master_key, shapad1, magic2, shapad2);
-	hasher->allocate_hash(hasher, concat, &master_receive_key);
-	master_receive_key.len = 16;
-
-	concat = chunk_cata("cccc", master_key, shapad1, magic3, shapad2);
-	hasher->allocate_hash(hasher, concat, &master_send_key);
-	master_send_key.len = 16;
-
-	*msk = chunk_cat("cccc", master_receive_key, master_send_key, keypad, keypad);
+	*msk = chunk_cat("cccc", chunk_create(master_receive_key, 16),
+					 chunk_create(master_send_key, 16), keypad, keypad);
 
 	hasher->destroy(hasher);
-	chunk_free(&master_key);
-	chunk_free(&master_receive_key);
-	chunk_free(&master_send_key);
 	return SUCCESS;
 }
 

src/libcharon/plugins/eap_md5/eap_md5.c
@@ -100,7 +100,11 @@ static status_t hash_challenge(private_eap_md5_t *this, chunk_t *response,
 		DBG1(DBG_IKE, "EAP-MD5 failed, MD5 not supported");
 		return FAILED;
 	}
-	hasher->allocate_hash(hasher, concat, response);
+	if (!hasher->allocate_hash(hasher, concat, response))
+	{
+		hasher->destroy(hasher);
+		return FAILED;
+	}
 	hasher->destroy(hasher);
 	return SUCCESS;
 }

src/libcharon/plugins/stroke/stroke_ca.c
@@ -354,10 +354,12 @@ METHOD(stroke_ca_t, check_for_hash_and_url, void,
 
 			if (cert->get_encoding(cert, CERT_ASN1_DER, &encoded))
 			{
-				hasher->allocate_hash(hasher, encoded, &hash);
-				section->hashes->insert_last(section->hashes,
+				if (hasher->allocate_hash(hasher, encoded, &hash))
+				{
+					section->hashes->insert_last(section->hashes,
 						identification_create_from_encoding(ID_KEY_ID, hash));
-				chunk_free(&hash);
+					chunk_free(&hash);
+				}
 				chunk_free(&encoded);
 			}
 			break;

src/libcharon/sa/ike_sa_manager.c
@@ -1164,8 +1164,13 @@ METHOD(ike_sa_manager_t, checkout_by_message, ike_sa_t*,
 		u_int64_t our_spi;
 		chunk_t hash;
 
-		this->hasher->allocate_hash(this->hasher,
-									message->get_packet_data(message), &hash);
+		if (!this->hasher->allocate_hash(this->hasher,
+									message->get_packet_data(message), &hash))
+		{
+			DBG1(DBG_MGR, "ignoring message, failed to hash message");
+			id->destroy(id);
+			return NULL;
+		}
 
 		/* ensure this is not a retransmit of an already handled init message */
 		switch (check_and_put_init_hash(this, hash, &our_spi))

src/libcharon/sa/ikev1/keymat_v1.c
@@ -554,7 +554,11 @@ METHOD(keymat_v1_t, derive_ike_keys, bool,
 
 	/* initial IV = hash(g^xi | g^xr) */
 	data = chunk_cata("cc", g_xi, g_xr);
-	this->hasher->allocate_hash(this->hasher, data, &this->phase1_iv.iv);
+	if (!this->hasher->allocate_hash(this->hasher, data, &this->phase1_iv.iv))
+	{
+		chunk_free(&dh_me);
+		return FALSE;
+	}
 	if (this->phase1_iv.iv.len > this->aead->get_block_size(this->aead))
 	{
 		this->phase1_iv.iv.len = this->aead->get_block_size(this->aead);
@@ -975,10 +979,15 @@ static bool generate_iv(private_keymat_v1_t *this, iv_data_t *iv)
 	else
 	{
 		/* initial phase 2 IV = hash(last_phase1_block | mid) */
-		u_int32_t net = htonl(iv->mid);
-		chunk_t data = chunk_cata("cc", this->phase1_iv.iv,
-								  chunk_from_thing(net));
-		this->hasher->allocate_hash(this->hasher, data, &iv->iv);
+		u_int32_t net;;
+		chunk_t data;
+
+		net = htonl(iv->mid);
+		data = chunk_cata("cc", this->phase1_iv.iv, chunk_from_thing(net));
+		if (!this->hasher->allocate_hash(this->hasher, data, &iv->iv))
+		{
+			return FALSE;
+		}
 		if (iv->iv.len > this->aead->get_block_size(this->aead))
 		{
 			iv->iv.len = this->aead->get_block_size(this->aead);

src/libcharon/sa/ikev1/tasks/isakmp_natd.c
@@ -100,7 +100,11 @@ static chunk_t generate_natd_hash(private_isakmp_natd_t *this,
 	natd_chunk = chunk_cata("cccc", chunk_from_thing(spi_i),
 						    chunk_from_thing(spi_r), host->get_address(host),
 						    chunk_from_thing(port));
-	hasher->allocate_hash(hasher, natd_chunk, &natd_hash);
+	if (!hasher->allocate_hash(hasher, natd_chunk, &natd_hash))
+	{
+		DBG1(DBG_IKE, "creating NAT-D payload hash failed");
+		return chunk_empty;
+	}
 	DBG3(DBG_IKE, "natd_chunk %B", &natd_chunk);
 	DBG3(DBG_IKE, "natd_hash %B", &natd_hash);
 
@@ -154,6 +158,10 @@ static hash_payload_t *build_natd_payload(private_isakmp_natd_t *this, bool src,
 		ike_sa_id_t *ike_sa_id = this->ike_sa->get_id(this->ike_sa);
 		hash = generate_natd_hash(this, ike_sa_id, host);
 	}
+	if (!hash.len)
+	{
+		return NULL;
+	}
 	payload = hash_payload_create(NAT_D_V1);
 	payload->set_hash(payload, hash);
 	chunk_free(&hash);
@@ -171,14 +179,20 @@ static void add_natd_payloads(private_isakmp_natd_t *this, message_t *message)
 	/* destination has to be added first */
 	host = message->get_destination(message);
 	payload = build_natd_payload(this, FALSE, host);
-	message->add_payload(message, (payload_t*)payload);
+	if (payload)
+	{
+		message->add_payload(message, (payload_t*)payload);
+	}
 
 	/* source is added second, compared with IKEv2 we always know the source,
 	 * as these payloads are added in the second Phase 1 exchange or the
 	 * response to the first */
 	host = message->get_source(message);
 	payload = build_natd_payload(this, TRUE, host);
-	message->add_payload(message, (payload_t*)payload);
+	if (payload)
+	{
+		message->add_payload(message, (payload_t*)payload);
+	}
 }
 
 /**

src/libcharon/sa/ikev2/connect_manager.c
@@ -839,7 +839,10 @@ static chunk_t build_signature(private_connect_manager_t *this,
 	/* signature = SHA1( MID | ME_CONNECTID | ME_ENDPOINT | ME_CONNECTKEY ) */
 	sig_chunk = chunk_cat("cccc", mid_chunk, check->connect_id,
 						  check->endpoint_raw, key_chunk);
-	this->hasher->allocate_hash(this->hasher, sig_chunk, &sig_hash);
+	if (!this->hasher->allocate_hash(this->hasher, sig_chunk, &sig_hash))
+	{
+		sig_hash = chunk_empty;
+	}
 	DBG3(DBG_IKE, "sig_chunk %#B", &sig_chunk);
 	DBG3(DBG_IKE, "sig_hash %#B", &sig_hash);
 

src/libcharon/sa/ikev2/tasks/ike_cert_post.c
@@ -78,7 +78,12 @@ static cert_payload_t *build_cert_payload(private_ike_cert_post_t *this,
 		hasher->destroy(hasher);
 		return NULL;
 	}
-	hasher->allocate_hash(hasher, encoded, &hash);
+	if (!hasher->allocate_hash(hasher, encoded, &hash))
+	{
+		hasher->destroy(hasher);
+		chunk_free(&encoded);
+		return cert_payload_create_from_cert(CERTIFICATE, cert);
+	}
 	chunk_free(&encoded);
 	hasher->destroy(hasher);
 	id = identification_create_from_encoding(ID_KEY_ID, hash);

commit 09e3717525420d62d5636962c29808d665aa8d13

Fix EAP-MSCHAPv2 master key derivation, broken with 87dd205b

src/libcharon/plugins/eap_mschapv2/eap_mschapv2.c
@@ -461,13 +461,21 @@ static status_t GenerateMSK(chunk_t password_hash_hash,
 		return FAILED;
 	}
 
-	master = chunk_create(master_key, 16);
 	concat = chunk_cata("ccc", password_hash_hash, nt_response, magic1);
+	if (!hasher->get_hash(hasher, concat, master_key))
+	{
+		hasher->destroy(hasher);
+		return FAILED;
+	}
+	master = chunk_create(master_key, 16);
 	concat = chunk_cata("cccc", master, shapad1, magic2, shapad2);
+	if (!hasher->get_hash(hasher, concat, master_receive_key))
+	{
+		hasher->destroy(hasher);
+		return FAILED;
+	}
 	concat = chunk_cata("cccc", master, shapad1, magic3, shapad2);
-	if (!hasher->get_hash(hasher, concat, master_key) ||
-		!hasher->get_hash(hasher, concat, master_receive_key) ||
-		!hasher->get_hash(hasher, concat, master_send_key))
+	if (!hasher->get_hash(hasher, concat, master_send_key))
 	{
 		hasher->destroy(hasher);
 		return FAILED;