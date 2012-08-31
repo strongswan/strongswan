0a8dc725dc64437891424d8d5ce48be1cb703f2e
Martin Willi <martin@revosec.ch>
2012-08-31 14:59:06 +0200

    Add a fallback callback to enum_name printf hook
    If mapping the enum value failed, the callback can directly write
    to the output stream to write the fallback value.

--- src/libstrongswan/enum.c
+++ src/libstrongswan/enum.c
@@ -78,6 +78,10 @@ int enum_printf_hook(printf_hook_data_t *data, printf_hook_spec_t *spec,
 	name = enum_to_name(e, val);
 	if (name == NULL)
 	{
+		if (e->cb)
+		{
+			return e->cb(data, NULL, e, val);
+		}
 		return print_in_hook(data, "(%d)", val);
 	}
 	else
@@ -109,6 +113,10 @@ int enum_dynamic_printf_hook(printf_hook_data_t *data, printf_hook_spec_t *spec,
 	}
 	if (name == NULL)
 	{
+		if (e && e->cb)
+		{
+			return e->cb(data, user, e, val);
+		}
 		return print_in_hook(data, "(%d)", val);
 	}
 	else
