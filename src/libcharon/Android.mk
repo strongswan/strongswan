6d30954ae809fb10427f9d860f76d0a298cb4919
Tobias Brunner <tobias@strongswan.org>
2012-09-14 14:10:14 +0200

    Removed the unneeded socket-raw plugin

--- src/libcharon/Android.mk
+++ src/libcharon/Android.mk
@@ -200,8 +200,6 @@ LOCAL_SRC_FILES += $(call add_plugin, socket-default)
 
 LOCAL_SRC_FILES += $(call add_plugin, socket-dynamic)
 
-LOCAL_SRC_FILES += $(call add_plugin, socket-raw)
-
 LOCAL_SRC_FILES += $(call add_plugin, stroke)
 ifneq ($(call plugin_enabled, stroke),)
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../stroke/
