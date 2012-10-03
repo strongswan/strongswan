e0d7c1eda7194ab13a69ce70dd9e1e953aa7d9a0
Martin Willi <martin@revosec.ch>
2012-10-03 16:25:36 +0200

    Add a lookip plugin stub to lookup connections by virtual IP

---
+++ src/libcharon/plugins/lookip/lookip_plugin.c
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2012 Martin Willi
+ * Copyright (C) 2012 revosec AG
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+
+#include "lookip_plugin.h"
+
+#include <daemon.h>
+
+typedef struct private_lookip_plugin_t private_lookip_plugin_t;
+
+/**
+ * private data of lookip plugin
+ */
+struct private_lookip_plugin_t {
+
+	/**
+	 * implements plugin interface
+	 */
+	lookip_plugin_t public;
+};
+
+METHOD(plugin_t, get_name, char*,
+	private_lookip_plugin_t *this)
+{
+	return "lookip";
+}
+
+METHOD(plugin_t, destroy, void,
+	private_lookip_plugin_t *this)
+{
+	free(this);
+}
+
+/**
+ * Plugin constructor
+ */
+plugin_t *lookip_plugin_create()
+{
+	private_lookip_plugin_t *this;
+
+	INIT(this,
+		.public = {
+			.plugin = {
+				.get_name = _get_name,
+				.reload = (void*)return_false,
+				.destroy = _destroy,
+			},
+		},
+	);
+
+	return &this->public.plugin;
+}
