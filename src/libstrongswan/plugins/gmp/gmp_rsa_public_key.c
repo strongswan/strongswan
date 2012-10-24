strongswan/strongswan

commit 5025135f70e3c6f24dec4143c7de8640627d9026

Check rng return value when generating DH secrets and primes in gmp plugin

src/libstrongswan/plugins/gmp/gmp_rsa_public_key.c
@@ -314,7 +314,7 @@ METHOD(public_key_t, encrypt_, bool,
 {
 	chunk_t em;
 	u_char *pos;
-	int padding, i;
+	int padding;
 	rng_t *rng;
 
 	if (scheme != ENCRYPT_RSA_PKCS1)
@@ -348,16 +348,12 @@ METHOD(public_key_t, encrypt_, bool,
 	*pos++ = 0x02;
 
 	/* fill with pseudo random octets */
-	rng->get_bytes(rng, padding, pos);
-
-	/* replace zero-valued random octets */
-	for (i = 0; i < padding; i++)
+	if (!rng_get_bytes_not_zero(rng, padding, pos, TRUE))
 	{
-		while (*pos == 0)
-		{
-			rng->get_bytes(rng, 1, pos);
-		}
-		pos++;
+		DBG1(DBG_LIB, "failed to allocate padding");
+		chunk_clear(&em);
+		rng->destroy(rng);
+		return FALSE;
 	}
 	rng->destroy(rng);
 

src/libstrongswan/plugins/gmp/gmp_diffie_hellman.c
@@ -230,8 +230,13 @@ static gmp_diffie_hellman_t *create_generic(diffie_hellman_group_t group,
 		destroy(this);
 		return NULL;
 	}
-
-	rng->allocate_bytes(rng, exp_len, &random);
+	if (!rng->allocate_bytes(rng, exp_len, &random))
+	{
+		DBG1(DBG_LIB, "failed to allocate DH secret");
+		rng->destroy(rng);
+		destroy(this);
+		return NULL;
+	}
 	rng->destroy(rng);
 
 	if (exp_len == this->p_len)

src/libstrongswan/plugins/gmp/gmp_rsa_private_key.c
@@ -149,7 +149,12 @@ static status_t compute_prime(private_gmp_rsa_private_key_t *this,
 	mpz_init(*prime);
 	do
 	{
-		rng->allocate_bytes(rng, prime_size, &random_bytes);
+		if (!rng->allocate_bytes(rng, prime_size, &random_bytes))
+		{
+			DBG1(DBG_LIB, "failed to allocate random prime");
+			rng->destroy(rng);
+			return FAILED;
+		}
 		/* make sure the two most significant bits are set */
 		random_bytes.ptr[0] = random_bytes.ptr[0] | 0xC0;
 

commit 828cefc3139a43ebc96d7cc335aa3366c6baf1f4

Fix RSA encryption padding terminator in gmp plugin, broken with 5025135f

src/libstrongswan/plugins/gmp/gmp_rsa_public_key.c
@@ -361,6 +361,8 @@ METHOD(public_key_t, encrypt_, bool,
 	}
 	rng->destroy(rng);
 
+	pos += padding;
+
 	/* append the padding terminator */
 	*pos++ = 0x00;
 