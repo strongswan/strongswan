01bc9bf428a5b0f5d0677fa3853bde52456b9431
Martin Willi <martin@revosec.ch>
2012-11-30 17:15:37 +0100

    Fix GPL license header to properly "sed" it

--- src/libcharon/sa/ikev2/tasks/ike_auth.c
+++ src/libcharon/sa/ikev2/tasks/ike_auth.c
@@ -12,7 +12,7 @@
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * for more details
+ * for more details.
  */
 
 #include "ike_auth.h"
