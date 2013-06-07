e3bdf03af4bc4a6ef59e7b891c58ba65dd1f0da0
Tobias Brunner <tobias@strongswan.org>
2013-06-07 18:57:15 +0200

    blowfish: Use plugin features to properly register crypter

--- src/libstrongswan/plugins/blowfish/blowfish_plugin.c
+++ src/libstrongswan/plugins/blowfish/blowfish_plugin.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2008 Martin Willi
  * Copyright (C) 2009 Andreas Steffen
+ * Copyright (C) 2008 Martin Willi
  * Hochschule fuer Technik Rapperswil
  *
  * This program is free software; you can redistribute it and/or modify it
@@ -38,11 +38,20 @@ METHOD(plugin_t, get_name, char*,
 	return "blowfish";
 }
 
+METHOD(plugin_t, get_features, int,
+	private_blowfish_plugin_t *this, plugin_feature_t *features[])
+{
+	static plugin_feature_t f[] = {
+		PLUGIN_REGISTER(CRYPTER, blowfish_crypter_create),
+			PLUGIN_PROVIDE(CRYPTER, ENCR_BLOWFISH, 0),
+	};
+	*features = f;
+	return countof(f);
+}
+
 METHOD(plugin_t, destroy, void,
 	private_blowfish_plugin_t *this)
 {
-	lib->crypto->remove_crypter(lib->crypto,
-								(crypter_constructor_t)blowfish_crypter_create);
 	free(this);
 }
 
@@ -57,15 +66,11 @@ plugin_t *blowfish_plugin_create()
 		.public = {
 			.plugin = {
 				.get_name = _get_name,
-				.reload = (void*)return_false,
+				.get_features = _get_features,
 				.destroy = _destroy,
 			},
 		},
 	);
 
-	lib->crypto->add_crypter(lib->crypto, ENCR_BLOWFISH, get_name(this),
-							 (crypter_constructor_t)blowfish_crypter_create);
-
 	return &this->public.plugin;
 }
-
