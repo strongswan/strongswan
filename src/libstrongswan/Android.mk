daf1880b3947d8c745f8e6290efcf076bf18a5a8
Martin Willi <martin@revosec.ch>
2013-06-26 17:13:11 +0200

    stream: add a stream service class abstracting services using BSD sockets

--- src/libstrongswan/Android.mk
+++ src/libstrongswan/Android.mk
@@ -27,6 +27,7 @@ database/database_factory.c fetcher/fetcher.c fetcher/fetcher_manager.c eap/eap.
 ipsec/ipsec_types.c \
 networking/host.c networking/host_resolver.c networking/packet.c \
 networking/tun_device.c networking/streams/stream.c \
+networking/streams/stream_service.c \
 pen/pen.c plugins/plugin_loader.c plugins/plugin_feature.c processing/jobs/job.c \
 processing/jobs/callback_job.c processing/processor.c processing/scheduler.c \
 processing/watcher.c resolver/resolver_manager.c resolver/rr_set.c \
