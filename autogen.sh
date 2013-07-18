af2db7a6510a571f9131a9143415496addcc6861
Martin Willi <martin@revosec.ch>
2013-07-18 12:01:18 +0200

    autoconf: replace autogen.sh custom script with a call to autoreconf -i

--- autogen.sh
+++ autogen.sh
@@ -1,20 +1,3 @@
 #!/bin/sh
 
-LIBTOOLIZE=`which glibtoolize 2>/dev/null`
-case "$LIBTOOLIZE" in
-	/* )	;;
-	*  )	LIBTOOLIZE=`which libtoolize 2>/dev/null`
-		case "$LIBTOOLIZE" in
-			/* )	;;
-			*  )	LIBTOOLIZE=libtoolize
-				;;
-		esac
-		;;
-esac
-
-$LIBTOOLIZE --force &&
-aclocal &&
-autoheader &&
-automake -a &&
-autoconf
-
+autoreconf -i
