645e9291f03a4d4714a1e27d8fea83643f6cb48a
Andreas Steffen <andreas.steffen@strongswan.org>
2013-07-19 19:36:07 +0200

    updated some TNC scenarios

--- testing/tests/tnc/tnccs-20-os/hosts/moon/etc/pts/data.sql
+++ testing/tests/tnc/tnccs-20-os/hosts/moon/etc/pts/data.sql
@@ -666,6 +666,46 @@ INSERT INTO groups_product_defaults (
   3, 22
 );
 
+/* Devices */
+
+INSERT INTO devices (                  /*  1 */
+  value, product, created  
+) VALUES (
+  'aabbccddeeff11223344556677889900', 4, 1372330615
+);
+
+/* Groups Members */
+
+INSERT INTO groups_members (
+  group_id, device_id
+) VALUES (
+  5, 1
+);
+
+/* Identities */
+
+INSERT INTO identities (
+  type, value
+) VALUES ( /* dave@strongswan.org */
+  4, X'64617665407374726f6e677377616e2e6f7267'
+);
+
+/* Sessions */
+
+INSERT INTO sessions (
+  time, connection, identity, device, product, rec
+) VALUES (
+  NOW, 1, 1, 1, 4, 0
+);
+
+/* Results */
+
+INSERT INTO results (
+  session, policy, rec, result
+) VALUES (
+  1, 1, 0, 'processed 355 packages: 0 not updated, 0 blacklisted, 4 ok, 351 not found'
+);
+
 /* Policies */
 
 INSERT INTO policies (			/*  1 */
