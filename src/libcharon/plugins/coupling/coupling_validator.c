62293ed27116e816fb38451c2d0ecf7f6b39e508
Tobias Brunner <tobias@strongswan.org>
2013-07-22 17:53:56 +0200

    coupling: Fix call to call_hook()

--- src/libcharon/plugins/coupling/coupling_validator.c
+++ src/libcharon/plugins/coupling/coupling_validator.c
@@ -167,7 +167,7 @@ METHOD(cert_validator_t, validate, bool,
 			{
 				DBG1(DBG_CFG, "coupling new certificate '%Y' failed",
 					 subject->get_subject(subject));
-				lib->credmgr->call_hook(lib->credmgr
+				lib->credmgr->call_hook(lib->credmgr,
 										CRED_HOOK_POLICY_VIOLATION, subject);
 			}
 		}
