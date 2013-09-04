f1d5d876190e6b4d71524be45d5624ca20162cb3
Tobias Brunner <tobias@strongswan.org>
2013-09-04 09:08:46 +0200

    pubkey_speed: Add header and fix usage

--- scripts/pubkey_speed.c
+++ scripts/pubkey_speed.c
@@ -1,3 +1,17 @@
+/*
+ * Copyright (C) 2009 Martin Willi
+ * Hochschule fuer Technik Rapperswil
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
 
 #include <stdio.h>
 #include <time.h>
@@ -21,7 +35,7 @@ double end_timing(struct timespec *start)
 
 static void usage()
 {
-	printf("usage: pubkey_speed plugins rsa|ecdsa rounds\n");
+	printf("usage: pubkey_speed plugins rsa|ecdsa rounds < key\n");
 	exit(1);
 }
 
@@ -138,4 +152,3 @@ int main(int argc, char *argv[])
 	free(sigs);
 	return 0;
 }
-
