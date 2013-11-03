79b8a384b50d608933a07da7043e471dd1c07e30
Andreas Steffen <andreas.steffen@strongswan.org>
2013-11-03 21:34:42 +0100

    Updated test_runner.h with new suites

--- src/libstrongswan/tests/test_runner.h
+++ src/libstrongswan/tests/test_runner.h
@@ -35,5 +35,7 @@ Suite *ecdsa_suite_create();
 Suite *rsa_suite_create();
 Suite *host_suite_create();
 Suite *printf_suite_create();
+Suite *pen_suite_create();
+Suite *asn1_suite_create();
 
 #endif /** TEST_RUNNER_H_ */
