11e7d0677c3ec6f241ba387d5aadba113b56e9be
Martin Willi <martin@revosec.ch>
2013-12-19 14:14:37 +0100

    socket-win: Install IKE bypass policies using bypass_socket()

--- src/libcharon/plugins/socket_win/socket_win_plugin.c
+++ src/libcharon/plugins/socket_win/socket_win_plugin.c
@@ -49,6 +49,7 @@ METHOD(plugin_t, get_features, int,
 	static plugin_feature_t f[] = {
 		PLUGIN_CALLBACK(socket_register, socket_win_socket_create),
 			PLUGIN_PROVIDE(CUSTOM, "socket"),
+				PLUGIN_DEPENDS(CUSTOM, "kernel-ipsec"),
 	};
 	*features = f;
 	return countof(f);
