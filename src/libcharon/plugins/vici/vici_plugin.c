6770cfe34aea20ce970dbc35fad4bd4685a2b58d
Martin Willi <martin@revosec.ch>
2014-01-20 10:59:21 +0100

    vici: Add a plugin stub for the "Versatile IKE Control Interface" plugin

---
+++ src/libcharon/plugins/vici/vici_plugin.c
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2014 Martin Willi
+ * Copyright (C) 2014 revosec AG
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+
+#include "vici_plugin.h"
+
+#include <library.h>
+
+typedef struct private_vici_plugin_t private_vici_plugin_t;
+
+/**
+ * Private members of vici_plugin_t
+ */
+struct private_vici_plugin_t {
+
+	/**
+	 * public functions
+	 */
+	vici_plugin_t public;
+};
+
+METHOD(plugin_t, get_name, char*,
+	private_vici_plugin_t *this)
+{
+	return "vici";
+}
+
+/**
+ * Register vici plugin features
+ */
+static bool register_vici(private_vici_plugin_t *this,
+						  plugin_feature_t *feature, bool reg, void *data)
+{
+	return TRUE;
+}
+
+METHOD(plugin_t, get_features, int,
+	private_vici_plugin_t *this, plugin_feature_t *features[])
+{
+	static plugin_feature_t f[] = {
+		PLUGIN_CALLBACK((plugin_feature_callback_t)register_vici, NULL),
+			PLUGIN_PROVIDE(CUSTOM, "vici"),
+	};
+	*features = f;
+	return countof(f);
+}
+
+METHOD(plugin_t, destroy, void,
+	private_vici_plugin_t *this)
+{
+	free(this);
+}
+
+/*
+ * see header file
+ */
+plugin_t *vici_plugin_create()
+{
+	private_vici_plugin_t *this;
+
+	INIT(this,
+		.public = {
+			.plugin = {
+				.get_name = _get_name,
+				.reload = (void*)return_false,
+				.get_features = _get_features,
+				.destroy = _destroy,
+			},
+		},
+	);
+
+	return &this->public.plugin;
+}
