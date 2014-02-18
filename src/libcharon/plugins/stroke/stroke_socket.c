5a04056295227f79fb21726f406be6bcc171c57d
Tobias Brunner <tobias@strongswan.org>
2014-02-18 13:25:26 +0100

    stroke: Use proper modifiers to print size_t arguments

--- src/libcharon/plugins/stroke/stroke_socket.c
+++ src/libcharon/plugins/stroke/stroke_socket.c
@@ -500,7 +500,7 @@ static void stroke_leases(private_stroke_socket_t *this,
 static void report_usage(FILE *out, int count, size_t bytes,
 						 backtrace_t *bt, bool detailed)
 {
-	fprintf(out, "%d bytes total, %d allocations, %d bytes average:\n",
+	fprintf(out, "%zu bytes total, %d allocations, %zu bytes average:\n",
 			bytes, count, bytes / count);
 	bt->log(bt, out, detailed);
 }
