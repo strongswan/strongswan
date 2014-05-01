60634df383ee5ba494283eded459acbb36c3c981
Andreas Steffen <andreas.steffen@strongswan.org>
2014-05-01 22:16:34 +0200

    Extended Ubuntu 14.04 database build

--- src/libpts/plugins/imv_attestation/build-database.sh
+++ src/libpts/plugins/imv_attestation/build-database.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 p="Ubuntu 14.04 x86_64"
-m="3.13.0-24-generic"
+a="x86_64-linux-gnu"
+k="3.13.0-24-generic"
 
 for hash in sha1 sha256
 do
@@ -15,6 +16,7 @@ do
   ipsec attest --add --product "$p" --$hash --dir  /etc/rcS.d
   ipsec attest --add --product "$p" --$hash --dir  /etc/rc2.d
   ipsec attest --add --product "$p" --$hash --dir  /etc/resolvconf/update.d
+  ipsec attest --add --product "$p" --$hash --file /etc/resolvconf/update-libc.d/avahi-daemon
   ipsec attest --add --product "$p" --$hash --dir  /etc/update-motd.d
 
   ipsec attest --add --product "$p" --$hash --dir  /lib
@@ -24,12 +26,15 @@ do
   ipsec attest --add --product "$p" --$hash --dir  /lib/udev
   ipsec attest --add --product "$p" --$hash --dir  /lib/systemd
   ipsec attest --add --product "$p" --$hash --dir  /lib/xtables
-  ipsec attest --add --product "$p" --$hash --dir  /lib/x86_64-linux-gnu
-  ipsec attest --add --product "$p" --$hash --dir  /lib/x86_64-linux-gnu/security
+  ipsec attest --add --product "$p" --$hash --dir  /lib/$a
+  ipsec attest --add --product "$p" --$hash --dir  /lib/$a/plymouth
+  ipsec attest --add --product "$p" --$hash --dir  /lib/$a/plymouth/renderers
+  ipsec attest --add --product "$p" --$hash --dir  /lib/$a/security
 
   ipsec attest --add --product "$p" --$hash --file /lib64/ld-linux-x86-64.so.2
 
   ipsec attest --add --product "$p" --$hash --dir  /usr/lib
+  ipsec attest --add --product "$p" --$hash --file /usr/lib/avahi/avahi-daemon-check-dns.sh
   ipsec attest --add --product "$p" --$hash --dir  /usr/lib/compiz
   ipsec attest --add --product "$p" --$hash --dir  /usr/lib/gvfs
   ipsec attest --add --product "$p" --$hash --dir  /usr/lib/libvirt/connection-driver
@@ -41,21 +46,25 @@ do
   ipsec attest --add --product "$p" --$hash --dir  /usr/lib/ubuntu-release-upgrader
   ipsec attest --add --product "$p" --$hash --dir  /usr/lib/unity-settings-daemon-1.0
   ipsec attest --add --product "$p" --$hash --dir  /usr/lib/update-notifier
+  ipsec attest --add --product "$p" --$hash --dir  /usr/lib/xorg/modules
   ipsec attest --add --product "$p" --$hash --dir  /usr/lib/xorg/modules/drivers
   ipsec attest --add --product "$p" --$hash --dir  /usr/lib/xorg/modules/extensions
   ipsec attest --add --product "$p" --$hash --dir  /usr/lib/xorg/modules/input
 
-  ipsec attest --add --product "$p" --$hash --dir  /usr/lib/x86_64-linux-gnu
-  ipsec attest --add --product "$p" --$hash --dir  /usr/lib/x86_64-linux-gnu/colord-plugins
-  ipsec attest --add --product "$p" --$hash --dir  /usr/lib/x86_64-linux-gnu/gconv/
-  ipsec attest --add --product "$p" --$hash --file /usr/lib/x86_64-linux-gnu/gedit/libgedit-private.so
-  ipsec attest --add --product "$p" --$hash --dir  /usr/lib/x86_64-linux-gnu/gedit/plugins
-  ipsec attest --add --product "$p" --$hash --dir  /usr/lib/x86_64-linux-gnu/gio/modules
-  ipsec attest --add --product "$p" --$hash --dir  /usr/lib/x86_64-linux-gnu/ModemManager
-  ipsec attest --add --product "$p" --$hash --dir  /usr/lib/x86_64-linux-gnu/NetworkManager
-  ipsec attest --add --product "$p" --$hash --dir  /usr/lib/x86_64-linux-gnu/nss
-  ipsec attest --add --product "$p" --$hash --dir  /usr/lib/x86_64-linux-gnu/samba
-  ipsec attest --add --product "$p" --$hash --dir  /usr/lib/x86_64-linux-gnu/sasl2
+  ipsec attest --add --product "$p" --$hash --dir  /usr/lib/$a
+  ipsec attest --add --product "$p" --$hash --dir  /usr/lib/$a/alsa-lib
+  ipsec attest --add --product "$p" --$hash --dir  /usr/lib/$a/colord-plugins
+  ipsec attest --add --product "$p" --$hash --dir  /usr/lib/$a/gconv/
+  ipsec attest --add --product "$p" --$hash --file /usr/lib/$a/libgedit-private.so
+  ipsec attest --add --product "$p" --$hash --dir  /usr/lib/$a/gedit/plugins
+  ipsec attest --add --product "$p" --$hash --dir  /usr/lib/$a/gio/modules
+  ipsec attest --add --product "$p" --$hash --dir  /usr/lib/$a/ModemManager
+  ipsec attest --add --product "$p" --$hash --dir  /usr/lib/$a/NetworkManager
+  ipsec attest --add --product "$p" --$hash --dir  /usr/lib/$a/nss
+  ipsec attest --add --product "$p" --$hash --dir  /usr/lib/$a/pkcs11
+  ipsec attest --add --product "$p" --$hash --dir  /usr/lib/$a/pulseaudio
+  ipsec attest --add --product "$p" --$hash --dir  /usr/lib/$a/samba
+  ipsec attest --add --product "$p" --$hash --dir  /usr/lib/$a/sasl2
 
   for file in `find /usr/lib/evolution-data-server -name *.so`
   do
@@ -72,7 +81,7 @@ do
     ipsec attest --add --product "$p" --$hash --file $file
   done
 
-  for file in `find /lib/modules/$m -name *.ko`
+  for file in `find /lib/modules/$k -name *.ko`
   do
     ipsec attest --add --product "$p" --$hash --file $file
   done
