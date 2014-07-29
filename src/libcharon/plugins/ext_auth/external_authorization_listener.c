6aea96493a0fe38cf7cc6c36f7f8db115a0e194b
Vyronas Tsingaras <vtsingaras@it.auth.gr>
2014-07-29 10:21:04 -0700

    Added license notice and fixed EAP handling

--- src/libcharon/plugins/ext_auth/external_authorization_listener.c
+++ src/libcharon/plugins/ext_auth/external_authorization_listener.c
@@ -1,3 +1,26 @@
+/*
+Copyright (c) 2014 Vyronas Tsingaras (vtsingaras@it.auth.gr)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+
 #include "external_authorization_listener.h"
 
 #include <stdlib.h>
@@ -31,34 +54,48 @@ METHOD(listener_t, authorize, bool,
 
 	if (final)
 	{
+		identification_t *eap_peer_id;
 		identification_t *peer_id;
 		identification_t *my_id;
 		my_id = ike_sa->get_my_id(ike_sa);
+		eap_peer_id = ike_sa->get_other_id(ike_sa);
 		peer_id = ike_sa->get_other_id(ike_sa);
-		if (peer_id && my_id && peer_id->equals(peer_id, my_id))
+		if ( (peer_id && my_id && peer_id->equals(peer_id, my_id)) || (eap_peer_id && my_id && eap_peer_id->equals(eap_peer_id, my_id)) )
 		{
 			DBG2(DBG_CFG, "called for my_id: '%Y'", my_id);
 			*success = TRUE;
 			return TRUE;
 		}
-		if (peer_id)
+		if (eap_peer_id)
 		{
+			char id_buf[512];
+			char cmd_buf[1024];
+			DBG2(DBG_CFG, "peer identity received: '%Y'", eap_peer_id);
+			snprintf(id_buf, sizeof(id_buf), "%Y", eap_peer_id);
+			DBG2(DBG_CFG, "calling program: %s", this->path);
+			snprintf(cmd_buf, sizeof(cmd_buf), "%s %s", this->path, (char*)id_buf);
+			authorized = WEXITSTATUS(system(cmd_buf));
+			DBG2(DBG_CFG, "script returned: %d", authorized);
+		}
+		else if (peer_id)
+		{
+			char id_buf[512];
+			char cmd_buf[1024];
 			DBG2(DBG_CFG, "peer identity received: '%Y'", peer_id);
-			char buf[255];
-			snprintf(buf, 255, "%Y", peer_id);
-			setenv("IPSEC_IDENTITY", (char*)buf, 1);
-			DBG2(DBG_CFG, "calling script: %s", this->path);
-			authorized = WEXITSTATUS(system(this->path));
+			snprintf(id_buf, sizeof(id_buf), "%Y", peer_id);
+			DBG2(DBG_CFG, "calling program: %s", this->path);
+			snprintf(cmd_buf, sizeof(cmd_buf), "%s %s", this->path, (char*)id_buf);
+			authorized = WEXITSTATUS(system(cmd_buf));
 			DBG2(DBG_CFG, "script returned: %d", authorized);
 		}
 		if (authorized == 0)
 		{
-			DBG2(DBG_CFG, "peer identity '%Y' authorized", peer_id);
+			DBG2(DBG_CFG, "peer identity '%Y' authorized", (eap_peer_id != NULL) ? eap_peer_id : peer_id);
 			*success = TRUE;
 		}
 		else
 		{
-			DBG1(DBG_CFG, "peer identity '%Y' not authorized externally", peer_id);
+			DBG1(DBG_CFG, "peer identity '%Y' not authorized", (eap_peer_id != NULL) ? eap_peer_id : peer_id);
 			*success = FALSE;
 		}
 	}
