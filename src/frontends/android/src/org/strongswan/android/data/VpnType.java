a1700c9903232057daa75a3477b252ce7c461f98
Tobias Brunner <tobias@strongswan.org>
2014-09-10 11:28:48 +0200

    android: Add EAP-TLS VPN type to the GUI

--- src/frontends/android/src/org/strongswan/android/data/VpnType.java
+++ src/frontends/android/src/org/strongswan/android/data/VpnType.java
@@ -23,6 +23,7 @@ public enum VpnType
 	IKEV2_EAP("ikev2-eap", EnumSet.of(VpnTypeFeature.USER_PASS)),
 	IKEV2_CERT("ikev2-cert", EnumSet.of(VpnTypeFeature.CERTIFICATE)),
 	IKEV2_CERT_EAP("ikev2-cert-eap", EnumSet.of(VpnTypeFeature.USER_PASS, VpnTypeFeature.CERTIFICATE)),
+	IKEV2_EAP_TLS("ikev2-eap-tls", EnumSet.of(VpnTypeFeature.CERTIFICATE)),
 	IKEV2_BYOD_EAP("ikev2-byod-eap", EnumSet.of(VpnTypeFeature.USER_PASS, VpnTypeFeature.CERTIFICATE, VpnTypeFeature.BYOD));
 
 	/**
