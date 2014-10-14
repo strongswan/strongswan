05a3f349a76096941d3d9b95de971af671e3a535
Martin Willi <martin@revosec.ch>
2014-10-14 11:11:34 +0200

    travis: Build-test updown and ext-auth plugins for Windows

--- scripts/test.sh
+++ scripts/test.sh
@@ -58,6 +58,7 @@ win*)
 			--enable-constraints --enable-revocation --enable-pem --enable-pkcs1
 			--enable-pkcs8 --enable-x509 --enable-pubkey --enable-acert
 			--enable-eap-tnc --enable-eap-ttls --enable-eap-identity
+			--enable-updown --enable-ext-auth
 			--enable-tnccs-20 --enable-imc-attestation --enable-imv-attestation
 			--enable-imc-os --enable-imv-os --enable-tnc-imv --enable-tnc-imc
 			--enable-pki --enable-swanctl --enable-socket-win"
