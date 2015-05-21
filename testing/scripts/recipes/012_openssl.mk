682aab205e39473263d4fcf15194526e5b1be490
Tobias Brunner <tobias@strongswan.org>
2015-05-21 09:32:37 +0200

    testing: Don't check parent dir (and subdirs) when downloading OpenSSL packages

--- testing/scripts/recipes/012_openssl.mk
+++ testing/scripts/recipes/012_openssl.mk
@@ -7,7 +7,7 @@ SRC = http://download.strongswan.org/testing/openssl-fips/
 all: install
 
 $(PKG):
-	wget -r $(SRC) --no-directories --directory-prefix $(PKG) --accept deb
+	wget -r $(SRC) --no-directories --directory-prefix $(PKG) --accept deb --no-parent
 
 install: $(PKG)
 	cd $(PKG) && dpkg -i *.deb
