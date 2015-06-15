c6822051135a02651b3c95d67870ed334678a9a0
Tobias Brunner <tobias@strongswan.org>
2015-06-15 16:59:12 +0200

    android: Extend GUI so the MTU can be entered
    This also adds a new area for advanced settings that is only displayed
    if the user requests it (or if advanced settings already have been set).
    
    The min. MTU for IPv6 is 1280, anything lower lets the TUN device
    creation fail if an IPv6 address has been assigned.  If lower MTUs are
    necessary we might be able to catch that later when setting the MTU and
    just use at least 1280 if an IPv6 address was assigned, but let's keep
    it simple for now.

--- src/frontends/android/src/org/strongswan/android/ui/VpnProfileDetailActivity.java
+++ src/frontends/android/src/org/strongswan/android/ui/VpnProfileDetailActivity.java
@@ -60,6 +60,8 @@ import android.widget.TextView;
 public class VpnProfileDetailActivity extends Activity
 {
 	private static final int SELECT_TRUSTED_CERTIFICATE = 0;
+	private static final int MTU_MIN = 1280;
+	private static final int MTU_MAX = 1500;
 
 	private VpnProfileDataSource mDataSource;
 	private Long mId;
@@ -79,6 +81,9 @@ public class VpnProfileDetailActivity extends Activity
 	private CheckBox mCheckAuto;
 	private RelativeLayout mSelectCert;
 	private RelativeLayout mTncNotice;
+	private CheckBox mShowAdvanced;
+	private ViewGroup mAdvancedSettings;
+	private EditText mMTU;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState)
@@ -107,6 +112,11 @@ public class VpnProfileDetailActivity extends Activity
 
 		mCheckAuto = (CheckBox)findViewById(R.id.ca_auto);
 		mSelectCert = (RelativeLayout)findViewById(R.id.select_certificate);
+
+		mShowAdvanced = (CheckBox)findViewById(R.id.show_advanced);
+		mAdvancedSettings = (ViewGroup)findViewById(R.id.advanced_settings);
+
+		mMTU = (EditText)findViewById(R.id.mtu);
 
 		mSelectVpnType.setOnItemSelectedListener(new OnItemSelectedListener() {
 			@Override
@@ -154,6 +164,14 @@ public class VpnProfileDetailActivity extends Activity
 			}
 		});
 
+		mShowAdvanced.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+			{
+				updateAdvancedSettings();
+			}
+		});
+
 		mId = savedInstanceState == null ? null : savedInstanceState.getLong(VpnProfileDataSource.KEY_ID);
 		if (mId == null)
 		{
@@ -165,6 +183,7 @@ public class VpnProfileDetailActivity extends Activity
 
 		updateCredentialView();
 		updateCertificateSelector();
+		updateAdvancedSettings();
 	}
 
 	@Override
@@ -315,6 +334,21 @@ public class VpnProfileDetailActivity extends Activity
 	}
 
 	/**
+	 * Update the advanced settings UI depending on whether any advanced
+	 * settings have already been made.
+	 */
+	private void updateAdvancedSettings()
+	{
+		boolean show = mShowAdvanced.isChecked();
+		if (!show && mProfile != null)
+		{
+			show = mProfile.getMTU() != null;
+		}
+		mShowAdvanced.setVisibility(!show ? View.VISIBLE : View.GONE);
+		mAdvancedSettings.setVisibility(show ? View.VISIBLE : View.GONE);
+	}
+
+	/**
 	 * Save or update the profile depending on whether we actually have a
 	 * profile object or not (this was created in updateProfileData)
 	 */
@@ -368,6 +402,12 @@ public class VpnProfileDetailActivity extends Activity
 			showCertificateAlert();
 			valid = false;
 		}
+		Integer mtu = getInteger(mMTU);
+		if (mtu != null && (mtu < MTU_MIN || mtu > MTU_MAX))
+		{
+			mMTU.setError(String.format(getString(R.string.alert_text_out_of_range), MTU_MIN, MTU_MAX));
+			valid = false;
+		}
 		return valid;
 	}
 
@@ -395,6 +435,7 @@ public class VpnProfileDetailActivity extends Activity
 		}
 		String certAlias = mCheckAuto.isChecked() ? null : mCertEntry.getAlias();
 		mProfile.setCertificateAlias(certAlias);
+		mProfile.setMTU(getInteger(mMTU));
 	}
 
 	/**
@@ -417,6 +458,7 @@ public class VpnProfileDetailActivity extends Activity
 				mVpnType = mProfile.getVpnType();
 				mUsername.setText(mProfile.getUsername());
 				mPassword.setText(mProfile.getPassword());
+				mMTU.setText(mProfile.getMTU() != null ? mProfile.getMTU().toString() : null);
 				useralias = mProfile.getUserCertificateAlias();
 				alias = mProfile.getCertificateAlias();
 				getActionBar().setTitle(mProfile.getName());
@@ -456,6 +498,17 @@ public class VpnProfileDetailActivity extends Activity
 				mCertEntry = null;
 			}
 		}
+	}
+
+	/**
+	 * Get the integer value in the given text box or null if empty
+	 *
+	 * @param view text box (numeric entry assumed)
+	 */
+	private Integer getInteger(EditText view)
+	{
+		String value = view.getText().toString().trim();
+		return value.isEmpty() ? null : Integer.valueOf(value);
 	}
 
 	private class SelectUserCertOnClickListener implements OnClickListener, KeyChainAliasCallback
