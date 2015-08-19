47ee60177ee5ff0f555033a2582068978e69ed20
Tobias Brunner <tobias@strongswan.org>
2015-08-19 17:25:30 +0200

    ikev1: Pass current auth-cfg when looking for key to determine auth method
    If multiple certificates use the same subjects we might choose the wrong
    one otherwise. This way we use the one referenced with leftcert and
    stored in the auth-cfg and we actually do the same thing later in the
    pubkey authenticator.
    
    Fixes #1077.

--- src/libcharon/sa/ikev1/phase1.c
+++ src/libcharon/sa/ikev1/phase1.c
@@ -404,7 +404,7 @@ static auth_method_t get_pubkey_method(private_phase1_t *this, auth_cfg_t *auth)
 		id = (identification_t*)auth->get(auth, AUTH_RULE_IDENTITY);
 		if (id)
 		{
-			private = lib->credmgr->get_private(lib->credmgr, KEY_ANY, id, NULL);
+			private = lib->credmgr->get_private(lib->credmgr, KEY_ANY, id, auth);
 			if (private)
 			{
 				switch (private->get_type(private))
