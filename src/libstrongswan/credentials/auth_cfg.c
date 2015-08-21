fdb90723b7f2b94b16596b8581b10eae3401f392
Tobias Brunner <tobias@strongswan.org>
2015-08-21 11:40:07 +0200

    auth-cfg: Don't enforce EAP_RADIUS
    Basically the same as e79b0e07e4ab.  EAP_RADIUS is also a virtual method
    that will identify itself as a different EAP method later.

--- src/libstrongswan/credentials/auth_cfg.c
+++ src/libstrongswan/credentials/auth_cfg.c
@@ -662,7 +662,8 @@ METHOD(auth_cfg_t, complies, bool,
 			case AUTH_RULE_EAP_TYPE:
 			{
 				if ((uintptr_t)value != (uintptr_t)get(this, t1) &&
-					(uintptr_t)value != EAP_DYNAMIC)
+					(uintptr_t)value != EAP_DYNAMIC &&
+					(uintptr_t)value != EAP_RADIUS)
 				{
 					success = FALSE;
 					if (log_error)
