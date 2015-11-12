586ad7641f356f242cdf6fa8707c8a83bd24764e
Marcin Waligórski <marcin.waligorski@fancyfon.com>
2015-11-12 08:31:37 +0100

    LocalKeystore for managing user and ca certificate locally - not from android keystore.

---
+++ src/frontends/android/app/src/main/java/org/strongswan/android/security/LocalKeystore.java
@@ -0,0 +1,192 @@
+/*
+ * Copyright © 2015 FancyFon Software Ltd.
+ * All rights reserved.
+ */
+package org.strongswan.android.security;
+
+
+import android.content.Context;
+import android.util.Log;
+import org.strongswan.android.logic.StrongSwanApplication;
+import org.strongswan.android.utils.Utils;
+
+import java.io.*;
+import java.security.*;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.Calendar;
+
+/**
+ * @author Marcin Waligórski <marcin.waligorski@fancyfon.com>
+ */
+public class LocalKeystore {
+
+    private static final String TAG = LocalKeystore.class.getSimpleName();
+    // change this password before releasing
+    private static final String PASSWORD = "TYPE_YOUR_PASSWORD_HERE";
+    private static final String KEYSTORE_TYPE = "PKCS12";
+    private static final String CA_KEYSTORE_TYPE = "BKS";
+
+    // TODO: Change this string to enum
+    public static final String CA_TYPE = "CA_";
+    public static final String USER_TYPE = "USER_";
+    private KeyStore userCertKeystore;
+    private KeyStore caCertKeystore;
+
+    public LocalKeystore() throws KeyStoreException {
+        userCertKeystore = KeyStore.getInstance(KEYSTORE_TYPE);
+        caCertKeystore = KeyStore.getInstance(CA_KEYSTORE_TYPE);
+    }
+
+    public boolean addPkcs12(byte[] pkcs12, String password, String certificateId) {
+        try {
+            userCertKeystore.load(new ByteArrayInputStream(pkcs12), password.toCharArray());
+            userCertKeystore.store(StrongSwanApplication.getContext().openFileOutput(USER_TYPE + certificateId, Context
+                    .MODE_PRIVATE), PASSWORD.toCharArray());
+            return true;
+        } catch (IOException e) {
+            Log.e(TAG, "Error adding certificate to keystore: " + e);
+        } catch (NoSuchAlgorithmException e) {
+            Log.e(TAG, "Error adding certificate to keystore: " + e);
+        } catch (CertificateException e) {
+            Log.e(TAG, "Error adding certificate to keystore: " + e);
+        } catch (KeyStoreException e) {
+            Log.e(TAG, "Error adding certificate to keystore: " + e);
+        }
+        return false;
+    }
+
+    public boolean addCaCertificate(byte[] caCertificate, String certificateId) {
+        try {
+            caCertKeystore.load(null, null);
+            Certificate ca = getCaCertificateFromBytes(caCertificate);
+            if(ca != null) {
+                caCertKeystore.setCertificateEntry(certificateId, ca);
+                caCertKeystore.store(StrongSwanApplication.getContext().openFileOutput(CA_TYPE + certificateId, Context
+                        .MODE_PRIVATE), PASSWORD.toCharArray());
+                return true;
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Error adding certificate to keystore: " + e);
+        } catch (NoSuchAlgorithmException e) {
+            Log.e(TAG, "Error adding certificate to keystore: " + e);
+        } catch (CertificateException e) {
+            Log.e(TAG, "Error adding certificate to keystore: " + e);
+        } catch (KeyStoreException e) {
+            Log.e(TAG, "Error adding certificate to keystore: " + e);
+        }
+        return false;
+    }
+
+    private Certificate getCaCertificateFromBytes(byte[] caCertificate) throws CertificateException, IOException {
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+        InputStream caInput = new BufferedInputStream(new ByteArrayInputStream(caCertificate));
+        Certificate ca;
+        try {
+            ca = cf.generateCertificate(caInput);
+        } finally {
+            caInput.close();
+        }
+        return ca;
+    }
+
+    public X509Certificate[] getCertificateChain(String certificateId, String alias) {
+        try {
+            Certificate[] certs = getCertificates(certificateId, alias);
+            return convertToX509Certificates(certs);
+        } catch (KeyStoreException e) {
+            Log.e(TAG, "Error reading certificate chain from keystore: " + e);
+        } catch (CertificateException e) {
+            Log.e(TAG, "Error reading certificate chain from keystore: " + e);
+        } catch (NoSuchAlgorithmException e) {
+            Log.e(TAG, "Error reading certificate chain from keystore: " + e);
+        } catch (FileNotFoundException e) {
+            Log.e(TAG, "Error reading certificate chain from keystore: " + e);
+        } catch (IOException e) {
+            Log.e(TAG, "Error reading certificate chain from keystore: " + e);
+        }
+        return null;
+    }
+
+    private X509Certificate[] convertToX509Certificates(Certificate[] certs) {
+        X509Certificate[] x509Certs = new X509Certificate[certs.length];
+        for(int i = 0; i < certs.length; i++) {
+            x509Certs[i] = (X509Certificate) certs[i];
+        }
+        return x509Certs;
+    }
+
+    private Certificate[] getCertificates(String certificateId, String alias) throws IOException, NoSuchAlgorithmException, CertificateException, KeyStoreException {
+        Certificate[] certs = null;
+        userCertKeystore.load(StrongSwanApplication.getContext().openFileInput(USER_TYPE + certificateId), PASSWORD
+                .toCharArray());
+        certs = userCertKeystore.getCertificateChain(alias);
+        return certs;
+    }
+
+    public X509Certificate getCertificate(String certificateId, String alias) {
+        try {
+            Certificate cert = null;
+            caCertKeystore.load(StrongSwanApplication.getContext().openFileInput(CA_TYPE + certificateId), PASSWORD
+                    .toCharArray());
+            cert = caCertKeystore.getCertificate(alias);
+            return (X509Certificate) cert;
+        } catch (KeyStoreException e) {
+            Log.e(TAG, "Error reading certificate from keystore: " + e);
+        } catch (CertificateException e) {
+            Log.e(TAG, "Error reading certificate from keystore: " + e);
+        } catch (NoSuchAlgorithmException e) {
+            Log.e(TAG, "Error reading certificate from keystore: " + e);
+        } catch (FileNotFoundException e) {
+            Log.e(TAG, "Error reading certificate from keystore: " + e);
+        } catch (IOException e) {
+            Log.e(TAG, "Error reading certificate from keystore: " + e);
+        }
+        return null;
+    }
+
+    public PrivateKey getPrivateKey(String certificateId, String alias) {
+        try {
+            userCertKeystore.load(StrongSwanApplication.getContext().openFileInput(USER_TYPE + certificateId), PASSWORD
+                    .toCharArray());
+            return (PrivateKey) userCertKeystore.getKey(alias, PASSWORD.toCharArray());
+        } catch (UnrecoverableKeyException e) {
+            Log.e(TAG, "Error reading private key from keystore: " + e);
+        } catch (NoSuchAlgorithmException e) {
+            Log.e(TAG, "Error reading private key from keystore: " + e);
+        } catch (KeyStoreException e) {
+            Log.e(TAG, "Error reading private key from keystore: " + e);
+        } catch (CertificateException e) {
+            Log.e(TAG, "Error reading private key from keystore: " + e);
+        } catch (FileNotFoundException e) {
+            Log.e(TAG, "Error reading private key from keystore: " + e);
+        } catch (IOException e) {
+            Log.e(TAG, "Error reading private key from keystore: " + e);
+        }
+        return null;
+    }
+
+    /**
+     * Calculates the SHA-1 hash of the current timestamp.
+     * @return hex encoded SHA-1 hash of the current timestamp or null if failed
+     */
+    public String generateId()
+    {
+        MessageDigest md;
+        try
+        {
+            md = java.security.MessageDigest.getInstance("SHA1");
+            md.update(Calendar.getInstance().getTime().toString()
+                    .getBytes());
+            byte[] hash = md.digest();
+            return Utils.bytesToHex(hash);
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            Log.e(TAG, "Error generating id: " + e);
+        }
+        return null;
+    }
+}
