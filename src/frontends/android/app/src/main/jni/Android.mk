8c0b38e9df145378d8f65594d5ce3a44ed0c5e7b
Tobias Brunner <tobias@strongswan.org>
2015-11-18 11:31:03 +0100

    android: Enable chapoly plugin in the app

--- src/frontends/android/app/src/main/jni/Android.mk
+++ src/frontends/android/app/src/main/jni/Android.mk
@@ -6,7 +6,7 @@ include $(CLEAR_VARS)
 strongswan_USE_BYOD := true
 
 strongswan_CHARON_PLUGINS := android-log openssl fips-prf random nonce pubkey \
-	pkcs1 pkcs8 pem xcbc hmac socket-default \
+	chapoly pkcs1 pkcs8 pem xcbc hmac socket-default \
 	eap-identity eap-mschapv2 eap-md5 eap-gtc eap-tls
 
 ifneq ($(strongswan_USE_BYOD),)
