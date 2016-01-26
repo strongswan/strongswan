2f3879c3fedefa621be40c3ef38a7fa218c59ee6
Tobias Brunner <tobias@strongswan.org>
2016-01-26 17:26:04 +0100

    android: Enable 64-bit ABIs

--- src/frontends/android/app/src/main/jni/Application.mk
+++ src/frontends/android/app/src/main/jni/Application.mk
@@ -1,3 +1,3 @@
 # select the ABI(s) to build for (see CPU-ARCH-ABIS.html in the NDK docs).
-APP_ABI := armeabi x86 mips
+APP_ABI := armeabi arm64-v8a x86 x86_64 mips mips64
 APP_PLATFORM := android-19
