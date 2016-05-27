6b2d9c851ad299aa44b40a2c1e126590cf4b33e3
Noel Kuntze <noel@familie-kuntze.de>
2016-05-27 18:46:14 +0200

    w32.h: Add header file for Windows

---
+++ src/libipsec/w32.h
@@ -0,0 +1,39 @@
+/*
+ * File:   %<%NAME%>%.%<%EXTENSION%>%
+ * Author: %<%USER%>%
+ *
+ * Created on %<%DATE%>%, %<%TIME%>%
+ */
+
+#ifndef WINDOWS_32_PROTOCOL_HEADERS
+#define WINDOWS_32_PROTOCOL_HEADERS%
+
+/*
+ * Structure of an internet header, naked of options.
+ */
+struct ip
+  {
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+    unsigned int ip_hl:4;               /* header length */
+    unsigned int ip_v:4;                /* version */
+#endif
+    uint8_t ip_tos;                    /* type of service */
+    u_short ip_len;                     /* total length */
+    u_short ip_id;                      /* identification */
+    u_short ip_off;                     /* fragment offset field */
+#define IP_RF 0x8000                    /* reserved fragment flag */
+#define IP_DF 0x4000                    /* dont fragment flag */
+#define IP_MF 0x2000                    /* more fragments flag */
+#define IP_OFFMASK 0x1fff               /* mask for fragmenting bits */
+    uint8_t ip_ttl;                    /* time to live */
+    uint8_t ip_p;                      /* protocol */
+    u_short ip_sum;                     /* checksum */
+    struct in_addr ip_src, ip_dst;      /* source and dest address */
+  };
+
+#define IPPROTO_IPIP 4
+#define IPPROTO_IPv6 41
+#define IPPROTO_NONE 59
+
+#endif /* WINDOWS_32_PROTOCOL_HEADERS */
+
