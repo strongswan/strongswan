219d760f019d105606ebcacf8697ada0b9d199c5
Mariusz Rafalski <mariusz.rafalski@fancyfon.com>
2016-06-21 10:01:23 +0200

    fix for displaying null fields in main activity + bring back inactive username field
    Change-Id: Iebf2196af16a46ca0f0750f862b7620c5786ccb0

--- src/frontends/android/app/src/main/java/org/strongswan/android/ui/adapter/VpnProfileAdapter.java
+++ src/frontends/android/app/src/main/java/org/strongswan/android/ui/adapter/VpnProfileAdapter.java
@@ -26,6 +26,7 @@ import org.strongswan.android.data.VpnProfile;
 import org.strongswan.android.data.VpnType.VpnTypeFeature;
 
 import android.content.Context;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -68,7 +69,8 @@ public class VpnProfileAdapter extends ArrayAdapter<VpnProfile>
 		if (profile.getVpnType().has(VpnTypeFeature.USER_PASS))
 		{	/* if the view is reused we make sure it is visible */
 			tv.setVisibility(View.VISIBLE);
-			tv.setText(getContext().getString(R.string.profile_username_label) + ": " + profile.getUsername());
+			String userName = (profile.getUsername() == null ? getContext().getString(R.string.username_absent) : profile.getUsername());
+			tv.setText(getContext().getString(R.string.profile_username_label) + " " + userName);
 		}
 		else if (profile.getVpnType().has(VpnTypeFeature.CERTIFICATE) &&
 				 profile.getLocalId() != null)
@@ -83,7 +85,8 @@ public class VpnProfileAdapter extends ArrayAdapter<VpnProfile>
 		tv = (TextView)vpnProfileView.findViewById(R.id.profile_item_certificate);
 		if (profile.getVpnType().has(VpnTypeFeature.CERTIFICATE))
 		{
-			tv.setText(getContext().getString(R.string.profile_user_certificate_label) + ": " + profile.getUserCertificateAlias());
+			String certDisplayData = (TextUtils.isEmpty(profile.getUserCertificateAlias()) ? getContext().getString(R.string.certificate_absent) : profile.getUserCertificateAlias());
+			tv.setText(getContext().getString(R.string.profile_user_certificate_label) + " " + certDisplayData);
 			tv.setVisibility(View.VISIBLE);
 		}
 		else
