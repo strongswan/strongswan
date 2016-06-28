1b32b88aff86d9ba8a4030fe1ab44cca887a2f6d
Noel Kuntze <noel@familie-kuntze.de>
2016-06-28 23:33:09 +0200

    kernel_libipsec_router.c: Add own copyright

--- src/libcharon/plugins/kernel_libipsec/kernel_libipsec_router.c
+++ src/libcharon/plugins/kernel_libipsec/kernel_libipsec_router.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2013 Tobias Brunner
+ * Copyright (C) 2016 Noel Kuntze
  * Hochschule fuer Technik Rapperswil
  *
  * This program is free software; you can redistribute it and/or modify it
