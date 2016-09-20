strongswan/strongswan

commit 4e8f5a189cce83c05a953387b3174fdf142b5f7c

travis: Add apidoc check
This requires at least Ubuntu 14.04 (the Doxygen version in 12.04 has some
issues with our Doxyfile and prints lots of warnings).

scripts/test.sh
@@ -110,6 +110,11 @@ osx)
 dist)
 	TARGET=distcheck
 	;;
+apidoc)
+	DEPS="doxygen"
+	CONFIG="--disable-defaults"
+	TARGET=apidoc
+	;;
 *)
 	echo "$0: unknown test $TEST" >&2
 	exit 1
@@ -146,5 +151,27 @@ CONFIG="$CONFIG
 
 echo "$ ./autogen.sh"
 ./autogen.sh || exit $?
-echo "$ CC=$CC CFLAGS=\"$CFLAGS\" ./configure $CONFIG && make $TARGET"
-CC="$CC" CFLAGS="$CFLAGS" ./configure $CONFIG && make -j4 $TARGET
+echo "$ CC=$CC CFLAGS=\"$CFLAGS\" ./configure $CONFIG"
+CC="$CC" CFLAGS="$CFLAGS" ./configure $CONFIG || exit $?
+
+case "$TEST" in
+apidoc)
+	exec 2>make.warnings
+	;;
+*)
+	;;
+esac
+
+echo "$ make $TARGET"
+make -j4 $TARGET
+
+case "$TEST" in
+apidoc)
+	if test -s make.warnings; then
+		cat make.warnings
+		exit 1
+	fi
+	;;
+*)
+	;;
+esac

.travis.yml
@@ -42,6 +42,8 @@ matrix:
     - compiler: gcc
       env: TEST=dist
     - compiler: gcc
+      env: TEST=apidoc
+    - compiler: gcc
       env: TEST=win64 MONOLITHIC=yes
     - compiler: gcc
       env: TEST=win32 MONOLITHIC=yes

commit 003fec52e08208cfcfd9316752fa1ba2609c955a

travis: Properly pass back result of make
Fixes: 4e8f5a189cce ("travis: Add apidoc check")

scripts/test.sh
@@ -159,7 +159,7 @@ apidoc)
 esac
 
 echo "$ make $TARGET"
-make -j4 $TARGET
+make -j4 $TARGET || exit $?
 
 case "$TEST" in
 apidoc)