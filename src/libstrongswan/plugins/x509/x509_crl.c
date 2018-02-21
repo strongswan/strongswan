9ba01ce3807c0e85e3d90736a09f29f2979b9181
Tobias Brunner <tobias@strongswan.org>
2018-02-21 11:13:42 +0100

    x509: Fix leak if a CRL contains multiple authorityKeyIdentifiers

--- src/libstrongswan/plugins/x509/x509_crl.c
+++ src/libstrongswan/plugins/x509/x509_crl.c
@@ -302,6 +302,7 @@ static bool parse(private_x509_crl_t *this)
 						}
 						break;
 					case OID_AUTHORITY_KEY_ID:
+						chunk_free(&this->authKeyIdentifier);
 						this->authKeyIdentifier =
 							x509_parse_authorityKeyIdentifier(
 									object, level, &this->authKeySerialNumber);
