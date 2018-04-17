4af1fd59dd19ddae38b792581dc99b8ec325dfbb
Tobias Brunner <tobias@strongswan.org>
2018-04-17 11:37:35 +0200

    thread: Properly clean up meta data of thread if pthread_create() fails

--- src/libstrongswan/threading/thread.c
+++ src/libstrongswan/threading/thread.c
@@ -348,6 +348,8 @@ thread_t *thread_create(thread_main_t main, void *arg)
 	{
 		DBG1(DBG_LIB, "failed to create thread!");
 		this->mutex->lock(this->mutex);
+		this->terminated = TRUE;
+		this->detached_or_joined = TRUE;
 		thread_destroy(this);
 		return NULL;
 	}
