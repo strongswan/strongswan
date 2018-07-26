7f94528061ef840d1db5bd86ab7dd84c62f847d7
Tobias Brunner <tobias@strongswan.org>
2018-07-26 17:57:36 +0200

    vici: Make PPK related options configurable

--- src/libcharon/plugins/vici/vici_query.c
+++ src/libcharon/plugins/vici/vici_query.c
@@ -787,6 +787,7 @@ CALLBACK(list_conns, vici_message_t*,
 	child_cfg_t *child_cfg;
 	char *ike, *str, *interface;
 	uint32_t manual_prio, dpd_delay, dpd_timeout;
+	identification_t *ppk_id;
 	linked_list_t *list;
 	traffic_selector_t *ts;
 	lifetime_cfg_t *lft;
@@ -847,6 +848,16 @@ CALLBACK(list_conns, vici_message_t*,
 		if (dpd_timeout)
 		{
 			b->add_kv(b, "dpd_timeout", "%u", dpd_timeout);
+		}
+
+		ppk_id = peer_cfg->get_ppk_id(peer_cfg);
+		if (ppk_id)
+		{
+			b->add_kv(b, "ppk_id", "%Y", ppk_id);
+		}
+		if (peer_cfg->ppk_required(peer_cfg))
+		{
+			b->add_kv(b, "ppk_required", "yes");
 		}
 
 		build_auth_cfgs(peer_cfg, TRUE, b);
