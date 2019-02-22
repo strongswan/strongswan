c5e22e2295ccf3cf9d7e97b605f42dc0d5a8da2e
Robert Zabel <robert.zabel@secucloud.com>
2019-02-22 18:19:46 +0100

    sqlplugin: return psk for unspecified left

--- src/libcharon/plugins/sql/sql_cred.c
+++ src/libcharon/plugins/sql/sql_cred.c
@@ -300,7 +300,7 @@ METHOD(credential_set_t, create_shared_enumerator, enumerator_t*,
 				"JOIN identities AS m ON sm.identity = m.id "
 				"JOIN shared_secret_identity AS so ON s.id = so.shared_secret "
 				"JOIN identities AS o ON so.identity = o.id "
-				"WHERE m.type = ? AND m.data = ? AND o.type = ? AND o.data = ? "
+				"WHERE m.type = ? AND m.data = IFNULL(?,'%any') AND o.type = ? AND o.data = ? "
 				"AND (? OR s.type = ?)",
 				DB_INT, me->get_type(me), DB_BLOB, me->get_encoding(me),
 				DB_INT, other->get_type(other), DB_BLOB, other->get_encoding(other),
