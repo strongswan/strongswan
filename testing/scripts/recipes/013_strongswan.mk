ceca26c88e1e940868ff36cf7305032b38d96577
Tobias Brunner <tobias@strongswan.org>
2019-03-25 14:28:21 +0100

    testing: Enable Python eggs in testing environment (i.e. vici's Python bindings)

--- testing/scripts/recipes/013_strongswan.mk
+++ testing/scripts/recipes/013_strongswan.mk
@@ -105,7 +105,8 @@ CONFIG_OPTS = \
 	--enable-newhope \
 	--enable-systemd \
 	--enable-counters \
-	--enable-save-keys
+	--enable-save-keys \
+	--enable-python-eggs-install
 
 export ADA_PROJECT_PATH=/usr/local/ada/lib/gnat
 
