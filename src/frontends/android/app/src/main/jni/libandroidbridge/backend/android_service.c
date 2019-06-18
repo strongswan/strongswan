strongswan/strongswan

commit 9486a2e5b0b5b1e133097c8c25491ca97a31d1db

ike-cfg: Pass arguments as struct

src/frontends/android/app/src/main/jni/libandroidbridge/backend/android_service.c
@@ -742,6 +742,13 @@ static job_requeue_t initiate(private_android_service_t *this)
 	proposal_t *proposal;
 	ike_sa_t *ike_sa;
 	auth_cfg_t *auth;
+	ike_cfg_create_t ike = {
+		.version = IKEV2,
+		.local = "0.0.0.0",
+		.local_port = charon->socket->get_port(charon->socket, FALSE),
+		.foce_encap = TRUE,
+		.fragmentation = FRAGMENTATION_YES,
+	};
 	peer_cfg_create_t peer = {
 		.cert_policy = CERT_ALWAYS_SEND,
 		.unique = UNIQUE_REPLACE,
@@ -761,23 +768,20 @@ static job_requeue_t initiate(private_android_service_t *this)
 		.dpd_action = ACTION_RESTART,
 		.close_action = ACTION_RESTART,
 	};
-	char *type, *server, *remote_id;
-	int port;
-	bool certreq;
+	char *type, *remote_id;
 
 	if (android_sdk_version >= ANDROID_LOLLIPOP)
 	{   /* only try once and notify the GUI on Android 5+ where we have a blocking TUN device */
 		peer.keyingtries = 1;
 	}
 
-	server = this->settings->get_str(this->settings, "connection.server", NULL);
-	port = this->settings->get_int(this->settings, "connection.port",
-								   IKEV2_UDP_PORT);
-	certreq = this->settings->get_bool(this->settings, "connection.certreq",
-									   TRUE);
-	ike_cfg = ike_cfg_create(IKEV2, certreq, TRUE, "0.0.0.0",
-							 charon->socket->get_port(charon->socket, FALSE),
-							 server, port, FRAGMENTATION_YES, 0);
+	ike.remote = this->settings->get_str(this->settings, "connection.server",
+										 NULL);
+	ike.remote_port = this->settings->get_int(this->settings, "connection.port",
+											  IKEV2_UDP_PORT);
+	ike.no_certreq = !this->settings->get_bool(this->settings,
+											   "connection.certreq", TRUE);
+	ike_cfg = ike_cfg_create(&ike);
 	proposal = parse_proposal(this, PROTO_IKE, "connection.ike_proposal");
 	if (proposal)
 	{

src/charon-cmd/cmd/cmd_connection.c
@@ -142,9 +142,13 @@ static peer_cfg_t* create_peer_cfg(private_cmd_connection_t *this)
 {
 	ike_cfg_t *ike_cfg;
 	peer_cfg_t *peer_cfg;
-	uint16_t local_port, remote_port = IKEV2_UDP_PORT;
-	ike_version_t version = IKE_ANY;
 	proposal_t *proposal;
+	ike_cfg_create_t ike = {
+		.local = "0.0.0.0",
+		.remote = this->host,
+		.remote_port = IKEV2_UDP_PORT,
+		.fragmentation = FRAGMENTATION_YES,
+	};
 	peer_cfg_create_t peer = {
 		.cert_policy = CERT_SEND_IF_ASKED,
 		.unique = UNIQUE_REPLACE,
@@ -161,7 +165,7 @@ static peer_cfg_t* create_peer_cfg(private_cmd_connection_t *this)
 		case PROF_V2_PUB:
 		case PROF_V2_EAP:
 		case PROF_V2_PUB_EAP:
-			version = IKEV2;
+			ike.version = IKEV2;
 			break;
 		case PROF_V1_PUB_AM:
 		case PROF_V1_XAUTH_AM:
@@ -173,17 +177,16 @@ static peer_cfg_t* create_peer_cfg(private_cmd_connection_t *this)
 		case PROF_V1_XAUTH:
 		case PROF_V1_XAUTH_PSK:
 		case PROF_V1_HYBRID:
-			version = IKEV1;
+			ike.version = IKEV1;
 			break;
 	}
 
-	local_port = charon->socket->get_port(charon->socket, FALSE);
-	if (local_port != IKEV2_UDP_PORT)
+	ike.local_port = charon->socket->get_port(charon->socket, FALSE);
+	if (ike.local_port != IKEV2_UDP_PORT)
 	{
-		remote_port = IKEV2_NATT_PORT;
+		ike.remote_port = IKEV2_NATT_PORT;
 	}
-	ike_cfg = ike_cfg_create(version, TRUE, FALSE, "0.0.0.0", local_port,
-					this->host, remote_port, FRAGMENTATION_NO, 0);
+	ike_cfg = ike_cfg_create(&ike);
 	if (this->ike_proposals->get_count(this->ike_proposals))
 	{
 		while (this->ike_proposals->remove_first(this->ike_proposals,

src/charon-nm/nm/nm_service.c
@@ -381,8 +381,8 @@ static gboolean connect_(NMVpnServicePlugin *plugin, NMConnection *connection,
 	NMSettingVpn *vpn;
 	enumerator_t *enumerator;
 	identification_t *user = NULL, *gateway = NULL;
-	const char *address, *str;
-	bool virtual, encap, proposal;
+	const char *str;
+	bool virtual, proposal;
 	proposal_t *prop;
 	ike_cfg_t *ike_cfg;
 	peer_cfg_t *peer_cfg;
@@ -394,6 +394,13 @@ static gboolean connect_(NMVpnServicePlugin *plugin, NMConnection *connection,
 	certificate_t *cert = NULL;
 	x509_t *x509;
 	bool agent = FALSE, smartcard = FALSE, loose_gateway_id = FALSE;
+	ike_cfg_create_t ike = {
+		.version = IKEV2,
+		.local = "%any",
+		.local_port = charon->socket->get_port(charon->socket, FALSE),
+		.remote_port = IKEV2_UDP_PORT,
+		.fragmentation = FRAGMENTATION_YES,
+	};
 	peer_cfg_create_t peer = {
 		.cert_policy = CERT_SEND_IF_ASKED,
 		.unique = UNIQUE_REPLACE,
@@ -430,8 +437,8 @@ static gboolean connect_(NMVpnServicePlugin *plugin, NMConnection *connection,
 		 priv->name);
 	DBG4(DBG_CFG, "%s",
 		 nm_setting_to_string(NM_SETTING(vpn)));
-	address = nm_setting_vpn_get_data_item(vpn, "address");
-	if (!address || !*address)
+	ike.remote = (char*)nm_setting_vpn_get_data_item(vpn, "address");
+	if (!ike.remote || !*ike.remote)
 	{
 		g_set_error(err, NM_VPN_PLUGIN_ERROR, NM_VPN_PLUGIN_ERROR_BAD_ARGUMENTS,
 					"Gateway address missing.");
@@ -440,7 +447,7 @@ static gboolean connect_(NMVpnServicePlugin *plugin, NMConnection *connection,
 	str = nm_setting_vpn_get_data_item(vpn, "virtual");
 	virtual = streq(str, "yes");
 	str = nm_setting_vpn_get_data_item(vpn, "encap");
-	encap = streq(str, "yes");
+	ike.force_encap = streq(str, "yes");
 	str = nm_setting_vpn_get_data_item(vpn, "ipcomp");
 	child.options |= streq(str, "yes") ? OPT_IPCOMP : 0;
 	str = nm_setting_vpn_get_data_item(vpn, "method");
@@ -503,7 +510,7 @@ static gboolean connect_(NMVpnServicePlugin *plugin, NMConnection *connection,
 		 * of the gateway as its identity. This identity will be used for
 		 * certificate lookup and requires the configured IP/DNS to be
 		 * included in the gateway certificate. */
-		gateway = identification_create_from_string((char*)address);
+		gateway = identification_create_from_string(ike.remote);
 		DBG1(DBG_CFG, "using CA certificate, gateway identity '%Y'", gateway);
 		loose_gateway_id = TRUE;
 	}
@@ -634,10 +641,7 @@ static gboolean connect_(NMVpnServicePlugin *plugin, NMConnection *connection,
 	/**
 	 * Set up configurations
 	 */
-	ike_cfg = ike_cfg_create(IKEV2, TRUE, encap, "%any",
-							 charon->socket->get_port(charon->socket, FALSE),
-							(char*)address, IKEV2_UDP_PORT,
-							 FRAGMENTATION_YES, 0);
+	ike_cfg = ike_cfg_create(&ike);
 
 	str = nm_setting_vpn_get_data_item(vpn, "proposal");
 	proposal = streq(str, "yes");

src/conftest/config.c
@@ -107,14 +107,21 @@ static ike_cfg_t *load_ike_config(private_config_t *this,
 	ike_cfg_t *ike_cfg;
 	proposal_t *proposal;
 	char *token;
+	ike_cfg_create_t ike = {
+		.version = IKEV2,
+		.local = settings->get_str(settings, "configs.%s.lhost",
+								   "%any", config),
+		.local_port = settings->get_int(settings, "configs.%s.lport",
+									500, config),
+		.remote = settings->get_str(settings, "configs.%s.rhost",
+									"%any", config),
+		.remote_port = settings->get_int(settings, "configs.%s.rport",
+									500, config),
+		.force_encap = settings->get_bool(settings, "configs.%s.fake_nat",
+									FALSE, config),
+	};
 
-	ike_cfg = ike_cfg_create(IKEV2, TRUE,
-		settings->get_bool(settings, "configs.%s.fake_nat", FALSE, config),
-		settings->get_str(settings, "configs.%s.lhost", "%any", config),
-		settings->get_int(settings, "configs.%s.lport", 500, config),
-		settings->get_str(settings, "configs.%s.rhost", "%any", config),
-		settings->get_int(settings, "configs.%s.rport", 500, config),
-		FRAGMENTATION_NO, 0);
+	ike_cfg = ike_cfg_create(&ike);
 	token = settings->get_str(settings, "configs.%s.proposal", NULL, config);
 	if (token)
 	{

src/frontends/osx/charon-xpc/xpc_dispatch.c
@@ -78,6 +78,14 @@ static peer_cfg_t* create_peer_cfg(char *name, char *host)
 	ike_cfg_t *ike_cfg;
 	peer_cfg_t *peer_cfg;
 	uint16_t local_port, remote_port = IKEV2_UDP_PORT;
+	ike_cfg_create_t ike = {
+		.version = IKEV2,
+		.local = "0.0.0.0",
+		.remote = host,
+		.remote_port = IKEV2_UDP_PORT,
+		.no_certreq = TRUE,
+		.fragmentation = FRAGMENTATION_YES,
+	};
 	peer_cfg_create_t peer = {
 		.cert_policy = CERT_SEND_IF_ASKED,
 		.unique = UNIQUE_REPLACE,
@@ -88,13 +96,12 @@ static peer_cfg_t* create_peer_cfg(char *name, char *host)
 		.dpd = 30,
 	};
 
-	local_port = charon->socket->get_port(charon->socket, FALSE);
-	if (local_port != IKEV2_UDP_PORT)
+	ike.local_port = charon->socket->get_port(charon->socket, FALSE);
+	if (ike.local_port != IKEV2_UDP_PORT)
 	{
-		remote_port = IKEV2_NATT_PORT;
+		ike.remote_port = IKEV2_NATT_PORT;
 	}
-	ike_cfg = ike_cfg_create(IKEV2, FALSE, FALSE, "0.0.0.0", local_port,
-							 host, remote_port, FRAGMENTATION_NO, 0);
+	ike_cfg = ike_cfg_create(&ike);
 	ike_cfg->add_proposal(ike_cfg, proposal_create_default(PROTO_IKE));
 	ike_cfg->add_proposal(ike_cfg, proposal_create_default_aead(PROTO_IKE));
 	peer_cfg = peer_cfg_create(name, ike_cfg, &peer);

src/libcharon/config/ike_cfg.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2018 Tobias Brunner
+ * Copyright (C) 2012-2019 Tobias Brunner
  * Copyright (C) 2005-2007 Martin Willi
  * Copyright (C) 2005 Jan Hutter
  * HSR Hochschule fuer Technik Rapperswil
@@ -609,13 +609,10 @@ bool ike_cfg_has_address(ike_cfg_t *cfg, host_t *addr, bool local)
 	return found;
 }
 
-/**
- * Described in header.
+/*
+ * Described in header
  */
-ike_cfg_t *ike_cfg_create(ike_version_t version, bool certreq, bool force_encap,
-						  char *me, uint16_t my_port,
-						  char *other, uint16_t other_port,
-						  fragmentation_t fragmentation, uint8_t dscp)
+ike_cfg_t *ike_cfg_create(ike_cfg_create_t *data)
 {
 	private_ike_cfg_t *this;
 
@@ -644,24 +641,24 @@ ike_cfg_t *ike_cfg_create(ike_version_t version, bool certreq, bool force_encap,
 			.destroy = _destroy,
 		},
 		.refcount = 1,
-		.version = version,
-		.certreq = certreq,
-		.force_encap = force_encap,
-		.fragmentation = fragmentation,
-		.me = strdup(me),
+		.version = data->version,
+		.certreq = !data->no_certreq,
+		.force_encap = data->force_encap,
+		.fragmentation = data->fragmentation,
+		.me = strdup(data->local),
 		.my_ranges = linked_list_create(),
 		.my_hosts = linked_list_create(),
-		.other = strdup(other),
+		.other = strdup(data->remote),
 		.other_ranges = linked_list_create(),
 		.other_hosts = linked_list_create(),
-		.my_port = my_port,
-		.other_port = other_port,
-		.dscp = dscp,
+		.my_port = data->local_port,
+		.other_port = data->remote_port,
+		.dscp = data->dscp,
 		.proposals = linked_list_create(),
 	);
 
-	parse_addresses(me, this->my_hosts, this->my_ranges);
-	parse_addresses(other, this->other_hosts, this->other_ranges);
+	parse_addresses(data->local, this->my_hosts, this->my_ranges);
+	parse_addresses(data->remote, this->other_hosts, this->other_ranges);
 
 	return &this->public;
 }

src/libcharon/config/ike_cfg.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2018 Tobias Brunner
+ * Copyright (C) 2012-2019 Tobias Brunner
  * Copyright (C) 2005-2007 Martin Willi
  * Copyright (C) 2005 Jan Hutter
  * HSR Hochschule fuer Technik Rapperswil
@@ -26,6 +26,7 @@
 typedef enum ike_version_t ike_version_t;
 typedef enum fragmentation_t fragmentation_t;
 typedef struct ike_cfg_t ike_cfg_t;
+typedef struct ike_cfg_create_t ike_cfg_create_t;
 
 #include <library.h>
 #include <networking/host.h>
@@ -241,30 +242,41 @@ struct ike_cfg_t {
 };
 
 /**
- * Creates a ike_cfg_t object.
+ * Data passed to the constructor of an ike_cfg_t object.
  *
- * Supplied hosts become owned by ike_cfg, strings get cloned.
+ * local and remote are comma separated lists of IP addresses, DNS names,
+ * IP ranges or subnets. When initiating, the first non-range/subnet address is
+ * used as address. When responding, a match is performed against all items in
+ * the list.
+ */
+struct ike_cfg_create_t {
+	/** IKE major version to use for this config */
+	ike_version_t version;
+	/** Address/DNS name of local peer (cloned) */
+	char *local;
+	/** IKE port to use as source, 500 uses IKEv2 port floating */
+	uint16_t local_port;
+	/** Address/DNS name of remote peer (cloned) */
+	char *remote;
+	/** IKE port to use as dest, 500 uses IKEv2 port floating */
+	uint16_t remote_port;
+	/** TRUE to not send any certificate requests */
+	bool no_certreq;
+	/** Enforce UDP encapsulation by faking NATD notify */
+	bool force_encap;
+	/** Use IKE fragmentation */
+	fragmentation_t fragmentation;
+	/** DSCP value to send IKE packets with */
+	uint8_t dscp;
+};
+
+/**
+ * Creates an ike_cfg_t object.
  *
- * me and other are comma separated lists of IP addresses, DNS names, IP ranges
- * or subnets. When initiating, the first non-range/subnet address is used
- * as address. When responding, a match is performed against all items in the
- * list.
- *
- * @param version			IKE major version to use for this config
- * @param certreq			TRUE to send a certificate request
- * @param force_encap		enforce UDP encapsulation by faking NATD notify
- * @param me				address/DNS name of local peer
- * @param my_port			IKE port to use as source, 500 uses IKEv2 port floating
- * @param other				address/DNS name of remote peer
- * @param other_port		IKE port to use as dest, 500 uses IKEv2 port floating
- * @param fragmentation		use IKEv1 fragmentation
- * @param dscp				DSCP value to send IKE packets with
+ * @param data				data for this ike_cfg
  * @return					ike_cfg_t object.
  */
-ike_cfg_t *ike_cfg_create(ike_version_t version, bool certreq, bool force_encap,
-						  char *me, uint16_t my_port,
-						  char *other, uint16_t other_port,
-						  fragmentation_t fragmentation, uint8_t dscp);
+ike_cfg_t *ike_cfg_create(ike_cfg_create_t *data);
 
 /**
  * Determine the address family of the local or remote address(es).  If multiple

src/libcharon/plugins/ha/ha_tunnel.c
@@ -190,6 +190,14 @@ static void setup_tunnel(private_ha_tunnel_t *this,
 	auth_cfg_t *auth_cfg;
 	child_cfg_t *child_cfg;
 	traffic_selector_t *ts;
+	ike_cfg_create_t ike = {
+		.version = IKEV2,
+		.local = local,
+		.local_port = charon->socket->get_port(charon->socket, FALSE),
+		.remote = remote,
+		.remote_port = IKEV2_UDP_PORT,
+		.no_certreq = TRUE,
+	};
 	peer_cfg_create_t peer = {
 		.cert_policy = CERT_NEVER_SEND,
 		.unique = UNIQUE_KEEP,
@@ -222,9 +230,7 @@ static void setup_tunnel(private_ha_tunnel_t *this,
 	lib->credmgr->add_set(lib->credmgr, &this->creds.public);
 
 	/* create config and backend */
-	ike_cfg = ike_cfg_create(IKEV2, FALSE, FALSE, local,
-							 charon->socket->get_port(charon->socket, FALSE),
-							 remote, IKEV2_UDP_PORT, FRAGMENTATION_NO, 0);
+	ike_cfg = ike_cfg_create(&ike);
 	ike_cfg->add_proposal(ike_cfg, proposal_create_default(PROTO_IKE));
 	ike_cfg->add_proposal(ike_cfg, proposal_create_default_aead(PROTO_IKE));
 	peer_cfg = peer_cfg_create(HA_CFG_NAME, ike_cfg, &peer);

commit 07a6e59b1cd2ce1bc66e716dc4caebe120c365b7

android: Fix remote identity fallback after changing IKE config creation
Fixes: 9486a2e5b0b5 ("ike-cfg: Pass arguments as struct")

src/frontends/android/app/src/main/jni/libandroidbridge/backend/android_service.c
@@ -829,7 +829,7 @@ static job_requeue_t initiate(private_android_service_t *this)
 	if (!gateway || gateway->get_type(gateway) == ID_ANY)
 	{
 		DESTROY_IF(gateway);
-		gateway = identification_create_from_string(server);
+		gateway = identification_create_from_string(ike.remote);
 		/* only use this if remote ID was not configured explicitly */
 		auth->add(auth, AUTH_RULE_IDENTITY_LOOSE, TRUE);
 	}