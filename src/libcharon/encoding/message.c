a1efa5510a6a6c253be951e12cc9e7e04eae342e
Tobias Brunner <tobias@strongswan.org>
2019-08-20 14:53:16 +0200

    generator: Make pointer to length field optional
    Only useful if we generate an IKE header.

--- src/libcharon/encoding/message.c
+++ src/libcharon/encoding/message.c
@@ -2089,7 +2089,6 @@ METHOD(message_t, fragment, status_t,
 	host_t *src, *dst;
 	chunk_t data;
 	status_t status;
-	uint32_t *lenpos;
 	size_t len;
 
 	src = this->packet->get_source(this->packet);
@@ -2126,7 +2125,7 @@ METHOD(message_t, fragment, status_t,
 			DESTROY_IF(generator);
 			return status;
 		}
-		data = generator->get_chunk(generator, &lenpos);
+		data = generator->get_chunk(generator, NULL);
 		len = data.len + (encrypted ? encrypted->get_length(encrypted) : 0);
 	}
 
@@ -2182,7 +2181,7 @@ METHOD(message_t, fragment, status_t,
 		}
 		next = encrypted->payload_interface.get_next_type((payload_t*)encrypted);
 		encrypted->generate_payloads(encrypted, generator);
-		data = generator->get_chunk(generator, &lenpos);
+		data = generator->get_chunk(generator, NULL);
 		if (!is_encoded(this))
 		{
 			encrypted->destroy(encrypted);
