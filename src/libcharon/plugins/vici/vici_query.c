c738704ab68c88450aab41892f3c23d2b2251f74
Andreas Steffen <andreas.steffen@strongswan.org>
2019-10-15 22:30:40 +0200

    vici: List drbgs in get_algorithms

--- src/libcharon/plugins/vici/vici_query.c
+++ src/libcharon/plugins/vici/vici_query.c
@@ -1231,6 +1231,7 @@ CALLBACK(get_algorithms, vici_message_t*,
 	hash_algorithm_t hash;
 	pseudo_random_function_t prf;
 	ext_out_function_t xof;
+	drbg_type_t drbg;
 	diffie_hellman_group_t group;
 	rng_quality_t quality;
 	const char *plugin_name;
@@ -1287,6 +1288,15 @@ CALLBACK(get_algorithms, vici_message_t*,
 	while (enumerator->enumerate(enumerator, &xof, &plugin_name))
 	{
 		add_algorithm(b, ext_out_function_names, xof, plugin_name);
+	}
+	enumerator->destroy(enumerator);
+	b->end_section(b);
+
+	b->begin_section(b, "drbg");
+	enumerator = lib->crypto->create_drbg_enumerator(lib->crypto);
+	while (enumerator->enumerate(enumerator, &drbg, &plugin_name))
+	{
+		add_algorithm(b, drbg_type_names, drbg, plugin_name);
 	}
 	enumerator->destroy(enumerator);
 	b->end_section(b);
