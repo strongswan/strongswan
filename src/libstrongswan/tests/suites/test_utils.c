strongswan/strongswan

commit 149d1bbb055a9326978d7caa01b937334bc23ff1

memory: Use explicit_bzero() as memwipe() if available

src/libstrongswan/utils/utils/memory.h
@@ -22,6 +22,10 @@
 #ifndef MEMORY_H_
 #define MEMORY_H_
 
+#ifdef HAVE_EXPLICIT_BZERO
+#include <string.h>
+#endif
+
 /**
  * Helper function that compares two binary blobs for equality
  */
@@ -82,6 +86,9 @@ static inline void *memset_noop(void *s, int c, size_t n)
  */
 void memxor(uint8_t dest[], const uint8_t src[], size_t n);
 
+#ifdef HAVE_EXPLICIT_BZERO
+#define memwipe(ptr, n) explicit_bzero(ptr, n)
+#else /* HAVE_EXPLICIT_BZERO */
 /**
  * Safely overwrite n bytes of memory at ptr with zero, non-inlining variant.
  */
@@ -133,6 +140,7 @@ static inline void memwipe(void *ptr, size_t n)
 		memwipe_noinline(ptr, n);
 	}
 }
+#endif /* HAVE_EXPLICIT_BZERO */
 
 /**
  * A variant of strstr with the characteristics of memchr, where haystack is not

configure.ac
@@ -650,7 +650,7 @@ AC_CHECK_FUNC(
 )
 
 AC_CHECK_FUNCS(prctl mallinfo getpass closefrom getpwnam_r getgrnam_r getpwuid_r)
-AC_CHECK_FUNCS(fmemopen funopen mmap memrchr setlinebuf strptime dirfd sigwaitinfo)
+AC_CHECK_FUNCS(fmemopen funopen mmap memrchr setlinebuf strptime dirfd sigwaitinfo explicit_bzero)
 
 AC_CHECK_FUNC([syslog], [
 	AC_DEFINE([HAVE_SYSLOG], [], [have syslog(3) and friends])

src/libstrongswan/utils/utils/memory.c
@@ -60,6 +60,7 @@ void memxor(uint8_t dst[], const uint8_t src[], size_t n)
 	}
 }
 
+#ifndef HAVE_EXPLICIT_BZERO
 /**
  * Described in header.
  */
@@ -67,6 +68,7 @@ void memwipe_noinline(void *ptr, size_t n)
 {
 	memwipe_inline(ptr, n);
 }
+#endif /* HAVE_EXPLICIT_BZERO */
 
 /**
  * Described in header.

commit cf98706bb8c48327ca65b08f849ec2292b239a57

utils: Handle NULL consistently if memwipe() is implemented via explicit_bzero()
Our own implementation ignores NULL values, however, explicit_bzero()
can't handle that, as indicated by the `__nonnull ((1))` attribute in the
function's signature in string.h, and causes a segmentation fault.  This
was noticed in one of the unit tests for NewHope.  Since we usually use
memwipe() via chunk_clear(), which already ignores NULL pointers, this
is not that much of an issue in practice.

Fixes: 149d1bbb055a ("memory: Use explicit_bzero() as memwipe() if available")

src/libstrongswan/utils/utils/memory.h
@@ -87,7 +87,13 @@ static inline void *memset_noop(void *s, int c, size_t n)
 void memxor(uint8_t dest[], const uint8_t src[], size_t n);
 
 #ifdef HAVE_EXPLICIT_BZERO
-#define memwipe(ptr, n) explicit_bzero(ptr, n)
+static inline void memwipe(void *ptr, size_t n)
+{
+	if (ptr)
+	{
+		explicit_bzero(ptr, n);
+	}
+}
 #else /* HAVE_EXPLICIT_BZERO */
 /**
  * Safely overwrite n bytes of memory at ptr with zero, non-inlining variant.

src/libstrongswan/tests/suites/test_utils.c
@@ -548,6 +548,63 @@ START_TEST(test_memstr)
 END_TEST
 
 /*******************************************************************************
+ * memwipe
+ */
+
+START_TEST(test_memwipe_null)
+{
+	memwipe(NULL, 16);
+}
+END_TEST
+
+static inline bool test_wiped_memory(char *buf, size_t len)
+{
+	int i;
+
+	/* comparing two bytes at once reduces the chances of conflicts if memory
+	 * got overwritten already */
+	for (i = 0; i < len; i += 2)
+	{
+		if (buf[i] != 0 && buf[i] == i &&
+			buf[i+1] != 0 && buf[i+1] == i+1)
+		{
+			return FALSE;
+		}
+	}
+	return TRUE;
+}
+
+START_TEST(test_memwipe_stack)
+{
+	char buf[64];
+	int i;
+
+	for (i = 0; i < sizeof(buf); i++)
+	{
+		buf[i] = i;
+	}
+	memwipe(buf, sizeof(buf));
+	ck_assert(test_wiped_memory(buf, sizeof(buf)));
+}
+END_TEST
+
+START_TEST(test_memwipe_heap)
+{
+	size_t len = 64;
+	char *buf = malloc(len);
+	int i;
+
+	for (i = 0; i < len; i++)
+	{
+		buf[i] = i;
+	}
+	memwipe(buf, len);
+	ck_assert(test_wiped_memory(buf, len));
+	free(buf);
+}
+END_TEST
+
+/*******************************************************************************
  * utils_memrchr
  */
 
@@ -1130,6 +1187,12 @@ Suite *utils_suite_create()
 
 	tc = tcase_create("memstr");
 	tcase_add_loop_test(tc, test_memstr, 0, countof(memstr_data));
+	suite_add_tcase(s, tc);
+
+	tc = tcase_create("memwipe");
+	tcase_add_test(tc, test_memwipe_null);
+	tcase_add_test(tc, test_memwipe_stack);
+	tcase_add_test(tc, test_memwipe_heap);
 	suite_add_tcase(s, tc);
 
 	tc = tcase_create("utils_memrchr");