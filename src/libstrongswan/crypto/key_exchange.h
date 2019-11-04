fd915c5703632d807af06f5bb97a6c57a60bc6e8
Andreas Steffen <andreas.steffen@strongswan.org>
2019-11-04 22:22:47 +0100

    key-exchange: Joint ke_test_vector format for DH and KEM
    Both Diffie-Hellman (DH) and Key Encapsulation Mechanism (KEM) based
    key exchange methods use a common ke_test_vector format. The
    set_seed() function is used to provide deterministic private key
    material for the crypto tests.

--- src/libstrongswan/crypto/key_exchange.h
+++ src/libstrongswan/crypto/key_exchange.h
@@ -1,4 +1,5 @@
 /*
+ * Copyright (C) 2016-2019 Andreas Steffen
  * Copyright (C) 2010-2020 Tobias Brunner
  * Copyright (C) 2005-2007 Martin Willi
  * Copyright (C) 2005 Jan Hutter
@@ -161,13 +162,14 @@ struct key_exchange_t {
 	 * Set a seed used for the derivation of private key material.
 	 *
 	 * Calling this method is usually not required, as the key exchange objects
-	 * generate the private key material themselves. This is optional to implement, and
-	 * used mostly for testing purposes.  The private key may be the actual key
-	 * or a seed for a DRBG.
+	 * generate the private key material themselves. This is optional to
+	 * implement, and used mostly for testing purposes.  The private key may be
+	 * the actual key or a DRBG instance.
 	 *
-	 * @param value		private key value to set
+	 * @param value		optional seed value to set (can be chunk_empty)
+	 * @param drbg		optional DRBG (can be NULL)
 	 */
-	bool (*set_private_key)(key_exchange_t *this, chunk_t value)
+	bool (*set_seed)(key_exchange_t *this, chunk_t value, drbg_t *drbg)
 		__attribute__((warn_unused_result));
 
 	/**
