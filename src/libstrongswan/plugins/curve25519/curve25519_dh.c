5a6850325a3406d9ba93e9728159539c51abfd02
Andreas Steffen <andreas.steffen@strongswan.org>
2019-11-04 22:22:47 +0100

    key-exchange: Joint ke_test_vector format for DH and KEM
    Both Diffie-Hellman (DH) and Key Encapsulation Mechanism (KEM) based
    key exchange methods use a common ke_test_vector format. The
    set_seed() function is used to provide deterministic private key
    material for the crypto tests.

--- src/libstrongswan/plugins/curve25519/curve25519_dh.c
+++ src/libstrongswan/plugins/curve25519/curve25519_dh.c
@@ -100,8 +100,8 @@ METHOD(key_exchange_t, get_public_key, bool,
 	return FALSE;
 }
 
-METHOD(key_exchange_t, set_private_key, bool,
-	private_curve25519_dh_t *this, chunk_t value)
+METHOD(key_exchange_t, set_seed, bool,
+	private_curve25519_dh_t *this, chunk_t value, drbg_t *drbg)
 {
 	if (value.len != CURVE25519_KEY_SIZE)
 	{
@@ -152,7 +152,7 @@ curve25519_dh_t *curve25519_dh_create(key_exchange_method_t group)
 				.get_shared_secret = _get_shared_secret,
 				.set_public_key = _set_public_key,
 				.get_public_key = _get_public_key,
-				.set_private_key = _set_private_key,
+				.set_seed = _set_seed,
 				.get_method = _get_method,
 				.destroy = _destroy,
 			},
