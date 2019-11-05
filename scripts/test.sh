bb47c4835158c8941562b438edddf5eb7e148998
Andreas Steffen <andreas.steffen@strongswan.org>
2019-11-05 21:52:20 +0100

    oqs: Add ML-KEM implementation based on liboqs

--- scripts/test.sh
+++ scripts/test.sh
@@ -286,7 +286,7 @@ all|alpine|codeql|coverage|sonarcloud|no-dbg|no-testable-ke)
 			--disable-kernel-pfroute --disable-keychain
 			--disable-lock-profiler --disable-padlock --disable-fuzzing
 			--disable-osx-attr --disable-tkm
-			--disable-unwind-backtraces
+			--disable-oqs --disable-unwind-backtraces
 			--disable-svc --disable-dbghelp-backtraces --disable-socket-win
 			--disable-kernel-wfp --disable-kernel-iph --disable-winhttp"
 	# not enabled on the build server
