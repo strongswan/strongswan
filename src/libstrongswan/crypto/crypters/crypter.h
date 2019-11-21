20f3d04b1314ae11b03fcd8e0b4d501747968867
Andreas Steffen <andreas.steffen@strongswan.org>
2019-11-21 12:58:48 +0100

    aesni: Added AES_ECB support

--- src/libstrongswan/crypto/crypters/crypter.h
+++ src/libstrongswan/crypto/crypters/crypter.h
@@ -64,6 +64,7 @@ enum encryption_algorithm_t {
 	ENCR_TWOFISH_CBC =      1027,
 	/* see macros below to handle RC2 (effective) key length */
 	ENCR_RC2_CBC =          1028,
+	ENCR_AES_ECB =			1029,
 };
 
 #define DES_BLOCK_SIZE			 8
