strongswan/strongswan

commit 7533cedb9a8a099d291f9f1f2e306b44b200db67

libtpmtss: Read RSA public key exponent instead of assuming its value
Up to now it was assumed that the RSA public key exponent is equal to 2^16+1.
Although this is probably true in most if not all cases, it is not correct
according to the TPM 2.0 specification.

This patch fixes that by reading the exponent from the structure returned
by TPM2_ReadPublic.

Closes strongswan/strongswan#121.

src/libtpmtss/tpm_tss_tss2_v1.c
@@ -481,6 +481,7 @@ METHOD(tpm_tss_t, get_public, chunk_t,
 			TPM2B_PUBLIC_KEY_RSA *rsa;
 			TPMT_RSA_SCHEME *scheme;
 			chunk_t aik_exponent, aik_modulus;
+			uint32_t exponent;
 
 			scheme = &public.t.publicArea.parameters.rsaDetail.scheme;
 			sig_alg   = scheme->scheme;
@@ -488,7 +489,15 @@ METHOD(tpm_tss_t, get_public, chunk_t,
 
 			rsa = &public.t.publicArea.unique.rsa;
 			aik_modulus = chunk_create(rsa->t.buffer, rsa->t.size);
-			aik_exponent = chunk_from_chars(0x01, 0x00, 0x01);
+			exponent = public.t.publicArea.parameters.rsaDetail.exponent;
+			if (!exponent)
+			{
+				aik_exponent = chunk_from_chars(0x01, 0x00, 0x01);
+			}
+			else
+			{
+				aik_exponent = chunk_from_thing(exponent);
+			}
 
 			/* subjectPublicKeyInfo encoding of RSA public key */
 			if (!lib->encoding->encode(lib->encoding, PUBKEY_SPKI_ASN1_DER,

src/libtpmtss/tpm_tss_tss2_v2.c
@@ -435,6 +435,7 @@ METHOD(tpm_tss_t, get_public, chunk_t,
 			TPM2B_PUBLIC_KEY_RSA *rsa;
 			TPMT_RSA_SCHEME *scheme;
 			chunk_t aik_exponent, aik_modulus;
+			uint32_t exponent;
 
 			scheme = &public.publicArea.parameters.rsaDetail.scheme;
 			sig_alg   = scheme->scheme;
@@ -442,7 +443,15 @@ METHOD(tpm_tss_t, get_public, chunk_t,
 
 			rsa = &public.publicArea.unique.rsa;
 			aik_modulus = chunk_create(rsa->buffer, rsa->size);
-			aik_exponent = chunk_from_chars(0x01, 0x00, 0x01);
+			exponent = public.publicArea.parameters.rsaDetail.exponent;
+			if (!exponent)
+			{
+				aik_exponent = chunk_from_chars(0x01, 0x00, 0x01);
+			}
+			else
+			{
+				aik_exponent = chunk_from_thing(exponent);
+			}
 
 			/* subjectPublicKeyInfo encoding of RSA public key */
 			if (!lib->encoding->encode(lib->encoding, PUBKEY_SPKI_ASN1_DER,

commit 8ee1242f14383d56d9d982edcaafea3392232e97

libtpmtss: Convert RSA exponent to big-endian when provided by a TPM 2.0
While the TPM expects and returns the data in big-endian, the SAPI
implementation converts it to native-endianness.  As stated in the
SAPI specification (section 3.2):

  8. All SAPI data SHALL be in native-endian format.  This means that
     the SAPI implementation will do any endian conversion required for
     both inputs and outputs.

So to use the exponent in a chunk we have to convert it to big-endian again.

Fixes: 7533cedb9a8a ("libtpmtss: Read RSA public key exponent instead of assuming its value")

src/libtpmtss/tpm_tss_tss2_v1.c
@@ -503,7 +503,7 @@ METHOD(tpm_tss_t, get_public, chunk_t,
 
 			rsa = &public.t.publicArea.unique.rsa;
 			aik_modulus = chunk_create(rsa->t.buffer, rsa->t.size);
-			exponent = public.t.publicArea.parameters.rsaDetail.exponent;
+			exponent = htonl(public.t.publicArea.parameters.rsaDetail.exponent);
 			if (!exponent)
 			{
 				aik_exponent = chunk_from_chars(0x01, 0x00, 0x01);

src/libtpmtss/tpm_tss_tss2_v2.c
@@ -457,7 +457,7 @@ METHOD(tpm_tss_t, get_public, chunk_t,
 
 			rsa = &public.publicArea.unique.rsa;
 			aik_modulus = chunk_create(rsa->buffer, rsa->size);
-			exponent = public.publicArea.parameters.rsaDetail.exponent;
+			exponent = htonl(public.publicArea.parameters.rsaDetail.exponent);
 			if (!exponent)
 			{
 				aik_exponent = chunk_from_chars(0x01, 0x00, 0x01);