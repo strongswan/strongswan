strongswan/strongswan

commit 0184a69b7b14b91d13d30906b6e3da2ab155c608

child-create: Properly handle DH group during migration when reestablishing
If such a task was active while reestablishing it will get queued on the
new IKE_SA.  If the DH group is already set, the DH groups won't be
stripped from the proposals and a KE payload will be sent, which is invalid
during IKE_AUTH.  We don't want to reset the group if the task is part of a
child-rekey task.

src/libcharon/sa/ikev2/tasks/child_create.c
@@ -1853,7 +1853,10 @@ METHOD(task_t, migrate, void,
 	{
 		this->proposals->destroy_offset(this->proposals, offsetof(proposal_t, destroy));
 	}
-
+	if (!this->rekey)
+	{
+		this->dh_group = MODP_NONE;
+	}
 	this->ike_sa = ike_sa;
 	this->keymat = (keymat_v2_t*)ike_sa->get_keymat(ike_sa);
 	this->proposal = NULL;

commit 59ebdac49bec9374080b2f1a0027ea6a26dece2f

child-create: Don't reset DH group when retrying after INVALID_KE_PAYLOAD
migrate() is called before retrying.

Fixes: 0184a69b7b14 ("child-create: Properly handle DH group during
migration when reestablishing")

src/libcharon/sa/ikev2/tasks/child_create.c
@@ -1853,7 +1853,7 @@ METHOD(task_t, migrate, void,
 	{
 		this->proposals->destroy_offset(this->proposals, offsetof(proposal_t, destroy));
 	}
-	if (!this->rekey)
+	if (!this->rekey && !this->retry)
 	{
 		this->dh_group = MODP_NONE;
 	}