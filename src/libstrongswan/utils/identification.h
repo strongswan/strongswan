810033167b0d0d0c1708f2786a2660017db0ce0c
Коренберг Марк <socketpair@gmail.com>
2020-07-15 13:25:56 +0500

    Drop S, SN===SURNAME, seialNumber=SERIAL_NUMBER
    https://tools.ietf.org/html/rfc4519#section-2.31

--- src/libstrongswan/utils/identification.h
+++ src/libstrongswan/utils/identification.h
@@ -183,9 +183,9 @@ enum id_part_t {
 	/** OrganizationUnit RDN of a DN */
 	ID_PART_RDN_OU,
 	/** Surname RDN of a DN */
-	ID_PART_RDN_S,
-	/** SerialNumber RDN of a DN */
 	ID_PART_RDN_SN,
+	/** SerialNumber RDN of a DN */
+	ID_PART_RDN_SERIAL_NUMBER,
 	/** StateOrProvince RDN of a DN */
 	ID_PART_RDN_ST,
 	/** Title RDN of a DN */
