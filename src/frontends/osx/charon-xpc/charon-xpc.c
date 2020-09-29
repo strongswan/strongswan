040608a48d03b53ee31556d06554b17bfb9811fa
Tobias Brunner <tobias@strongswan.org>
2020-09-29 15:38:24 +0200

    Simplify handling of terminating signals in all libcharon wrappers
    Closes strongswan/strongswan#182.

--- src/frontends/osx/charon-xpc/charon-xpc.c
+++ src/frontends/osx/charon-xpc/charon-xpc.c
@@ -92,11 +92,9 @@ static int run()
 		switch (sig)
 		{
 			case SIGINT:
-				DBG1(DBG_DMN, "signal of type SIGINT received. Shutting down");
-				charon->bus->alert(charon->bus, ALERT_SHUTDOWN_SIGNAL, sig);
-				return 0;
 			case SIGTERM:
-				DBG1(DBG_DMN, "signal of type SIGTERM received. Shutting down");
+				DBG1(DBG_DMN, "%s received, shutting down",
+					 sig == SIGINT ? "SIGINT" : "SIGTERM");
 				charon->bus->alert(charon->bus, ALERT_SHUTDOWN_SIGNAL, sig);
 				return 0;
 		}
