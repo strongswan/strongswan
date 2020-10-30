87c563a16412d924ca8718ecbd8b3dab59f602d8
Pascal Knecht <pascal.knecht@hsr.ch>
2020-10-30 15:15:30 +0100

    tls-server: Mutual authentication support for TLS 1.3
    This commit also addresses the side effect that additional messages have
    an influence on the derivation of the application traffic secrets. Therefore,
    key derivation is relocated after the server finished message has been sent,
    so the additional messages from the client (Certificate, CertificateVerify)
    don't affect the key derivation. Only the outbound key is switched there, the
    inbound key remains in use until the client's finished message has been
    processed.

--- src/libtls/tests/suites/test_socket.c
+++ src/libtls/tests/suites/test_socket.c
@@ -664,12 +664,6 @@ START_TEST(test_tls13)
 }
 END_TEST
 
-START_TEST(test_tls13_mutual)
-{
-	test_tls(TLS_1_3, 5670, TRUE, _i);
-}
-END_TEST
-
 START_TEST(test_tls12)
 {
 	test_tls(TLS_1_2, 5671, FALSE, _i);
@@ -744,11 +738,6 @@ Suite *socket_suite_create()
 	tc = tcase_create("TLS 1.3/anon");
 	tcase_add_checked_fixture(tc, setup_creds, teardown_creds);
 	add_tls_test(test_tls13, TLS_1_3);
-	suite_add_tcase(s, tc);
-
-	tc = tcase_create("TLS 1.3/mutl");
-	tcase_add_checked_fixture(tc, setup_creds, teardown_creds);
-	add_tls_test(test_tls13_mutual, TLS_1_3);
 	suite_add_tcase(s, tc);
 
 	tc = tcase_create("TLS 1.2/anon");
