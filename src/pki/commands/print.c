d14b0a970c62d1107c550e3a38d33bc724f67976
Andreas Steffen <andreas.steffen@strongswan.org>
2020-11-16 20:22:21 +0100

    bliss: Removed legacy BLISS signatures

--- src/pki/commands/print.c
+++ src/pki/commands/print.c
@@ -119,12 +119,6 @@ static int print()
 					type = CRED_PRIVATE_KEY;
 					subtype = KEY_ED448;
 				}
-				else if (streq(arg, "bliss") ||
-						 streq(arg, "bliss-priv"))
-				{
-					type = CRED_PRIVATE_KEY;
-					subtype = KEY_BLISS;
-				}
 				else
 				{
 					return command_usage( "invalid input type");
@@ -203,7 +197,7 @@ static void __attribute__ ((constructor))reg()
 		{ print, 'a', "print",
 		"print a credential in a human readable form",
 		{"[--in file|--keyid hex]",
-		 "[--type x509|crl|ac|pub|priv|rsa|ecdsa|ed25519|ed448|bliss]"},
+		 "[--type x509|crl|ac|pub|priv|rsa|ecdsa|ed25519|ed448]"},
 		{
 			{"help",	'h', 0, "show usage information"},
 			{"in",		'i', 1, "input file, default: stdin"},
