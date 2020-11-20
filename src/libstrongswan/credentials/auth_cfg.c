9c6a50eae8016e0d332465f9ae76fef728f169d8
Andreas Steffen <andreas.steffen@strongswan.org>
2020-11-20 17:30:15 +0100

    oqs: Support of Falcon signature algorithms

--- src/libstrongswan/credentials/auth_cfg.c
+++ src/libstrongswan/credentials/auth_cfg.c
@@ -601,6 +601,8 @@ METHOD(auth_cfg_t, add_pubkey_constraints, void,
 			{ "identity",	SIGN_DILITHIUM_2,				KEY_DILITHIUM_2, },
 			{ "identity",	SIGN_DILITHIUM_3,				KEY_DILITHIUM_3, },
 			{ "identity",	SIGN_DILITHIUM_4,				KEY_DILITHIUM_4, },
+			{ "identity",	SIGN_FALCON_512,				KEY_FALCON_512,  },
+			{ "identity",	SIGN_FALCON_1024,				KEY_FALCON_1024, },
 		};
 
 		if (expected_strength != AUTH_RULE_MAX)
