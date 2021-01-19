1af4ae8732890e818d3f8ad5d3863bc1948aef5d
Tobias Brunner <tobias@strongswan.org>
2021-01-19 17:03:56 +0100

    cirrus: Build farp plugin on FreeBSD

--- scripts/test.sh
+++ scripts/test.sh
@@ -253,7 +253,8 @@ freebsd)
 			--enable-load-tester --enable-ldap --enable-mediation
 			--enable-mysql --enable-sqlite --enable-tpm --enable-tss-tss2
 			--enable-unbound --enable-unity --enable-xauth-eap --enable-xauth-pam
-			--with-printf-hooks=builtin --enable-attr-sql --enable-sql"
+			--with-printf-hooks=builtin --enable-attr-sql --enable-sql
+			--enable-farp"
 	DEPS="git gmp openldap-client libxml2 mysql80-client sqlite3 unbound ldns tpm2-tss"
 	export GPERF=/usr/local/bin/gperf
 	export LEX=/usr/local/bin/flex
