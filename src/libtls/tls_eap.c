strongswan/strongswan

commit 083f38259c79ee5d1ac443bbfd0146c1fa03978e

tls-eap: Conclude EAP method also after processing packets
With TLS 1.3, the server sends its Finished message first, so the
session is complete after processing the client's Finished message,
without having to send anything else (in particular no acknowledgement
as the last message from the client is no fragment).

src/libtls/tls_eap.c
@@ -394,6 +394,10 @@ METHOD(tls_eap_t, process, status_t,
 	switch (status)
 	{
 		case INVALID_STATE:
+			if (this->tls->is_complete(this->tls))
+			{
+				return SUCCESS;
+			}
 			*out = create_ack(this);
 			return NEED_MORE;
 		case FAILED:

commit 024120f8ea3e3faab4d87a2f083c3da16e15b91a

tls-eap: Only servers conclude EAP method after processing packets
As client with older TLS versions, we have to ack the receipt of the server's
Finished message instead.

Fixes: 083f38259c79 ("tls-eap: Conclude EAP method also after processing packets")

src/libtls/tls_eap.c
@@ -394,7 +394,7 @@ METHOD(tls_eap_t, process, status_t,
 	switch (status)
 	{
 		case INVALID_STATE:
-			if (this->tls->is_complete(this->tls))
+			if (this->is_server && this->tls->is_complete(this->tls))
 			{
 				return SUCCESS;
 			}