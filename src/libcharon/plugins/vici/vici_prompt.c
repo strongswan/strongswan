5df6279618835cf6be18b24a98e6e78bc0c8d4a0
Noel Kuntze <noel.kuntze@thermi.consulting>
2021-02-25 17:44:23 +0100

    vici: slight lint

--- src/libcharon/plugins/vici/vici_prompt.c
+++ src/libcharon/plugins/vici/vici_prompt.c
@@ -116,9 +116,7 @@ typedef struct {
 /* Subtract the `struct timeval' values X and Y,
    storing the result in RESULT.
    Return 1 if the difference is negative, otherwise 0.  */
-
-int
-timeval_subtract (struct timeval *result, struct timeval *x, struct timeval *y)
+int timeval_subtract (struct timeval *result, struct timeval *x, struct timeval *y)
 {
 	/* Perform the carry for the later subtraction by updating @var{y}. */
 	if (x->tv_usec < y->tv_usec) {
