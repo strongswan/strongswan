d73ca81685bf39c9cd98e067aed7a0f9ed9e3a69
Andreas Steffen <andreas.steffen@strongswan.org>
2021-03-21 12:10:40 +0100

    oqs: Upgraded Dilithium to NIST Round 3.1

--- src/libstrongswan/crypto/hashers/hasher.c
+++ src/libstrongswan/crypto/hashers/hasher.c
@@ -158,7 +158,7 @@ hash_algorithm_t hasher_algorithm_from_oid(int oid)
 		case OID_ED448:
 		case OID_DILITHIUM_2:
 		case OID_DILITHIUM_3:
-		case OID_DILITHIUM_4:
+		case OID_DILITHIUM_5:
 		case OID_FALCON_512:
 		case OID_FALCON_1024:
 			return HASH_IDENTITY;
@@ -498,11 +498,11 @@ int hasher_signature_algorithm_to_oid(hash_algorithm_t alg, key_type_t key)
 				default:
 					return OID_UNKNOWN;
 			}
-		case KEY_DILITHIUM_4:
+		case KEY_DILITHIUM_5:
 			switch (alg)
 			{
 				case HASH_IDENTITY:
-					return OID_DILITHIUM_4;
+					return OID_DILITHIUM_5;
 				default:
 					return OID_UNKNOWN;
 			}
@@ -550,7 +550,7 @@ hash_algorithm_t hasher_from_signature_scheme(signature_scheme_t scheme,
 		case SIGN_ED448:
 		case SIGN_DILITHIUM_2:
 		case SIGN_DILITHIUM_3:
-		case SIGN_DILITHIUM_4:
+		case SIGN_DILITHIUM_5:
 		case SIGN_FALCON_512:
 		case SIGN_FALCON_1024:
 			return HASH_IDENTITY;
