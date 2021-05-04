strongswan/strongswan

commit bd323ae6c8325812b95a0b61598a35940b06bfb1

openssl: Migrate from deprecated EC_POINT_[set|get]_affine_coordinates_GFp() functions

src/libstrongswan/plugins/openssl/openssl_ec_diffie_hellman.c
@@ -506,7 +506,11 @@ EC_GROUP *ec_group_new_brainpool(bp_curve *curve)
 		goto failed;
 	}
 	G = EC_POINT_new(group);
+#ifdef OPENSSL_VERSION_NUMBER >= 0x1010100fL
+	if (!G || !EC_POINT_set_affine_coordinates(group, G, x, y, ctx))
+#else
 	if (!G || !EC_POINT_set_affine_coordinates_GFp(group, G, x, y, ctx))
+#endif
 	{
 		goto failed;
 	}

commit 1de13f9037a8209364289f5e06f54898d1afa95f

openssl: Fix OpenSSL version check for EC_POINT_set_affine_coordinates
Fixes: bd323ae6c832 ("openssl: Migrate from deprecated EC_POINT_[set|get]_affine_coordinates_GFp() functions")
Closes strongswan/strongswan#332

src/libstrongswan/plugins/openssl/openssl_ec_diffie_hellman.c
@@ -506,7 +506,7 @@ EC_GROUP *ec_group_new_brainpool(bp_curve *curve)
 		goto failed;
 	}
 	G = EC_POINT_new(group);
-#ifdef OPENSSL_VERSION_NUMBER >= 0x1010100fL
+#if OPENSSL_VERSION_NUMBER >= 0x1010100fL
 	if (!G || !EC_POINT_set_affine_coordinates(group, G, x, y, ctx))
 #else
 	if (!G || !EC_POINT_set_affine_coordinates_GFp(group, G, x, y, ctx))