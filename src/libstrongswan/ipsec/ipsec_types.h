12395cedf39f3f909a5d92e4ce42d84cc3f790d2
Tobias Brunner <tobias@strongswan.org>
2021-05-10 16:28:15 +0200

    ipsec-types: Add identifier for the maximum (=no) CPU ID

--- src/libstrongswan/ipsec/ipsec_types.h
+++ src/libstrongswan/ipsec/ipsec_types.h
@@ -278,4 +278,9 @@ bool if_id_from_string(const char *value, uint32_t *if_id);
  */
 void allocate_unique_if_ids(uint32_t *in, uint32_t *out);
 
+/**
+ * Maximum CPU ID (used if no CPU ID specified).
+ */
+#define CPU_ID_MAX UINT32_MAX
+
 #endif /** IPSEC_TYPES_H_ @}*/
