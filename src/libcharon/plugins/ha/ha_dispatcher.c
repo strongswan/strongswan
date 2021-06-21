strongswan/strongswan

commit 20dfbcad0875fc0b3fa2be1478d3b5adedee8e92

ha: Register new IKE_SAs before calling inherit_post()

src/libcharon/plugins/ha/ha_dispatcher.c
@@ -259,7 +259,10 @@ static void process_ike_add(private_ha_dispatcher_t *this, ha_message_t *message
 		if (ok)
 		{
 			if (old_sa)
-			{
+			{	/* register IKE_SA before calling inherit_post() so no scheduled
+				 * jobs are lost */
+				charon->ike_sa_manager->checkout_new(charon->ike_sa_manager,
+													 old_sa);
 				ike_sa->inherit_pre(ike_sa, old_sa);
 				ike_sa->inherit_post(ike_sa, old_sa);
 				charon->ike_sa_manager->checkin_and_destroy(

commit 79b526deba692155ca3b2761b59ad17f35728320

ha: Register the correct IKE_SA with the manager after a rekeying
Fixes: 20dfbcad0875 ("ha: Register new IKE_SAs before calling inherit_post()")
Closes strongswan/strongswan#456.

src/libcharon/plugins/ha/ha_dispatcher.c
@@ -262,7 +262,7 @@ static void process_ike_add(private_ha_dispatcher_t *this, ha_message_t *message
 			{	/* register IKE_SA before calling inherit_post() so no scheduled
 				 * jobs are lost */
 				charon->ike_sa_manager->checkout_new(charon->ike_sa_manager,
-													 old_sa);
+													 ike_sa);
 				ike_sa->inherit_pre(ike_sa, old_sa);
 				ike_sa->inherit_post(ike_sa, old_sa);
 				charon->ike_sa_manager->checkin_and_destroy(