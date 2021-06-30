2cd5314de7507737df55e7ce380d66c25eb489e8
Tobias Brunner <tobias@strongswan.org>
2021-06-30 15:16:47 +0200

    testing: Use specific versions of swidGenerator and strongTNC
    This way we get updated versions automatically (referencing "master"
    required manually deleting the downloaded archives and the unpacked
    directories).  It also allows switching versions when working in different
    branches (note that REV can also be set to a commit ID, e.g. to test
    changes before tagging them later and merging the branch).

--- testing/scripts/recipes/015_strongTNC.mk
+++ testing/scripts/recipes/015_strongTNC.mk
@@ -1,8 +1,10 @@
 #!/usr/bin/make
 
 PKG = strongTNC
-ZIP = $(PKG)-master.zip
-SRC = https://github.com/strongswan/$(PKG)/archive/master.zip
+REV = 0.9.9
+DIR = $(PKG)-$(REV)
+ZIP = $(PKG)-$(REV).zip
+SRC = https://github.com/strongswan/$(PKG)/archive/$(REV).zip
 DEPS = $(PKG)-deps
 
 all: install
@@ -10,14 +12,16 @@ all: install
 $(ZIP):
 	wget --ca-directory=/usr/share/ca-certificates/mozilla/ $(SRC) -O $(ZIP)
 
-$(PKG)-master: $(ZIP)
-	unzip -u $(ZIP)
+.$(PKG)-unpacked-$(REV): $(ZIP)
+	[ -d $(DIR) ] || unzip $(ZIP)
+	@touch $@
 
-$(DEPS): $(PKG)-master
+.$(PKG)-deps-$(REV): .$(PKG)-unpacked-$(REV)
 	mkdir -p $(DEPS)
-	pip download -d $(DEPS) -r $(PKG)-master/requirements.txt six
+	pip download -d $(DEPS) -r $(DIR)/requirements.txt six
+	@touch $@
 
-install: $(DEPS)
+install: .$(PKG)-deps-$(REV)
 	# six is required for djangorestframework-camel-case
-	pip install --no-index --find-links=file://`pwd`/$(DEPS) -r $(PKG)-master/requirements.txt six
-	cp -r $(PKG)-master /var/www/tnc && chgrp -R www-data /var/www/tnc && chmod g+sw /var/www/tnc
+	pip install --no-index --find-links=file://`pwd`/$(DEPS) -r $(DIR)/requirements.txt six
+	cp -r $(DIR) /var/www/tnc && chgrp -R www-data /var/www/tnc && chmod g+sw /var/www/tnc
