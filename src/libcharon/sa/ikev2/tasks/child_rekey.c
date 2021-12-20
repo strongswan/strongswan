92f54e55e98a3adb52ef556e84e033c831ce70ac
Tobias Brunner <tobias@strongswan.org>
2021-12-20 16:10:19 +0100

    child-rekey: Maintain security label during rekeying

--- src/libcharon/sa/ikev2/tasks/child_rekey.c
+++ src/libcharon/sa/ikev2/tasks/child_rekey.c
@@ -210,6 +210,8 @@ METHOD(task_t, build_i, status_t,
 	this->child_create->use_if_ids(this->child_create,
 						this->child_sa->get_if_id(this->child_sa, TRUE),
 						this->child_sa->get_if_id(this->child_sa, FALSE));
+	this->child_create->use_label(this->child_create,
+						this->child_sa->get_label(this->child_sa));
 
 	if (this->child_create->task.build(&this->child_create->task,
 									   message) != NEED_MORE)
@@ -272,6 +274,8 @@ METHOD(task_t, build_r, status_t,
 	this->child_create->use_if_ids(this->child_create,
 						this->child_sa->get_if_id(this->child_sa, TRUE),
 						this->child_sa->get_if_id(this->child_sa, FALSE));
+	this->child_create->use_label(this->child_create,
+						this->child_sa->get_label(this->child_sa));
 	config = this->child_sa->get_config(this->child_sa);
 	this->child_create->set_config(this->child_create, config->get_ref(config));
 	this->child_create->task.build(&this->child_create->task, message);
