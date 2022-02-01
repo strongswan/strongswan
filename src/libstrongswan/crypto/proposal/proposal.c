strongswan/strongswan

commit c7bef954eec6953c87f7a336fd0c6e74c1b1ede2

proposal: Add AES-GCM to the ESP default AEAD proposal
References #3461.

src/libstrongswan/crypto/proposal/proposal.c
@@ -1298,9 +1298,13 @@ proposal_t *proposal_create_default_aead(protocol_id_t protocol)
 			}
 			return &this->public;
 		case PROTO_ESP:
-			/* we currently don't include any AEAD proposal for ESP, as we
-			 * don't know if our kernel backend actually supports it. */
-			return NULL;
+			/* AES-GCM should be supported by pretty much all current kernels,
+			 * RFC 8221 even made it mandatory */
+			this = (private_proposal_t*)proposal_create(protocol, 0);
+			add_algorithm(this, ENCRYPTION_ALGORITHM, ENCR_AES_GCM_ICV16, 128);
+			add_algorithm(this, ENCRYPTION_ALGORITHM, ENCR_AES_GCM_ICV16, 192);
+			add_algorithm(this, ENCRYPTION_ALGORITHM, ENCR_AES_GCM_ICV16, 256);
+			return &this->public;
 		case PROTO_AH:
 		default:
 			return NULL;

commit 23b6746d3701ff91e36da3b6d78aad5723bfd3ec

proposal: Add ESN transform to default ESP AEAD proposal
The commit mentioned below adds an AES-GCM default proposal for ESP. That
proposal does not include any ESN or non-ESN transform to indicate if extended
sequence numbers are supported.

A standards-compliant peer will include one or more ESN support transforms,
and will be unable to select this proposal due to a proposal mismatch.

Fix the default AES-GCM proposal by adding a NO_ESN algorithm. While ESN is
supported in the Linux kernel for a while, having it in the default proposal
can be problematic with kernel-ipsec or on other systems.

Fixes: c7bef954eec6 ("proposal: Add AES-GCM to the ESP default AEAD proposal")

src/libstrongswan/crypto/proposal/proposal.c
@@ -1304,6 +1304,7 @@ proposal_t *proposal_create_default_aead(protocol_id_t protocol)
 			add_algorithm(this, ENCRYPTION_ALGORITHM, ENCR_AES_GCM_ICV16, 128);
 			add_algorithm(this, ENCRYPTION_ALGORITHM, ENCR_AES_GCM_ICV16, 192);
 			add_algorithm(this, ENCRYPTION_ALGORITHM, ENCR_AES_GCM_ICV16, 256);
+			add_algorithm(this, EXTENDED_SEQUENCE_NUMBERS, NO_EXT_SEQ_NUMBERS, 0);
 			return &this->public;
 		case PROTO_AH:
 		default: