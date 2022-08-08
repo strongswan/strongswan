d848fc1c209c225404ad0285ba549f49dca73685
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-08-08 09:52:19 +0200

    src/libstrongswan/plugins/wolfssl: rename encrypt
    Rename encrypt to strongswan_encrypt to avoid the following build
    failure when wolfssl is built with --enable-opensslextra:
    
    In file included from ../../../../src/libstrongswan/utils/utils.h:59,
                     from ../../../../src/libstrongswan/library.h:101,
                     from wolfssl_common.h:29,
                     from wolfssl_aead.c:23:
    wolfssl_aead.c:90:16: error: conflicting types for 'encrypt'; have '_Bool(union <anonymous>,  chunk_t,  chunk_t,  chunk_t,  chunk_t *)'
       90 | METHOD(aead_t, encrypt, bool,
          |                ^~~~~~~
    ../../../../src/libstrongswan/utils/utils/object.h:99:20: note: in definition of macro 'METHOD'
       99 |         static ret name(union {iface *_public; this;} \
          |                    ^~~~
    In file included from /home/autobuild/autobuild/instance-5/output-1/host/powerpc64le-buildroot-linux-musl/sysroot/usr/include/wolfssl/wolfcrypt/wc_port.h:573,
                     from /home/autobuild/autobuild/instance-5/output-1/host/powerpc64le-buildroot-linux-musl/sysroot/usr/include/wolfssl/wolfcrypt/types.h:35,
                     from /home/autobuild/autobuild/instance-5/output-1/host/powerpc64le-buildroot-linux-musl/sysroot/usr/include/wolfssl/wolfcrypt/logging.h:33,
                     from /home/autobuild/autobuild/instance-5/output-1/host/powerpc64le-buildroot-linux-musl/sysroot/usr/include/wolfssl/ssl.h:35,
                     from wolfssl_common.h:64,
                     from wolfssl_aead.c:23:
    /home/autobuild/autobuild/instance-5/output-1/host/powerpc64le-buildroot-linux-musl/sysroot/usr/include/unistd.h:149:6: note: previous declaration of 'encrypt' with type 'void(char *, int)'
      149 | void encrypt(char *, int);
          |      ^~~~~~~
    
    Fixes:
     - http://autobuild.buildroot.org/results/02f080c2f6d8272cb8cc1de66e058d66fb7499bc
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>

--- src/libstrongswan/plugins/wolfssl/wolfssl_rsa_public_key.c
+++ src/libstrongswan/plugins/wolfssl/wolfssl_rsa_public_key.c
@@ -216,7 +216,7 @@ METHOD(public_key_t, verify, bool,
 	}
 }
 
-METHOD(public_key_t, encrypt, bool,
+METHOD(public_key_t, strongswan_encrypt, bool,
 	private_wolfssl_rsa_public_key_t *this, encryption_scheme_t scheme,
 	void *params, chunk_t plain, chunk_t *crypto)
 {
@@ -440,7 +440,7 @@ static private_wolfssl_rsa_public_key_t *create_empty()
 			.key = {
 				.get_type = _get_type,
 				.verify = _verify,
-				.encrypt = _encrypt,
+				.encrypt = _strongswan_encrypt,
 				.equals = public_key_equals,
 				.get_keysize = _get_keysize,
 				.get_fingerprint = _get_fingerprint,
