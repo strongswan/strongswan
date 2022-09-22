strongswan/strongswan

commit 5f9ad62a8156c543868d1487e5a3d0d425c969ad

socket-dynamic: Don't set SO_REUSEADDR on IKE sockets anymore
Same as the previous commit.

src/libcharon/plugins/socket_dynamic/socket_dynamic_socket.c
@@ -410,12 +410,6 @@ static int open_socket(private_socket_dynamic_socket_t *this,
 		DBG1(DBG_NET, "could not open socket: %s", strerror(errno));
 		return 0;
 	}
-	if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (void*)&on, sizeof(on)) < 0)
-	{
-		DBG1(DBG_NET, "unable to set SO_REUSEADDR on socket: %s", strerror(errno));
-		close(fd);
-		return 0;
-	}
 
 	if (bind(fd, &addr.s, addrlen) < 0)
 	{

commit 03ea02175e41845038ccf8ea905ac03c91cc9027

socket-dynamic: Use IPv6-only mode for IPv6 sockets
Same as the previous commit.

Fixes: 5f9ad62a8156 ("socket-dynamic: Don't set SO_REUSEADDR on IKE sockets anymore")

src/libcharon/plugins/socket_dynamic/socket_dynamic_socket.c
@@ -410,7 +410,13 @@ static int open_socket(private_socket_dynamic_socket_t *this,
 		DBG1(DBG_NET, "could not open socket: %s", strerror(errno));
 		return 0;
 	}
-
+	if (family == AF_INET6 &&
+		setsockopt(fd, SOL_IPV6, IPV6_V6ONLY, &on, sizeof(on)) < 0)
+	{
+		DBG1(DBG_NET, "unable to set IPV6_V6ONLY on socket: %s", strerror(errno));
+		close(fd);
+		return 0;
+	}
 	if (bind(fd, &addr.s, addrlen) < 0)
 	{
 		DBG1(DBG_NET, "unable to bind socket: %s", strerror(errno));