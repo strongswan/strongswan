d42f4367dd6c2b324f81a56f6c99706eb5cebbc9
Tobias Brunner <tobias@strongswan.org>
2022-10-06 12:06:36 +0200

    cirrus: Don't explicitly install openldap*-client on FreeBSD
    This avoids a conflicts as mysql80-client has a dependency on either
    openldap24-client (FreeBSD 12) or openldap26-client (FreeBSD 13) so e.g.
    installing openldap24-client on FreeBSD 13 causes
    
      openldap26-client-2.6.3 conflicts with openldap24-client-2.4.59_4 on /usr/local/bin/ldapadd
    
    Similarly, when installing openldap26-client on FreeBSD 12.  So just let
    the mysql80-client package decide which version is installed.

--- scripts/test.sh
+++ scripts/test.sh
@@ -324,7 +324,7 @@ freebsd)
 			--enable-unbound --enable-unity --enable-xauth-eap --enable-xauth-pam
 			--with-printf-hooks=builtin --enable-attr-sql --enable-sql
 			--enable-farp"
-	DEPS="git gmp openldap24-client libxml2 mysql80-client sqlite3 unbound ldns tpm2-tss"
+	DEPS="git gmp libxml2 mysql80-client sqlite3 unbound ldns tpm2-tss"
 	;;
 fuzzing)
 	CFLAGS="$CFLAGS -DNO_CHECK_MEMWIPE"
