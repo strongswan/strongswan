b3e3836455cb7b3e8979c9cb034e56c1e7770178
Tobias Brunner <tobias@strongswan.org>
2023-10-30 17:34:51 +0100

    library: Add manager for OCSP responders
    Registered OCSP responders should return VALIDATION_SKIPPED for issuer
    certificates they are not responsible for. However, VALIDATION_FAILED is
    currently treated the same way, so that's fine as well.

--- src/libstrongswan/credentials/ocsp_responders.h
+++ src/libstrongswan/credentials/ocsp_responders.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 Andreas Steffen, strongSec GmbH
+ * Copyright (C) 2023 Tobias Brunner
  *
  * Copyright (C) secunet Security Networks AG
  *
@@ -15,26 +15,24 @@
  */
 
 /**
- * @defgroup ocsp_responder ocsp_responder
- * @{ @ingroup certificates
+ * @defgroup ocsp_responders ocsp_responders
+ * @{ @ingroup credentials
  */
 
-#ifndef OCSP_RESPONDER_H_
-#define OCSP_RESPONDER_H_
+#ifndef OCSP_RESPONDERS_H_
+#define OCSP_RESPONDERS_H_
 
-typedef struct ocsp_responder_t ocsp_responder_t;
+typedef struct ocsp_responders_t ocsp_responders_t;
 
-#include <credentials/certificates/crl.h>
+#include <credentials/certificates/ocsp_responder.h>
 
 /**
- * OCSP responder object.
+ * Manages OCSP responders.
  */
-struct ocsp_responder_t {
+struct ocsp_responders_t {
 
 	/**
 	 * Check the status of a certificate given by its serial number
-	 *
-	 * @note Return VALIDATION_SKIPPED if not responsible for the given CA
 	 *
 	 * @param cacert			X.509 certificate of issuer CA
 	 * @param serial_number		serial number of the certificate to be checked
@@ -42,17 +40,37 @@ struct ocsp_responder_t {
 	 * @param reason	        receives reason of revocation, if revoked
 	 * @return					certificate validation status
 	 */
-	cert_validation_t (*get_status)(ocsp_responder_t *this,
+	cert_validation_t (*get_status)(ocsp_responders_t *this,
 									certificate_t *cacert,
 									chunk_t serial_number,
 									time_t *revocation_time,
 									crl_reason_t *revocation_reason);
 
 	/**
-	 * Destroy an ocsp_responder_t object.
+	 * Register an OCSP responder with this manager.
+	 *
+	 * @param responder			OCSP responder to register
 	 */
-	void (*destroy)(ocsp_responder_t *this);
+	void (*add_responder)(ocsp_responders_t *this,
+						  ocsp_responder_t *responder);
 
+	/**
+	 * Unregister an OCSP responder from this manager.
+	 *
+	 * @param responder			OCSP responder to unregister
+	 */
+	void (*remove_responder)(ocsp_responders_t *this,
+							 ocsp_responder_t *responder);
+
+	/**
+	 * Destroy a ocsp_responders_t instance.
+	 */
+	void (*destroy)(ocsp_responders_t *this);
 };
 
-#endif /** OCSP_RESPONDER_H_ @}*/
+/**
+ * Create a ocsp_responders_t instance.
+ */
+ocsp_responders_t *ocsp_responders_create();
+
+#endif /** OCSP_RESPONDERS_H_ @}*/
