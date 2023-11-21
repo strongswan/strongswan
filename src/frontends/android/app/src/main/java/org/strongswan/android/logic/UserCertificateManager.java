110d5d84b9ffc57482963ea64e85da32c40e0964
Markus Pfeiffer <markus.pfeiffer@relution.io>
2023-11-21 15:37:24 +0100

    Add UserCertificateManager
    Add manager that can be used to install, replace or delete currently installed user certificates based on the app's current managed configuration

---
+++ src/frontends/android/app/src/main/java/org/strongswan/android/logic/UserCertificateManager.java
@@ -0,0 +1,73 @@
+package org.strongswan.android.logic;
+
+import android.content.Context;
+
+import org.strongswan.android.data.DatabaseHelper;
+import org.strongswan.android.data.ManagedConfigurationService;
+import org.strongswan.android.data.UserCertificate;
+import org.strongswan.android.data.UserCertificateRepository;
+import org.strongswan.android.utils.Difference;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.core.util.Pair;
+
+public class UserCertificateManager
+{
+	@NonNull
+	private final UserCertificateRepository userCertificateRepository;
+	@NonNull
+	private final UserCertificateInstaller userCertificateInstaller;
+
+	public UserCertificateManager(
+		@NonNull final Context context,
+		@NonNull final ManagedConfigurationService managedConfigurationService,
+		@NonNull final DatabaseHelper databaseHelper)
+	{
+		this.userCertificateRepository = new UserCertificateRepository(managedConfigurationService, databaseHelper);
+		this.userCertificateInstaller = new UserCertificateInstaller(context);
+	}
+
+	public void update()
+	{
+		final List<UserCertificate> configured = userCertificateRepository.getConfiguredKeyStores();
+		final List<UserCertificate> installed = userCertificateRepository.getInstalledKeyStores();
+
+		final Difference<UserCertificate> diff = Difference.between(installed, configured, UserCertificate::getVpnProfileUuid);
+		if (diff.isEmpty())
+		{
+			return;
+		}
+
+		for (final UserCertificate insert : diff.getInserts())
+		{
+			install(insert);
+		}
+
+		for (final Pair<UserCertificate, UserCertificate> update : diff.getUpdates())
+		{
+			remove(update.first);
+			install(update.second);
+		}
+
+		for (final UserCertificate delete : diff.getDeletes())
+		{
+			remove(delete);
+		}
+	}
+
+	private void install(@NonNull final UserCertificate userCertificate)
+	{
+		if (userCertificateInstaller.tryInstall(userCertificate))
+		{
+			userCertificateRepository.addInstalledKeyStore(userCertificate);
+		}
+	}
+
+	private void remove(@NonNull final UserCertificate userCertificate)
+	{
+		userCertificateInstaller.tryRemove(userCertificate);
+		userCertificateRepository.removeInstalledKeyStore(userCertificate);
+	}
+}
