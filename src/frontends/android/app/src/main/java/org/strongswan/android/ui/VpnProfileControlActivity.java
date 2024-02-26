strongswan/strongswan

commit 8e3b921abed7eefcd55e91a42981260f3345a68c

android: Always use UUID to access profiles
Use the UUID rather than the ID to ensure there are no conflicts between
profiles from the database and managed profiles.

src/frontends/android/app/src/main/java/org/strongswan/android/ui/VpnProfileControlActivity.java
@@ -64,7 +64,7 @@ public class VpnProfileControlActivity extends AppCompatActivity
 {
 	public static final String START_PROFILE = "org.strongswan.android.action.START_PROFILE";
 	public static final String DISCONNECT = "org.strongswan.android.action.DISCONNECT";
-	public static final String EXTRA_VPN_PROFILE_ID = "org.strongswan.android.VPN_PROFILE_ID";
+	public static final String EXTRA_VPN_PROFILE_UUID = "org.strongswan.android.VPN_PROFILE_UUID";
 
 	private static final String WAITING_FOR_RESULT = "WAITING_FOR_RESULT";
 	private static final String PROFILE_NAME = "PROFILE_NAME";
@@ -377,18 +377,10 @@ public class VpnProfileControlActivity extends AppCompatActivity
 
 		VpnProfileDataSource dataSource = new VpnProfileSource(this);
 		dataSource.open();
-		String profileUUID = intent.getStringExtra(EXTRA_VPN_PROFILE_ID);
+		String profileUUID = intent.getStringExtra(EXTRA_VPN_PROFILE_UUID);
 		if (profileUUID != null)
 		{
 			profile = dataSource.getVpnProfile(profileUUID);
-		}
-		else
-		{
-			long profileId = intent.getLongExtra(EXTRA_VPN_PROFILE_ID, 0);
-			if (profileId > 0)
-			{
-				profile = dataSource.getVpnProfile(profileId);
-			}
 		}
 		dataSource.close();
 
@@ -414,7 +406,7 @@ public class VpnProfileControlActivity extends AppCompatActivity
 
 		removeFragmentByTag(DIALOG_TAG);
 
-		String profileUUID = intent.getStringExtra(EXTRA_VPN_PROFILE_ID);
+		String profileUUID = intent.getStringExtra(EXTRA_VPN_PROFILE_UUID);
 		if (profileUUID != null)
 		{
 			VpnProfileDataSource dataSource = new VpnProfileSource(this);

src/frontends/android/app/src/main/java/org/strongswan/android/data/VpnProfile.java
@@ -22,6 +22,7 @@ package org.strongswan.android.data;
 import android.text.TextUtils;
 
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.SortedSet;
 import java.util.TreeSet;
 import java.util.UUID;
@@ -339,16 +340,22 @@ public class VpnProfile implements Cloneable
 	@Override
 	public boolean equals(Object o)
 	{
-		if (o != null && o instanceof VpnProfile)
+		if (o == this)
 		{
-			VpnProfile other = (VpnProfile)o;
-			if (this.mUUID != null && other.getUUID() != null)
-			{
-				return this.mUUID.equals(other.getUUID());
-			}
-			return this.mId == other.getId();
+			return true;
 		}
-		return false;
+		if (o == null || getClass() != o.getClass())
+		{
+			return false;
+		}
+		VpnProfile that = (VpnProfile)o;
+		return Objects.equals(mUUID, that.mUUID);
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return Objects.hash(mUUID);
 	}
 
 	@Override

src/frontends/android/app/src/main/java/org/strongswan/android/data/VpnProfileDataSource.java
@@ -90,14 +90,6 @@ public interface VpnProfileDataSource
 	boolean deleteVpnProfile(VpnProfile profile);
 
 	/**
-	 * Get a single VPN profile from the database.
-	 *
-	 * @param id the ID of the VPN profile
-	 * @return the profile or null, if not found
-	 */
-	VpnProfile getVpnProfile(long id);
-
-	/**
 	 * Get a single VPN profile from the database by its UUID.
 	 *
 	 * @param uuid the UUID of the VPN profile

src/frontends/android/app/src/main/java/org/strongswan/android/data/VpnProfileSource.java
@@ -72,20 +72,6 @@ public class VpnProfileSource implements VpnProfileDataSource
 	}
 
 	@Override
-	public VpnProfile getVpnProfile(long id)
-	{
-		for (final VpnProfileDataSource source : dataSources)
-		{
-			final VpnProfile profile = source.getVpnProfile(id);
-			if (profile != null)
-			{
-				return profile;
-			}
-		}
-		return null;
-	}
-
-	@Override
 	public VpnProfile getVpnProfile(UUID uuid)
 	{
 		for (final VpnProfileDataSource source : dataSources)

src/frontends/android/app/src/main/java/org/strongswan/android/data/VpnProfileSqlDataSource.java
@@ -298,30 +298,16 @@ public class VpnProfileSqlDataSource implements VpnProfileDataSource
 	@Override
 	public boolean updateVpnProfile(VpnProfile profile)
 	{
-		long id = profile.getId();
+		final UUID uuid = profile.getUUID();
 		ContentValues values = ContentValuesFromVpnProfile(profile);
-		return mDatabase.update(TABLE_VPNPROFILE, values, KEY_ID + " = " + id, null) > 0;
+		return mDatabase.update(TABLE_VPNPROFILE, values, KEY_UUID + " = ?", new String[]{uuid.toString()}) > 0;
 	}
 
 	@Override
 	public boolean deleteVpnProfile(VpnProfile profile)
 	{
-		long id = profile.getId();
-		return mDatabase.delete(TABLE_VPNPROFILE, KEY_ID + " = " + id, null) > 0;
-	}
-
-	@Override
-	public VpnProfile getVpnProfile(long id)
-	{
-		VpnProfile profile = null;
-		Cursor cursor = mDatabase.query(TABLE_VPNPROFILE, ALL_COLUMNS,
-										KEY_ID + "=" + id, null, null, null, null);
-		if (cursor.moveToFirst())
-		{
-			profile = VpnProfileFromCursor(cursor);
-		}
-		cursor.close();
-		return profile;
+		final UUID uuid = profile.getUUID();
+		return mDatabase.delete(TABLE_VPNPROFILE, KEY_UUID + " = ?", new String[]{uuid.toString()}) > 0;
 	}
 
 	@Override
@@ -358,7 +344,6 @@ public class VpnProfileSqlDataSource implements VpnProfileDataSource
 	private VpnProfile VpnProfileFromCursor(Cursor cursor)
 	{
 		VpnProfile profile = new VpnProfile();
-		profile.setId(cursor.getLong(cursor.getColumnIndexOrThrow(KEY_ID)));
 		profile.setUUID(UUID.fromString(cursor.getString(cursor.getColumnIndexOrThrow(KEY_UUID))));
 		profile.setName(cursor.getString(cursor.getColumnIndexOrThrow(KEY_NAME)));
 		profile.setGateway(cursor.getString(cursor.getColumnIndexOrThrow(KEY_GATEWAY)));

src/frontends/android/app/src/main/java/org/strongswan/android/logic/CharonVpnService.java
@@ -462,7 +462,7 @@ public class CharonVpnService extends VpnService implements Runnable, VpnStateSe
 				Intent intent = new Intent(getApplicationContext(), VpnProfileControlActivity.class);
 				intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 				intent.setAction(VpnProfileControlActivity.START_PROFILE);
-				intent.putExtra(VpnProfileControlActivity.EXTRA_VPN_PROFILE_ID, profile.getUUID().toString());
+				intent.putExtra(VpnProfileControlActivity.EXTRA_VPN_PROFILE_UUID, profile.getUUID().toString());
 				int flags = PendingIntent.FLAG_UPDATE_CURRENT;
 				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
 				{

src/frontends/android/app/src/main/java/org/strongswan/android/logic/VpnStateService.java
@@ -55,11 +55,11 @@ public class VpnStateService extends Service
 	private ErrorState mError = ErrorState.NO_ERROR;
 	private ImcState mImcState = ImcState.UNKNOWN;
 	private final LinkedList<RemediationInstruction> mRemediationInstructions = new LinkedList<RemediationInstruction>();
-	private static long RETRY_INTERVAL = 1000;
+	private static final long RETRY_INTERVAL = 1000;
 	/* cap the retry interval at 2 minutes */
-	private static long MAX_RETRY_INTERVAL = 120000;
-	private static int RETRY_MSG = 1;
-	private RetryTimeoutProvider mTimeoutProvider = new RetryTimeoutProvider();
+	private static final long MAX_RETRY_INTERVAL = 120000;
+	private static final int RETRY_MSG = 1;
+	private final RetryTimeoutProvider mTimeoutProvider = new RetryTimeoutProvider();
 	private long mRetryTimeout;
 	private long mRetryIn;
 
@@ -89,7 +89,7 @@ public class VpnStateService extends Service
 	 */
 	public interface VpnStateListener
 	{
-		public void stateChanged();
+		void stateChanged();
 	}
 
 	/**
@@ -169,6 +169,7 @@ public class VpnStateService extends Service
 
 	/**
 	 * Get the total number of seconds until there is an automatic retry to reconnect.
+	 *
 	 * @return total number of seconds until the retry
 	 */
 	public int getRetryTimeout()
@@ -178,6 +179,7 @@ public class VpnStateService extends Service
 
 	/**
 	 * Get the number of seconds until there is an automatic retry to reconnect.
+	 *
 	 * @return number of seconds until the retry
 	 */
 	public int getRetryIn()
@@ -283,8 +285,9 @@ public class VpnStateService extends Service
 
 	/**
 	 * Connect (or reconnect) a profile
+	 *
 	 * @param profileInfo optional profile info (basically the UUID and password), taken from the
-	 *                    previous profile if null
+	 * previous profile if null
 	 * @param fromScratch true if this is a manual retry/reconnect or a completely new connection
 	 */
 	public void connect(Bundle profileInfo, boolean fromScratch)
@@ -330,7 +333,7 @@ public class VpnStateService extends Service
 				Intent intent = new Intent(this, VpnProfileControlActivity.class);
 				intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 				intent.setAction(VpnProfileControlActivity.START_PROFILE);
-				intent.putExtra(VpnProfileControlActivity.EXTRA_VPN_PROFILE_ID, mProfile.getUUID().toString());
+				intent.putExtra(VpnProfileControlActivity.EXTRA_VPN_PROFILE_UUID, mProfile.getUUID().toString());
 				startActivity(intent);
 				/* reset the retry timer immediately in case the user needs more time to enter the password */
 				notifyListeners(() -> {
@@ -353,7 +356,8 @@ public class VpnStateService extends Service
 	 */
 	private void notifyListeners(final Callable<Boolean> change)
 	{
-		mHandler.post(new Runnable() {
+		mHandler.post(new Runnable()
+		{
 			@Override
 			public void run()
 			{
@@ -386,7 +390,8 @@ public class VpnStateService extends Service
 	 */
 	public void startConnection(final VpnProfile profile)
 	{
-		notifyListeners(new Callable<Boolean>() {
+		notifyListeners(new Callable<Boolean>()
+		{
 			@Override
 			public Boolean call() throws Exception
 			{
@@ -411,7 +416,8 @@ public class VpnStateService extends Service
 	 */
 	public void setState(final State state)
 	{
-		notifyListeners(new Callable<Boolean>() {
+		notifyListeners(new Callable<Boolean>()
+		{
 			@Override
 			public Boolean call() throws Exception
 			{
@@ -438,7 +444,8 @@ public class VpnStateService extends Service
 	 */
 	public void setError(final ErrorState error)
 	{
-		notifyListeners(new Callable<Boolean>() {
+		notifyListeners(new Callable<Boolean>()
+		{
 			@Override
 			public Boolean call() throws Exception
 			{
@@ -471,7 +478,8 @@ public class VpnStateService extends Service
 	 */
 	public void setImcState(final ImcState state)
 	{
-		notifyListeners(new Callable<Boolean>() {
+		notifyListeners(new Callable<Boolean>()
+		{
 			@Override
 			public Boolean call() throws Exception
 			{
@@ -501,7 +509,8 @@ public class VpnStateService extends Service
 	 */
 	public void addRemediationInstruction(final RemediationInstruction instruction)
 	{
-		mHandler.post(new Runnable() {
+		mHandler.post(new Runnable()
+		{
 			@Override
 			public void run()
 			{
@@ -535,7 +544,8 @@ public class VpnStateService extends Service
 	/**
 	 * Special Handler subclass that handles the retry countdown (more accurate than CountDownTimer)
 	 */
-	private static class RetryHandler extends Handler {
+	private static class RetryHandler extends Handler
+	{
 		WeakReference<VpnStateService> mService;
 
 		public RetryHandler(Looper looper, VpnStateService service)
@@ -604,6 +614,7 @@ public class VpnStateService extends Service
 		/**
 		 * Called each time a new retry timeout is started. The timeout increases until reset() is
 		 * called and the base timeout is returned again.
+		 *
 		 * @param error Error state
 		 */
 		public long getTimeout(ErrorState error)

src/frontends/android/app/src/main/java/org/strongswan/android/ui/MainActivity.java
@@ -115,7 +115,7 @@ public class MainActivity extends AppCompatActivity implements OnVpnProfileSelec
 	{
 		Intent intent = new Intent(this, VpnProfileControlActivity.class);
 		intent.setAction(VpnProfileControlActivity.START_PROFILE);
-		intent.putExtra(VpnProfileControlActivity.EXTRA_VPN_PROFILE_ID, profile.getUUID().toString());
+		intent.putExtra(VpnProfileControlActivity.EXTRA_VPN_PROFILE_UUID, profile.getUUID().toString());
 		startActivity(intent);
 	}
 

commit 45371da84667b9e464415c18968fdb422bce6571

android: Add fallback for the old name of the profile ID extra
This fixes existing shortcuts and automation via Intents.

Fixes: 8e3b921abed7 ("android: Always use UUID to access profiles")

src/frontends/android/app/src/main/java/org/strongswan/android/ui/VpnProfileControlActivity.java
@@ -65,6 +65,7 @@ public class VpnProfileControlActivity extends AppCompatActivity
 	public static final String START_PROFILE = "org.strongswan.android.action.START_PROFILE";
 	public static final String DISCONNECT = "org.strongswan.android.action.DISCONNECT";
 	public static final String EXTRA_VPN_PROFILE_UUID = "org.strongswan.android.VPN_PROFILE_UUID";
+	private static final String EXTRA_VPN_PROFILE_ID = "org.strongswan.android.VPN_PROFILE_ID";
 
 	private static final String WAITING_FOR_RESULT = "WAITING_FOR_RESULT";
 	private static final String PROFILE_NAME = "PROFILE_NAME";
@@ -378,6 +379,10 @@ public class VpnProfileControlActivity extends AppCompatActivity
 		VpnProfileDataSource dataSource = new VpnProfileSource(this);
 		dataSource.open();
 		String profileUUID = intent.getStringExtra(EXTRA_VPN_PROFILE_UUID);
+		if (profileUUID == null)
+		{
+			profileUUID = intent.getStringExtra(EXTRA_VPN_PROFILE_ID);
+		}
 		if (profileUUID != null)
 		{
 			profile = dataSource.getVpnProfile(profileUUID);
@@ -407,6 +412,10 @@ public class VpnProfileControlActivity extends AppCompatActivity
 		removeFragmentByTag(DIALOG_TAG);
 
 		String profileUUID = intent.getStringExtra(EXTRA_VPN_PROFILE_UUID);
+		if (profileUUID == null)
+		{
+			profileUUID = intent.getStringExtra(EXTRA_VPN_PROFILE_ID);
+		}
 		if (profileUUID != null)
 		{
 			VpnProfileDataSource dataSource = new VpnProfileSource(this);