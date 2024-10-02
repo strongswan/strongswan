strongswan/strongswan

commit 33412158f58c6dabefa91b39ca7282a6584a8261

ike: Send AEAD ESP default proposal first
We generally prefer AEAD nowadays.

References #3461.

src/libcharon/plugins/vici/vici_config.c
@@ -2006,12 +2006,12 @@ CALLBACK(children_sn, bool,
 	}
 	if (child.proposals->get_count(child.proposals) == 0)
 	{
-		proposal = proposal_create_default(PROTO_ESP);
+		proposal = proposal_create_default_aead(PROTO_ESP);
 		if (proposal)
 		{
 			child.proposals->insert_last(child.proposals, proposal);
 		}
-		proposal = proposal_create_default_aead(PROTO_ESP);
+		proposal = proposal_create_default(PROTO_ESP);
 		if (proposal)
 		{
 			child.proposals->insert_last(child.proposals, proposal);

src/charon-cmd/cmd/cmd_connection.c
@@ -362,9 +362,8 @@ static child_cfg_t* create_child_cfg(private_cmd_connection_t *this,
 	}
 	else
 	{
+		child_cfg->add_proposal(child_cfg, proposal_create_default_aead(PROTO_ESP));
 		child_cfg->add_proposal(child_cfg, proposal_create_default(PROTO_ESP));
-		child_cfg->add_proposal(child_cfg,
-								proposal_create_default_aead(PROTO_ESP));
 	}
 	while (this->local_ts->remove_first(this->local_ts, (void**)&ts) == SUCCESS)
 	{

src/charon-nm/nm/nm_service.c
@@ -834,8 +834,8 @@ static gboolean connect_(NMVpnServicePlugin *plugin, NMConnection *connection,
 	}
 	else
 	{
-		child_cfg->add_proposal(child_cfg, proposal_create_default(PROTO_ESP));
 		child_cfg->add_proposal(child_cfg, proposal_create_default_aead(PROTO_ESP));
+		child_cfg->add_proposal(child_cfg, proposal_create_default(PROTO_ESP));
 	}
 	ts = traffic_selector_create_dynamic(0, 0, 65535);
 	child_cfg->add_traffic_selector(child_cfg, TRUE, ts);

src/conftest/config.c
@@ -192,9 +192,8 @@ static child_cfg_t *load_child_config(private_config_t *this,
 	}
 	else
 	{
+		child_cfg->add_proposal(child_cfg, proposal_create_default_aead(PROTO_ESP));
 		child_cfg->add_proposal(child_cfg, proposal_create_default(PROTO_ESP));
-		child_cfg->add_proposal(child_cfg,
-								proposal_create_default_aead(PROTO_ESP));
 	}
 
 	token = settings->get_str(settings, "configs.%s.%s.lts", NULL, config, child);

src/frontends/osx/charon-xpc/xpc_dispatch.c
@@ -150,8 +150,8 @@ static child_cfg_t* create_child_cfg(char *name)
 	child_cfg->add_proposal(child_cfg, proposal_create_from_string(PROTO_ESP,
 										"aes128gcm8-aes128gcm12-aes128gcm16-"
 										"aes256gcm8-aes256gcm12-aes256gcm16"));
-	child_cfg->add_proposal(child_cfg, proposal_create_default(PROTO_ESP));
 	child_cfg->add_proposal(child_cfg, proposal_create_default_aead(PROTO_ESP));
+	child_cfg->add_proposal(child_cfg, proposal_create_default(PROTO_ESP));
 	ts = traffic_selector_create_dynamic(0, 0, 65535);
 	child_cfg->add_traffic_selector(child_cfg, TRUE, ts);
 	ts = traffic_selector_create_from_string(0, TS_IPV4_ADDR_RANGE,

src/libcharon/plugins/ha/ha_tunnel.c
@@ -256,8 +256,8 @@ static void setup_tunnel(private_ha_tunnel_t *this,
 	child_cfg->add_traffic_selector(child_cfg, FALSE, ts);
 	ts = traffic_selector_create_dynamic(IPPROTO_ICMP, 0, 65535);
 	child_cfg->add_traffic_selector(child_cfg, FALSE, ts);
-	child_cfg->add_proposal(child_cfg, proposal_create_default(PROTO_ESP));
 	child_cfg->add_proposal(child_cfg, proposal_create_default_aead(PROTO_ESP));
+	child_cfg->add_proposal(child_cfg, proposal_create_default(PROTO_ESP));
 	peer_cfg->add_child_cfg(peer_cfg, child_cfg);
 
 	this->backend.cfg = peer_cfg;

src/libcharon/plugins/medcli/medcli_config.c
@@ -196,8 +196,8 @@ METHOD(backend_t, get_peer_cfg_by_name, peer_cfg_t*,
 	peer_cfg->add_auth_cfg(peer_cfg, auth, FALSE);
 
 	child_cfg = child_cfg_create(name, &child);
-	child_cfg->add_proposal(child_cfg, proposal_create_default(PROTO_ESP));
 	child_cfg->add_proposal(child_cfg, proposal_create_default_aead(PROTO_ESP));
+	child_cfg->add_proposal(child_cfg, proposal_create_default(PROTO_ESP));
 	child_cfg->add_traffic_selector(child_cfg, TRUE, ts_from_string(local_net));
 	child_cfg->add_traffic_selector(child_cfg, FALSE, ts_from_string(remote_net));
 	peer_cfg->add_child_cfg(peer_cfg, child_cfg);
@@ -277,8 +277,8 @@ METHOD(enumerator_t, peer_enumerator_enumerate, bool,
 	this->current->add_auth_cfg(this->current, auth, FALSE);
 
 	child_cfg = child_cfg_create(name, &child);
-	child_cfg->add_proposal(child_cfg, proposal_create_default(PROTO_ESP));
 	child_cfg->add_proposal(child_cfg, proposal_create_default_aead(PROTO_ESP));
+	child_cfg->add_proposal(child_cfg, proposal_create_default(PROTO_ESP));
 	child_cfg->add_traffic_selector(child_cfg, TRUE, ts_from_string(local_net));
 	child_cfg->add_traffic_selector(child_cfg, FALSE, ts_from_string(remote_net));
 	this->current->add_child_cfg(this->current, child_cfg);

src/libcharon/plugins/sql/sql_config.c
@@ -153,8 +153,8 @@ static void add_esp_proposals(private_sql_config_t *this,
 	}
 	if (use_default)
 	{
-		child->add_proposal(child, proposal_create_default(PROTO_ESP));
 		child->add_proposal(child, proposal_create_default_aead(PROTO_ESP));
+		child->add_proposal(child, proposal_create_default(PROTO_ESP));
 	}
 }
 

commit 8e020bc9e31ecd7e08bdf6116e110d189ce26c90

vici-config: Same order for default ESP proposals if unset or set to "default"
The order was different when not setting `esp_proposals` or explicitly
configuring `default`.

Fixes: 33412158f58c ("ike: Send AEAD ESP default proposal first")

src/libcharon/plugins/vici/vici_config.c
@@ -602,6 +602,34 @@ static void free_child_data(child_data_t *data)
 }
 
 /**
+ * Add the default proposals for the given protocol.  We currently prefer AEAD
+ * for ESP but not for IKE.
+ */
+static void add_default_proposals(linked_list_t *list, protocol_id_t proto)
+{
+	proposal_t *first, *second;
+
+	if (proto == PROTO_IKE)
+	{
+		first = proposal_create_default(proto);
+		second = proposal_create_default_aead(proto);
+	}
+	else
+	{
+		first = proposal_create_default_aead(proto);
+		second = proposal_create_default(proto);
+	}
+	if (first)
+	{
+		list->insert_last(list, first);
+	}
+	if (second)
+	{
+		list->insert_last(list, second);
+	}
+}
+
+/**
  * Common proposal parsing
  */
 static bool parse_proposal(linked_list_t *list, protocol_id_t proto, chunk_t v)
@@ -615,16 +643,7 @@ static bool parse_proposal(linked_list_t *list, protocol_id_t proto, chunk_t v)
 	}
 	if (strcaseeq("default", buf))
 	{
-		proposal = proposal_create_default(proto);
-		if (proposal)
-		{
-			list->insert_last(list, proposal);
-		}
-		proposal = proposal_create_default_aead(proto);
-		if (proposal)
-		{
-			list->insert_last(list, proposal);
-		}
+		add_default_proposals(list, proto);
 		return TRUE;
 	}
 	proposal = proposal_create_from_string(proto, buf);
@@ -2134,16 +2153,7 @@ CALLBACK(children_sn, bool,
 	}
 	if (child.proposals->get_count(child.proposals) == 0)
 	{
-		proposal = proposal_create_default_aead(PROTO_ESP);
-		if (proposal)
-		{
-			child.proposals->insert_last(child.proposals, proposal);
-		}
-		proposal = proposal_create_default(PROTO_ESP);
-		if (proposal)
-		{
-			child.proposals->insert_last(child.proposals, proposal);
-		}
+		add_default_proposals(child.proposals, PROTO_ESP);
 	}
 
 	check_lifetimes(&child.cfg.lifetime);
@@ -2740,16 +2750,7 @@ CALLBACK(config_sn, bool,
 	}
 	if (peer.proposals->get_count(peer.proposals) == 0)
 	{
-		proposal = proposal_create_default(PROTO_IKE);
-		if (proposal)
-		{
-			peer.proposals->insert_last(peer.proposals, proposal);
-		}
-		proposal = proposal_create_default_aead(PROTO_IKE);
-		if (proposal)
-		{
-			peer.proposals->insert_last(peer.proposals, proposal);
-		}
+		add_default_proposals(peer.proposals, PROTO_IKE);
 	}
 	if (!peer.local_addrs)
 	{