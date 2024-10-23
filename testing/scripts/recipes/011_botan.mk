974f9c37dfeff2ad2a5ce692784b44c241a22cfa
Tobias Brunner <tobias@strongswan.org>
2024-10-23 16:48:36 +0200

    Use Botan 3.6.1 for tests
    With 3.6.0 support for ML-KEM was added.

--- testing/scripts/recipes/011_botan.mk
+++ testing/scripts/recipes/011_botan.mk
@@ -2,7 +2,7 @@
 
 PKG = botan
 SRC = https://github.com/randombit/$(PKG).git
-REV = 3.3.0
+REV = 3.6.1
 
 NUM_CPUS := $(shell getconf _NPROCESSORS_ONLN)
 
@@ -10,7 +10,7 @@ NUM_CPUS := $(shell getconf _NPROCESSORS_ONLN)
 CONFIG_OPTS = \
 	--without-os-features=threads \
 	--disable-modules=locking_allocator \
-	--disable-modules=pkcs11,tls,x509,xmss \
+	--disable-modules=pkcs11,tls,x509,xmss
 
 all: install
 
