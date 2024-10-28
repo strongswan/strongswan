307dea6b5f6135253f5cd3e0cd2b9e8c16b62ed8
Tobias Brunner <tobias@strongswan.org>
2024-10-28 10:34:08 +0100

    Use wolfSSL 5.7.4 for tests
    This adds support for ML-KEM etc.

--- scripts/test.sh
+++ scripts/test.sh
@@ -37,7 +37,7 @@ build_botan()
 
 build_wolfssl()
 {
-	WOLFSSL_REV=v5.7.2-stable
+	WOLFSSL_REV=v5.7.4-stable
 	WOLFSSL_DIR=$DEPS_BUILD_DIR/wolfssl
 
 	if test -d "$WOLFSSL_DIR"; then
@@ -47,7 +47,8 @@ build_wolfssl()
 	echo "$ build_wolfssl()"
 
 	WOLFSSL_CFLAGS="-DWOLFSSL_PUBLIC_MP -DWOLFSSL_DES_ECB -DHAVE_AES_ECB \
-					-DHAVE_ECC_BRAINPOOL -DWOLFSSL_MIN_AUTH_TAG_SZ=8"
+					-DHAVE_ECC_BRAINPOOL -DWOLFSSL_MIN_AUTH_TAG_SZ=8 \
+					-DRSA_MIN_SIZE=1024"
 	WOLFSSL_CONFIG="--prefix=$DEPS_PREFIX
 					--disable-crypttests --disable-examples
 					--enable-aesccm --enable-aesctr --enable-camellia
--- testing/scripts/recipes/012_wolfssl.mk
+++ testing/scripts/recipes/012_wolfssl.mk
@@ -2,16 +2,14 @@
 
 PKG = wolfssl
 SRC = https://github.com/wolfSSL/$(PKG).git
-REV = v5.7.2-stable
+REV = v5.7.4-stable
 
 NUM_CPUS := $(shell getconf _NPROCESSORS_ONLN)
 
 CFLAGS = \
 	-DWOLFSSL_PUBLIC_MP \
-	-DWOLFSSL_DES_ECB \
 	-DHAVE_AES_ECB \
-	-DHAVE_ECC_BRAINPOOL \
-	-DWOLFSSL_MIN_AUTH_TAG_SZ=8
+	-DHAVE_ECC_BRAINPOOL
 
 CONFIG_OPTS = \
 	--disable-crypttests \
