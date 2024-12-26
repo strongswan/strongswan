9eb7f98e6d6935b7a46b8fdabc48de9f3314c012
Andreas Steffen <andreas.steffen@strongswan.org>
2024-12-26 11:42:51 +0100

    ml: Add ML-DSA support

--- src/libstrongswan/plugins/ml/ml_utils.c
+++ src/libstrongswan/plugins/ml/ml_utils.c
@@ -53,3 +53,75 @@ void ml_write_bytes_le(uint8_t *buf, size_t len, uint32_t val)
 		val >>= 8;
 	}
 }
+
+/*
+ * Described in header
+ */
+void ml_decompose(int32_t a, int32_t *a0, int32_t *a1, int32_t gamma2)
+{
+	int32_t t0, t1;
+
+	t1  = (a + 127) >> 7;
+
+	if (gamma2 == (ML_DSA_Q-1)/32)
+	{
+		t1  = (t1 * 1025 + (1 << 21)) >> 22;
+		t1 &= 15;
+	}
+	else
+	{
+		t1  = (t1 * 11275 + (1 << 23)) >> 24;
+		t1 ^= ((43 - t1) >> 31) & t1;
+	}
+
+	t0 = a - t1 * 2 * gamma2;
+	t0 -= (((ML_DSA_Q-1)/2 - t0) >> 31) & ML_DSA_Q;
+
+	*a0 = t0;
+	*a1 = t1;
+}
+
+/*
+ * Described in header
+ */
+int32_t ml_use_hint(int32_t a, int32_t hint, int32_t gamma2)
+{
+	int32_t a0, a1;
+
+	ml_decompose(a, &a0, &a1, gamma2);
+
+	if (hint == 0)
+	{
+		return a1;
+	}
+	if (gamma2 == (ML_DSA_Q-1)/32)
+	{
+		if (a0 > 0)
+		{
+			return (a1 + 1) & 15;
+		}
+		else
+		{
+			return (a1 - 1) & 15;
+		}
+	}
+	else
+    {
+		if (a0 > 0)
+		{
+			return (a1 == 43) ?  0 : a1 + 1;
+		}
+		else
+		{
+			return (a1 ==  0) ? 43 : a1 - 1;
+		}
+    }
+}
+
+/*
+ * Described in header
+ */
+int32_t ml_make_hint(int32_t a0, int32_t a1, int32_t gamma2)
+{
+	return (a0 > gamma2 || a0 < -gamma2 || (a0 == -gamma2 && a1 != 0)) ? 1 : 0;
+}
