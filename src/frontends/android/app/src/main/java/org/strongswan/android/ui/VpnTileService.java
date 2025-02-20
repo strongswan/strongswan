strongswan/strongswan

commit 5237bf3a5c6fa9eb13ad8ba416a7865876b0d6f7

android: Suppress deprecation warning because of startActivityAndCollapse()

src/frontends/android/app/src/main/java/org/strongswan/android/ui/VpnTileService.java
@@ -187,7 +187,7 @@ public class VpnTileService extends TileService implements VpnStateService.VpnSt
 					}
 					else
 					{
-						startActivityAndCollapse(intent);
+						startActivityAndCollapseCompat(intent);
 					}
 				}
 				else
@@ -214,8 +214,14 @@ public class VpnTileService extends TileService implements VpnStateService.VpnSt
 		}
 		else
 		{
-			startActivityAndCollapse(intent);
+			startActivityAndCollapseCompat(intent);
 		}
+	}
+
+	@SuppressWarnings("deprecation")
+	private void startActivityAndCollapseCompat(Intent intent)
+	{
+		startActivityAndCollapse(intent);
 	}
 
 	@Override

commit e4d6bcef480859ca194347b1b5f5a7f5afa70bcc

android: Move annotation to method where startActivityAndCollapse() is called
Fixes: 5237bf3a5c6f ("android: Suppress deprecation warning because of startActivityAndCollapse()")

src/frontends/android/app/src/main/java/org/strongswan/android/ui/VpnTileService.java
@@ -130,7 +130,6 @@ public class VpnTileService extends TileService implements VpnStateService.VpnSt
 		return mDataSource != null ? mDataSource.getVpnProfile(uuid) : null;
 	}
 
-	@SuppressLint("StartActivityAndCollapseDeprecated")
 	@Override
 	public void onClick()
 	{
@@ -219,6 +218,7 @@ public class VpnTileService extends TileService implements VpnStateService.VpnSt
 	}
 
 	@SuppressWarnings("deprecation")
+	@SuppressLint("StartActivityAndCollapseDeprecated")
 	private void startActivityAndCollapseCompat(Intent intent)
 	{
 		startActivityAndCollapse(intent);