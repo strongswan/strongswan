2d71ce0d8a70f8476ede0a1133b1cb6c731fe2a0
Tobias Brunner <tobias@strongswan.org>
2025-02-21 16:11:04 +0100

    github: Use OpenSSL 3.5.0 to test ML-KEM

--- scripts/test.sh
+++ scripts/test.sh
@@ -92,7 +92,7 @@ build_tss2()
 
 build_openssl()
 {
-	SSL_REV=openssl-3.4.1
+	SSL_REV=openssl-3.5.0
 	SSL_DIR=$DEPS_BUILD_DIR/openssl
 	SSL_INS=$DEPS_PREFIX/ssl
 	SSL_OPT="-d shared no-dtls no-ssl3 no-zlib no-idea no-psk
