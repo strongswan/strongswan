f740faccac16835ba0da126309e82ff4e65f05e9
Tobias Brunner <tobias@strongswan.org>
2025-02-27 15:50:02 +0100

    github: Don't use pip3 to install Python packages anymore
    Currently, the runner images enable break-system-packages globally.
    However, this workaround will be removed by the end of March.  So
    we switch to installing these packages as intended via distro (the
    alternative would be to use pipx, at least for tox).

--- scripts/test.sh
+++ scripts/test.sh
@@ -306,8 +306,14 @@ all|alpine|codeql|coverage|sonarcloud|no-dbg)
 	DEPS="$DEPS libcurl4-gnutls-dev libsoup2.4-dev libunbound-dev libldns-dev
 		  libmysqlclient-dev libsqlite3-dev clearsilver-dev libfcgi-dev
 		  libldap2-dev libpcsclite-dev libpam0g-dev binutils-dev libnm-dev
-		  libgcrypt20-dev libjson-c-dev python3-pip libtspi-dev libsystemd-dev
-		  libselinux1-dev libiptc-dev"
+		  libgcrypt20-dev libjson-c-dev libtspi-dev libsystemd-dev
+		  libselinux1-dev libiptc-dev python3-build tox"
+	if [ "$ID" = "ubuntu" -a "$VERSION_ID" = "22.04" -a "$1" = "build-deps" ]; then
+		# python3-build is broken on 22.04 with venv (https://bugs.launchpad.net/ubuntu/+source/python-build/+bug/1992108)
+		# while installing python3-virtualenv should help, it doesn't. as even
+		# after uninstalling python3-venv, build prefers the latter
+		sudo python3 -m pip install --upgrade build
+	fi
 	if [ "$TEST" = "alpine" ]; then
 		# override the whole list for alpine
 		DEPS="git gmp-dev openldap-dev curl-dev ldns-dev unbound-dev libsoup-dev
@@ -324,7 +330,6 @@ all|alpine|codeql|coverage|sonarcloud|no-dbg)
 		# and no Clearsilver
 		CONFIG="$CONFIG --disable-fast --disable-manager --disable-medsrv"
 	fi
-	PYDEPS="build tox"
 	if test "$1" = "build-deps"; then
 		build_botan
 		build_wolfssl
@@ -489,10 +494,6 @@ deps)
 		pkg install -y bison flex gperf gettext $DEPS
 		;;
 	esac
-	exit $?
-	;;
-pydeps)
-	test -z "$PYDEPS" || pip3 -q install --user $PYDEPS
 	exit $?
 	;;
 build-deps)
