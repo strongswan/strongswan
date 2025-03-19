c4d0e932eed211152681c29806c45a7d87a18300
Tobias Brunner <tobias@strongswan.org>
2025-03-19 15:34:58 +0100

    child-cfg: Use separate method to get configured traffic selectors
    Optionally with "dynamic" traffic selectors resolved.  A new method
    is added for those cases where we actually want to select potentially
    narrowed traffic selectors using a supplied list.  The latter now also
    always logs details, while the former does not.

--- src/libcharon/plugins/smp/smp.c
+++ src/libcharon/plugins/smp/smp.c
@@ -330,12 +330,10 @@ static void request_query_config(xmlTextReaderPtr reader, xmlTextWriterPtr write
 			xmlTextWriterStartElement(writer, "childconfig");
 			xmlTextWriterWriteElement(writer, "name",
 									  child_cfg->get_name(child_cfg));
-			list = child_cfg->get_traffic_selectors(child_cfg, TRUE, NULL,
-													NULL, FALSE);
+			list = child_cfg->get_traffic_selectors(child_cfg, TRUE, NULL);
 			write_networks(writer, "local", list);
 			list->destroy_offset(list, offsetof(traffic_selector_t, destroy));
-			list = child_cfg->get_traffic_selectors(child_cfg, FALSE, NULL,
-													NULL, FALSE);
+			list = child_cfg->get_traffic_selectors(child_cfg, FALSE, NULL);
 			write_networks(writer, "remote", list);
 			list->destroy_offset(list, offsetof(traffic_selector_t, destroy));
 			xmlTextWriterEndElement(writer);
