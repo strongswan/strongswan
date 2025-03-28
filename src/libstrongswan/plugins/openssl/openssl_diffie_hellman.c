4cf75f125c5d76560ff59ebc8ec1e25b820398ca
Tobias Brunner <tobias@strongswan.org>
2025-03-28 12:06:37 +0100

    Add configure option to disable testing key exchange methods
    If this is used, the functionality to set a private key/value/seed for
    key exchange methods is removed (including from the interface to avoid
    accidentally forgetting to wrap implementations and uses of set_seed()).
    
    The set_seed() method is assigned outside the INIT() macro to avoid
    potentially undefined behavior (preprocessing directives in macro
    arguments).
    
    The test done by the crypto tester is a simple functionality test.

--- src/libstrongswan/plugins/openssl/openssl_diffie_hellman.c
+++ src/libstrongswan/plugins/openssl/openssl_diffie_hellman.c
@@ -180,6 +180,7 @@ METHOD(key_exchange_t, set_public_key, bool,
 	return TRUE;
 }
 
+#ifdef TESTABLE_KE
 #if OPENSSL_VERSION_NUMBER >= 0x30000000L
 
 /**
@@ -273,6 +274,7 @@ METHOD(key_exchange_t, set_seed, bool,
 }
 
 #endif /* OPENSSL_VERSION_NUMBER */
+#endif /* TESTABLE_KE */
 
 METHOD(key_exchange_t, destroy, void,
 	private_openssl_diffie_hellman_t *this)
@@ -304,13 +306,16 @@ openssl_diffie_hellman_t *openssl_diffie_hellman_create(
 				.get_shared_secret = _get_shared_secret,
 				.set_public_key = _set_public_key,
 				.get_public_key = _get_public_key,
-				.set_seed = _set_seed,
 				.get_method = _get_method,
 				.destroy = _destroy,
 			},
 		},
 		.group = group,
 	);
+
+#ifdef TESTABLE_KE
+	this->public.ke.set_seed = _set_seed;
+#endif
 
 	if (group == MODP_CUSTOM)
 	{
