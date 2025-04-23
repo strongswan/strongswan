c4eb8379c4e938abd1d549419ebfd9eec48cbeec
Juliusz Sosinowicz <juliusz@wolfssl.com>
2025-04-23 16:32:52 +0200

    wolfssl: Unlock keys if necessary when using FIPS module
    Wrap the functions that require it in PRIVATE_KEY_UNLOCK/PRIVATE_KEY_LOCK.
    This can't be done at plugin initialization because it needs to be done
    for every thread. strongSwan currently doesn't provide on-thread-create
    callbacks for plugins so we need to wrap each direct call. Another reason
    to do so is that some functions we call (e.g. wc_EccKeyToDer) internally
    call PRIVATE_KEY_UNLOCK/PRIVATE_KEY_LOCK and would leave the keys locked
    for that particular thread.

--- src/libstrongswan/plugins/wolfssl/wolfssl_kdf.c
+++ src/libstrongswan/plugins/wolfssl/wolfssl_kdf.c
@@ -82,22 +82,27 @@ METHOD(kdf_t, get_length, size_t,
 METHOD(kdf_t, get_bytes, bool,
 	private_kdf_t *this, size_t out_len, uint8_t *buffer)
 {
+	int ret;
+
 	if (this->type == KDF_PRF)
 	{
-		if (out_len != get_length(this) ||
-			wc_HKDF_Extract(this->hash, this->salt.ptr, this->salt.len,
-							this->key.ptr, this->key.len, buffer))
+		if (out_len != get_length(this))
 		{
 			return FALSE;
 		}
-		return TRUE;
+		PRIVATE_KEY_UNLOCK();
+		ret = wc_HKDF_Extract(this->hash, this->salt.ptr, this->salt.len,
+							  this->key.ptr, this->key.len, buffer);
+		PRIVATE_KEY_LOCK();
 	}
-	if (wc_HKDF_Expand(this->hash, this->key.ptr, this->key.len,
-					   this->salt.ptr, this->salt.len, buffer, out_len))
+	else
 	{
-		return FALSE;
+		PRIVATE_KEY_UNLOCK();
+		ret = wc_HKDF_Expand(this->hash, this->key.ptr, this->key.len,
+							 this->salt.ptr, this->salt.len, buffer, out_len);
+		PRIVATE_KEY_LOCK();
 	}
-	return TRUE;
+	return ret == 0;
 }
 
 METHOD(kdf_t, allocate_bytes, bool,
