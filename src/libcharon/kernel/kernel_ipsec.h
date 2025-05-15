33db7a200f6b4e8f587b40469d18ef25f951fa9d
Tobias Brunner <tobias@strongswan.org>
2025-05-15 09:20:52 +0200

    kernel-ipsec: Add flag to disable sending fragments across AGGFRAG payloads
    We have to set this if the peer indicates that it doesn't support
    handling such fragments in the notify.

--- src/libcharon/kernel/kernel_ipsec.h
+++ src/libcharon/kernel/kernel_ipsec.h
@@ -113,6 +113,8 @@ struct kernel_ipsec_add_sa_t {
 	bool copy_ecn;
 	/** Whether to copy the DSCP header field to/from the outer header */
 	dscp_copy_t copy_dscp;
+	/** TRUE if the peer doesn't support receiving fragments in AGGFRAG pkts */
+	bool iptfs_dont_frag;
 	/** TRUE if initiator of the exchange creating the SA */
 	bool initiator;
 	/** TRUE if this is an inbound SA */
