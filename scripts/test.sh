36f7c98f4eaa56fc13eb5c380a6da86a69870702
Tobias Brunner <tobias@strongswan.org>
2025-07-01 08:32:46 +0200

    github: Use AWS-LC 1.55.0 for tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -132,7 +132,7 @@ build_openssl()
 
 build_awslc()
 {
-	LC_REV=1.52.0
+	LC_REV=1.55.0
 	LC_PKG=aws-lc-$LC_REV
 	LC_DIR=$DEPS_BUILD_DIR/$LC_PKG
 	LC_SRC=https://github.com/aws/aws-lc/archive/refs/tags/v${LC_REV}.tar.gz
