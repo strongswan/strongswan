d4c2d5b12e16230f2559a655a2281995225b3a30
Tobias Brunner <tobias@strongswan.org>
2026-02-26 18:47:57 +0100

    wip: compsigs: Add plugin that provides composite key and signature handling
    This wraps existing implementations of the underlying key types and
    hashes and provides required encoding and decoding for the combined
    keys and signatures.
    
    wip: some fixmes

---
+++ src/libstrongswan/plugins/compsigs/compsigs_plugin.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2026 Tobias Brunner
+ *
+ * Copyright (C) secunet Security Networks AG
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+
+/**
+ * Plugin that provides composite key types and signatures where ML-DSA is
+ * combined with a traditional algorithm. It acts as a wrapper around
+ * implementations of the underlying algorithms and provides some additional
+ * encoding/decoding necessary for the composite keys/signatures.
+ *
+ * @defgroup compsigs_p compsigs
+ * @ingroup plugins
+ *
+ * @defgroup compsigs_plugin compsigs_plugin
+ * @{ @ingroup compsigs_p
+ */
+
+#ifndef COMPSIGS_PLUGIN_H_
+#define COMPSIGS_PLUGIN_H_
+
+#endif /** COMPSIGS_PLUGIN_H_ @}*/
