strongswan/strongswan

commit e6cce7ff0d1b38ba720dcdda6bbc0308839f85f4

Prepend point format to ECDH public key

src/libtls/tls_server.c
@@ -412,13 +412,21 @@ static status_t process_key_exchange_dhe(private_tls_server_t *this,
 
 	ec = diffie_hellman_group_is_ec(this->dh->get_dh_group(this->dh));
 	if ((ec && !reader->read_data8(reader, &pub)) ||
-		(!ec && !reader->read_data16(reader, &pub)))
+		(!ec && (!reader->read_data16(reader, &pub) || pub.len == 0)))
 	{
 		DBG1(DBG_TLS, "received invalid Client Key Exchange");
 		this->alert->add(this->alert, TLS_FATAL, TLS_DECODE_ERROR);
 		return NEED_MORE;
 	}
-	this->dh->set_other_public_value(this->dh, pub);
+
+	if (pub.ptr[0] != TLS_ECP_UNCOMPRESSED)
+	{
+		DBG1(DBG_TLS, "DH point format '%N' not supported",
+			 tls_ecp_format_names, pub.ptr[0]);
+		this->alert->add(this->alert, TLS_FATAL, TLS_INTERNAL_ERROR);
+		return NEED_MORE;
+	}
+	this->dh->set_other_public_value(this->dh, chunk_skip(pub, 1));
 	if (this->dh->get_shared_secret(this->dh, &premaster) != SUCCESS)
 	{
 		DBG1(DBG_TLS, "calculating premaster from DH failed");
@@ -847,8 +855,10 @@ static status_t send_server_key_exchange(private_tls_server_t *this,
 		writer->write_data16(writer, chunk);
 	}
 	else
-	{	/* 8bit header for EC groups */
-		writer->write_data8(writer, chunk);
+	{	/* ECP uses 8bit length header only, but a point format */
+		writer->write_uint8(writer, chunk.len + 1);
+		writer->write_uint8(writer, TLS_ECP_UNCOMPRESSED);
+		writer->write_data(writer, chunk);
 	}
 	free(chunk.ptr);
 

src/libtls/tls_crypto.c
@@ -306,6 +306,14 @@ ENUM(tls_named_curve_names, TLS_SECT163K1, TLS_SECP521R1,
 	"SECP521R1",
 );
 
+ENUM(tls_ecp_format_names, TLS_ECP_COMPRESSED, TLS_ECP_HYBRID_Y,
+	"compressed",
+	"compressed y",
+	"uncompressed",
+	"uncompressed y",
+	"hybrid",
+	"hybrid y",
+);
 
 typedef struct private_tls_crypto_t private_tls_crypto_t;
 

src/libtls/tls_crypto.h
@@ -359,6 +359,22 @@ enum tls_named_curve_t {
 extern enum_name_t *tls_named_curve_names;
 
 /**
+ * EC Point format, ANSI X9.62.
+ */
+enum tls_ecp_format_t {
+	TLS_ECP_COMPRESSED =	2,
+	TLS_ECP_COMPRESSED_Y =	3,
+	TLS_ECP_UNCOMPRESSED =	4,
+	TLS_ECP_HYBRID =		6,
+	TLS_ECP_HYBRID_Y =		7,
+};
+
+/**
+ * Enum names for tls_ecp_format_t.
+ */
+extern enum_name_t *tls_ecp_format_names;
+
+/**
  * TLS crypto helper functions.
  */
 struct tls_crypto_t {

src/libtls/tls_peer.c
@@ -402,7 +402,7 @@ static status_t process_ec_key_exchange(private_tls_peer_t *this,
 		return NEED_MORE;
 	}
 	if (!reader->read_uint16(reader, &curve) ||
-		!reader->read_data8(reader, &pub))
+		!reader->read_data8(reader, &pub) || pub.len == 0)
 	{
 		DBG1(DBG_TLS, "received invalid Server Key Exchange");
 		this->alert->add(this->alert, TLS_FATAL, TLS_DECODE_ERROR);
@@ -448,7 +448,15 @@ static status_t process_ec_key_exchange(private_tls_peer_t *this,
 		this->alert->add(this->alert, TLS_FATAL, TLS_INTERNAL_ERROR);
 		return NEED_MORE;
 	}
-	this->dh->set_other_public_value(this->dh, pub);
+
+	if (pub.ptr[0] != TLS_ECP_UNCOMPRESSED)
+	{
+		DBG1(DBG_TLS, "DH point format '%N' not supported",
+			 tls_ecp_format_names, pub.ptr[0]);
+		this->alert->add(this->alert, TLS_FATAL, TLS_INTERNAL_ERROR);
+		return NEED_MORE;
+	}
+	this->dh->set_other_public_value(this->dh, chunk_skip(pub, 1));
 
 	this->state = STATE_KEY_EXCHANGE_RECEIVED;
 	return NEED_MORE;
@@ -908,8 +916,10 @@ static status_t send_key_exchange_dhe(private_tls_peer_t *this,
 		writer->write_data16(writer, pub);
 	}
 	else
-	{	/* ECP uses 8bit length header only */
-		writer->write_data8(writer, pub);
+	{	/* ECP uses 8bit length header only, but a point format */
+		writer->write_uint8(writer, pub.len + 1);
+		writer->write_uint8(writer, TLS_ECP_UNCOMPRESSED);
+		writer->write_data(writer, pub);
 	}
 	free(pub.ptr);
 

commit 1e0643bef105704337efc141a37dfcfbaa53cb1f

tls-server: Only accept non-empty ECDH public keys with TLS < 1.3
This prevents a crash due to a null-pointer dereference when processing
an empty ECDH public key.

The previous length check only applied in the `!ec` case, so in the `ec`
case, the access to `pub.ptr[0]` was unguarded.  If a crafted TLS
record ends with an empty ClientKeyExchange, then `read_data8` sets
`pub` to `chunk_empty`, causing a null-pointer dereference.

Note that if some data follows the empty ClientKeyExchange, this just
causes a 1-byte out-of-bounds read that has no further effect as the
TLS session is aborted immediately.  Either because the read value
doesn't equal TLS_ANSI_UNCOMPRESSED or because the empty public key
is rejected by `set_public_key()`.

The referenced commit that introduced the pointer access, added the
check for `pub.len` specifically to the `!ec` case, while the pointer
access was initially unconditional (probably because the code was just
copied from `tls_peer.c` which processes ECDH public keys in a separate
function, so there was no `ec` flag).  The latter was fixed a couple of
days later with 7b3c01845f63 ("Read the compression type byte for EC
groups, only").  However, that commit didn't change the length check.
Anyway, it's possible that the original intention was to add the check
to the `ec` case on the previous line, or that there was some confusion
with the parenthesis and something like the current code was intended to
begin with.

Fixes: e6cce7ff0d1b ("Prepend point format to ECDH public key")
Fixes: CVE-2026-35332

src/libtls/tls_server.c
@@ -862,7 +862,7 @@ static status_t process_key_exchange_dhe(private_tls_server_t *this,
 	group = this->dh->get_method(this->dh);
 	ec = key_exchange_is_ecdh(group);
 	if ((ec && !reader->read_data8(reader, &pub)) ||
-		(!ec && (!reader->read_data16(reader, &pub) || pub.len == 0)))
+		(!ec && !reader->read_data16(reader, &pub)) || pub.len == 0)
 	{
 		DBG1(DBG_TLS, "received invalid Client Key Exchange");
 		this->alert->add(this->alert, TLS_FATAL, TLS_DECODE_ERROR);