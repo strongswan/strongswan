c340659c2461069c51b515a0875c78ebe7a6d74b
Tobias Brunner <tobias@strongswan.org>
2026-04-24 10:56:54 +0200

    wip: fixup: compsigs: Add plugin that provides composite key and signature handling
    wip: adds special handling for shake in case it stays like that. also,
    the test vector for that type ends with 0x00, so the test needed a fixup

--- src/libstrongswan/plugins/compsigs/compsigs_public_key.c
+++ src/libstrongswan/plugins/compsigs/compsigs_public_key.c
@@ -67,15 +67,31 @@ bool compsigs_get_mprime(const compsigs_params_t *params, chunk_t ctx,
 						 chunk_t data, chunk_t *msg)
 {
 	hasher_t *hasher;
+	xof_t *xof;
 	chunk_t ph;
 
-	hasher = lib->crypto->create_hasher(lib->crypto, params->prehash);
-	if (!hasher || !hasher->allocate_hash(hasher, data, &ph))
+	/* special case this type as it's the only one with an XOF */
+	if (params->type == KEY_MLDSA87_ED448)
 	{
-		DESTROY_IF(hasher);
-		return FALSE;
+		xof = lib->crypto->create_xof(lib->crypto, XOF_SHAKE_256);
+		if (!xof || !xof->set_seed(xof, data) ||
+			!xof->allocate_bytes(xof, 64, &ph))
+		{
+			DESTROY_IF(xof);
+			return FALSE;
+		}
+		xof->destroy(xof);
 	}
-	hasher->destroy(hasher);
+	else
+	{
+		hasher = lib->crypto->create_hasher(lib->crypto, params->prehash);
+		if (!hasher || !hasher->allocate_hash(hasher, data, &ph))
+		{
+			DESTROY_IF(hasher);
+			return FALSE;
+		}
+		hasher->destroy(hasher);
+	}
 
 	/* FIXME: the context has an additional zero-byte only used for ML-DSA,
 	 * possibly remove it here and add it in the ML-DSA implementations */
