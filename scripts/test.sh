cc3e08af376ac6ff00cccd2861b98b17370b1045
Tobias Brunner <tobias@strongswan.org>
2026-08-25 16:14:53 +0200

    github: Use OpenSSL 3.6.4 and 4.0.2 for tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -94,7 +94,7 @@ build_tss2()
 
 build_openssl()
 {
-	: ${SSL_REV=openssl-3.6.3}
+	: ${SSL_REV=openssl-3.6.4}
 	SSL_DIR=$DEPS_BUILD_DIR/openssl
 	SSL_INS=$DEPS_PREFIX/ssl
 	SSL_OPT="-d shared no-dtls no-ssl3 no-zlib no-idea no-psk
@@ -213,7 +213,7 @@ openssl*)
 		use_custom_openssl $1
 	elif test "$TEST" = "openssl-4"; then
 		DEPS=""
-		SSL_REV=openssl-4.0.1
+		SSL_REV=openssl-4.0.2
 		use_custom_openssl $1
 	elif test "$TEST" = "openssl-awslc"; then
 		DEPS="cmake ninja-build golang"
