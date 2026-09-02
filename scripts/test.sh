2bc525a3d632f938fef165a8d16e91d2b7267661
Tobias Brunner <tobias@strongswan.org>
2026-09-02 07:43:00 +0200

    github: Use AWS-LC 5.7.0 for tests
    Now supports the Brainpool curves.

--- scripts/test.sh
+++ scripts/test.sh
@@ -123,7 +123,7 @@ build_openssl()
 
 build_awslc()
 {
-	LC_REV=5.3.0
+	LC_REV=5.7.0
 	LC_PKG=aws-lc-$LC_REV
 	LC_DIR=$DEPS_BUILD_DIR/$LC_PKG
 	LC_SRC=https://github.com/aws/aws-lc/archive/refs/tags/v${LC_REV}.tar.gz
