strongswan/strongswan

commit 0fa9c958114f902c55f4922631e2ca4e5e77c736

windows: Provide a complete native Windows threading backend

src/libstrongswan/threading/windows/thread.c
@@ -1,0 +1,619 @@
+/*
+ * Copyright (C) 2013 Martin Willi
+ * Copyright (C) 2013 revosec AG
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+
+#include <library.h>
+#include <utils/debug.h>
+#include <threading/spinlock.h>
+#include <threading/thread.h>
+#include <collections/hashtable.h>
+#include <collections/array.h>
+
+#include "thread.h"
+
+
+typedef struct private_thread_t private_thread_t;
+
+struct private_thread_t {
+
+	/**
+	 * Public interface.
+	 */
+	thread_t public;
+
+	/**
+	 * GetCurrentThreadId() of thread
+	 */
+	DWORD id;
+
+	/**
+	 * Printable thread id returned by thread_current_id()
+	 */
+	u_int tid;
+
+	/**
+	 * Windows thread handle
+	 */
+	HANDLE handle;
+
+	/**
+	 * Main function of this thread (NULL for the main thread).
+	 */
+	thread_main_t main;
+
+	/**
+	 * Argument for the main function.
+	 */
+	void *arg;
+
+	/**
+	 * Thread return value
+	 */
+	void *ret;
+
+	/**
+	 * Stack of cleanup handlers, as cleanup_t
+	 */
+	array_t *cleanup;
+
+	/**
+	 * Thread specific values for this thread, as cleanup_t
+	 */
+	hashtable_t *tls;
+
+	/**
+	 * Thread terminated?
+	 */
+	bool terminated;
+
+	/**
+	 * Thread detached?
+	 */
+	bool detached;
+
+	/**
+	 * Is thread in cancellable state
+	 */
+	bool cancelability;
+
+	/**
+	 * Has the thread been cancelled
+	 */
+	bool canceled;
+
+	/**
+	 * Active condition variable thread is waiting in, if any
+	 */
+	CONDITION_VARIABLE *condvar;
+};
+
+/**
+ * Global list of threads, GetCurrentThreadId() => private_thread_t
+ */
+static hashtable_t *threads;
+
+/**
+ * Lock for threads table
+ */
+static spinlock_t *threads_lock;
+
+/**
+ * Counter to assign printable thread IDs
+ */
+static u_int threads_ids = 0;
+
+/**
+ * Forward declaration
+ */
+static private_thread_t *create_internal(DWORD id);
+
+/**
+ * Set leak detective state
+ */
+static inline bool set_leak_detective(bool state)
+{
+#ifdef LEAK_DETECTIVE
+	if (lib && lib->leak_detective)
+	{
+		return lib->leak_detective->set_state(lib->leak_detective, state);
+	}
+#endif
+	return FALSE;
+}
+
+/**
+ * Store thread in index
+ */
+static void put_thread(private_thread_t *this)
+{
+	bool old;
+
+	old = set_leak_detective(FALSE);
+	threads_lock->lock(threads_lock);
+
+	this = threads->put(threads, (void*)(uintptr_t)this->id, this);
+
+	threads_lock->unlock(threads_lock);
+	set_leak_detective(old);
+}
+
+/**
+ * Remove thread from index
+ */
+static void remove_thread(private_thread_t *this)
+{
+	bool old;
+
+	old = set_leak_detective(FALSE);
+	threads_lock->lock(threads_lock);
+
+	threads->remove(threads, (void*)(uintptr_t)this->id);
+
+	threads_lock->unlock(threads_lock);
+	set_leak_detective(old);
+}
+
+/**
+ * Get thread data for calling thread
+ */
+static private_thread_t *get_current_thread()
+{
+	private_thread_t *this;
+
+	threads_lock->lock(threads_lock);
+
+	this = threads->get(threads, (void*)(uintptr_t)GetCurrentThreadId());
+
+	threads_lock->unlock(threads_lock);
+
+	if (!this)
+	{
+		this = create_internal(GetCurrentThreadId());
+		put_thread(this);
+	}
+
+	return this;
+}
+
+/**
+ * See header.
+ */
+void* thread_tls_put(void *key, void *value)
+{
+	private_thread_t *thread;
+	bool old;
+
+	thread = get_current_thread();
+
+	old = set_leak_detective(FALSE);
+	value = thread->tls->put(thread->tls, key, value);
+	set_leak_detective(old);
+
+	return value;
+}
+
+/**
+ * See header.
+ */
+void* thread_tls_get(void *key)
+{
+	private_thread_t *thread;
+	void *value;
+	bool old;
+
+	thread = get_current_thread();
+
+	old = set_leak_detective(FALSE);
+	value = thread->tls->get(thread->tls, key);
+	set_leak_detective(old);
+
+	return value;
+}
+
+/**
+ * See header.
+ */
+void* thread_tls_remove(void *key)
+{
+	private_thread_t *thread;
+	void *value;
+	bool old;
+
+	thread = get_current_thread();
+
+	old = set_leak_detective(FALSE);
+	value = thread->tls->remove(thread->tls, key);
+	set_leak_detective(old);
+
+	return value;
+}
+
+/**
+ * Thread cleanup data
+ */
+typedef struct {
+	/** Cleanup callback function */
+	thread_cleanup_t cb;
+	/** Argument provided to the cleanup function */
+	void *arg;
+} cleanup_t;
+
+/**
+ * Invoke pushed/tls cleanup handlers
+ */
+static void docleanup(private_thread_t *this)
+{
+	enumerator_t *enumerator;
+	cleanup_t cleanup, *tls;
+	bool old;
+
+	old = set_leak_detective(FALSE);
+
+	while (array_remove(this->cleanup, -1, &cleanup))
+	{
+		set_leak_detective(old);
+		cleanup.cb(cleanup.arg);
+		set_leak_detective(FALSE);
+	}
+
+	enumerator = this->tls->create_enumerator(this->tls);
+	while (enumerator->enumerate(enumerator, NULL, &tls))
+	{
+		this->tls->remove_at(this->tls, enumerator);
+
+		set_leak_detective(old);
+		thread_tls_cleanup(tls);
+		set_leak_detective(FALSE);
+	}
+	enumerator->destroy(enumerator);
+
+	set_leak_detective(old);
+}
+
+/**
+ * Clean up and destroy a thread
+ */
+static void destroy(private_thread_t *this)
+{
+	bool old;
+
+	docleanup(this);
+
+	old = set_leak_detective(FALSE);
+
+	array_destroy(this->cleanup);
+	this->tls->destroy(this->tls);
+	if (this->handle)
+	{
+		CloseHandle(this->handle);
+	}
+	free(this);
+
+	set_leak_detective(old);
+}
+
+/**
+ * End a thread, destroy when detached
+ */
+static void end_thread(private_thread_t *this)
+{
+	if (this->detached)
+	{
+		remove_thread(this);
+		destroy(this);
+	}
+	else
+	{
+		this->terminated = TRUE;
+		docleanup(this);
+	}
+}
+
+/**
+ * See header.
+ */
+void thread_set_active_condvar(CONDITION_VARIABLE *condvar)
+{
+	private_thread_t *thread;
+
+	thread = get_current_thread();
+
+	threads_lock->lock(threads_lock);
+	thread->condvar = condvar;
+	threads_lock->unlock(threads_lock);
+
+	/* this is a cancellation point, as condvar wait is one */
+	SleepEx(0, TRUE);
+}
+
+/**
+ * APC to cancel a thread
+ */
+static void docancel(private_thread_t *this)
+{
+	/* make sure cancel() does not access this anymore */
+	threads_lock->lock(threads_lock);
+	threads_lock->unlock(threads_lock);
+
+	end_thread(this);
+	ExitThread(0);
+}
+
+METHOD(thread_t, cancel, void,
+	private_thread_t *this)
+{
+	this->canceled = TRUE;
+	if (this->cancelability)
+	{
+		threads_lock->lock(threads_lock);
+		QueueUserAPC((void*)docancel, this->handle, (uintptr_t)this);
+		if (this->condvar)
+		{
+			WakeAllConditionVariable(this->condvar);
+		}
+		threads_lock->unlock(threads_lock);
+	}
+}
+
+METHOD(thread_t, kill_, void,
+	private_thread_t *this, int sig)
+{
+}
+
+METHOD(thread_t, detach, void,
+	private_thread_t *this)
+{
+	this->detached = TRUE;
+}
+
+METHOD(thread_t, join, void*,
+	private_thread_t *this)
+{
+	void *ret;
+
+	if (this->detached)
+	{
+		return NULL;
+	}
+
+	while (!this->terminated)
+	{
+		/* join is a cancellation point, use alertable wait */
+		WaitForSingleObjectEx(this->handle, INFINITE, TRUE);
+	}
+
+	ret = this->ret;
+
+	remove_thread(this);
+	destroy(this);
+
+	return ret;
+}
+
+/**
+ * Main function wrapper for threads
+ */
+static DWORD thread_cb(private_thread_t *this)
+{
+	/* Enable cancelability once the thread starts. We must check for any
+	 * pending cancellation request an queue the APC that gets executed
+	 * at the first cancellation point. */
+	this->cancelability = TRUE;
+	if (this->canceled)
+	{
+		cancel(this);
+	}
+
+	this->ret = this->main(this->arg);
+
+	end_thread(this);
+
+	return 0;
+}
+
+/**
+ * Create an internal thread object.
+ */
+static private_thread_t *create_internal(DWORD id)
+{
+	private_thread_t *this;
+	bool old;
+
+	old = set_leak_detective(FALSE);
+
+	INIT(this,
+		.public = {
+			.cancel = _cancel,
+			.kill = _kill_,
+			.detach = _detach,
+			.join = _join,
+		},
+		.cleanup = array_create(sizeof(cleanup_t), 0),
+		.tls = hashtable_create(hashtable_hash_ptr, hashtable_equals_ptr, 4),
+		.id = id,
+		.cancelability = TRUE,
+	);
+
+	set_leak_detective(old);
+
+	threads_lock->lock(threads_lock);
+	this->tid = threads_ids++;
+	threads_lock->unlock(threads_lock);
+
+	if (id)
+	{
+		this->handle = OpenThread(THREAD_ALL_ACCESS, FALSE, id);
+	}
+	return this;
+}
+
+/**
+ * Described in header.
+ */
+thread_t *thread_create(thread_main_t main, void *arg)
+{
+	private_thread_t *this;
+
+	this = create_internal(0);
+
+	this->main = main;
+	this->arg = arg;
+	/* not cancellable until started */
+	this->cancelability = FALSE;
+
+	this->handle = CreateThread(NULL, 0, (void*)thread_cb, this,
+								CREATE_SUSPENDED, &this->id);
+	if (!this->handle)
+	{
+		destroy(this);
+		return NULL;
+	}
+
+	put_thread(this);
+
+	DBG2(DBG_LIB, "created thread %u", this->id);
+
+	ResumeThread(this->handle);
+
+	return &this->public;
+}
+
+/**
+ * Described in header.
+ */
+thread_t *thread_current()
+{
+	return &get_current_thread()->public;
+}
+
+/**
+ * Described in header.
+ */
+u_int thread_current_id()
+{
+	return get_current_thread()->tid;
+}
+
+/**
+ * Described in header.
+ */
+void thread_cleanup_push(thread_cleanup_t cb, void *arg)
+{
+	private_thread_t *this;
+	cleanup_t cleanup = {
+		.cb = cb,
+		.arg = arg,
+	};
+	bool old;
+
+	this = get_current_thread();
+
+	old = set_leak_detective(FALSE);
+	array_insert(this->cleanup, -1, &cleanup);
+	set_leak_detective(old);
+}
+
+/**
+ * Described in header
+ */
+void thread_cleanup_pop(bool execute)
+{
+	private_thread_t *this;
+	cleanup_t cleanup = {};
+	bool old;
+
+	this = get_current_thread();
+
+	old = set_leak_detective(FALSE);
+	array_remove(this->cleanup, -1, &cleanup);
+	set_leak_detective(old);
+
+	if (execute)
+	{
+		cleanup.cb(cleanup.arg);
+	}
+}
+
+/**
+ * Described in header.
+ */
+bool thread_cancelability(bool enable)
+{
+	private_thread_t *this;
+	bool old;
+
+	this = get_current_thread();
+	old = this->cancelability;
+	this->cancelability = enable;
+
+	if (enable && !old && this->canceled)
+	{
+		cancel(this);
+	}
+	return old;
+}
+
+/**
+ * Described in header.
+ */
+void thread_cancellation_point()
+{
+	bool old;
+
+	old = thread_cancelability(TRUE);
+	SleepEx(0, TRUE);
+	thread_cancelability(old);
+}
+
+/**
+ * Described in header.
+ */
+void thread_exit(void *val)
+{
+	private_thread_t *this;
+
+	this = get_current_thread();
+	this->ret = val;
+
+	end_thread(this);
+	ExitThread(0);
+}
+
+/*
+ * Described in header.
+ */
+void threads_init()
+{
+	threads_lock = spinlock_create();
+	threads = hashtable_create(hashtable_hash_ptr, hashtable_equals_ptr, 4);
+
+	/* reset counter should we initialize more than once */
+	threads_ids = 0;
+
+	put_thread(create_internal(GetCurrentThreadId()));
+}
+
+/**
+ * Described in header.
+ */
+void threads_deinit()
+{
+	private_thread_t *this;
+
+	this = threads->remove(threads, (void*)(uintptr_t)GetCurrentThreadId());
+	destroy(this);
+
+	threads_lock->destroy(threads_lock);
+	threads->destroy(threads);
+}

src/libstrongswan/threading/windows/thread.h
@@ -1,0 +1,92 @@
+/*
+ * Copyright (C) 2013 Martin Willi
+ * Copyright (C) 2013 revosec AG
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+
+#ifndef WINDOWS_THREAD_H_
+#define WINDOWS_THREAD_H_
+
+/**
+ * @defgroup windowsthread windows
+ * @ingroup threading
+ *
+ * @defgroup threadwindows thread
+ * @{ @ingroup windowsthread
+ */
+
+/**
+ * MinGW seems to miss the actual struct definition
+ */
+typedef struct {
+	PVOID Ptr;
+} SRWLOCK, *PSRWLOCK, CONDITION_VARIABLE, *PCONDITION_VARIABLE;
+
+VOID WINAPI InitializeConditionVariable(PCONDITION_VARIABLE ConditionVariable);
+BOOL WINAPI SleepConditionVariableCS(PCONDITION_VARIABLE ConditionVariable,
+					PCRITICAL_SECTION CriticalSection, DWORD dwMilliseconds);
+BOOL WINAPI SleepConditionVariableSRW(PCONDITION_VARIABLE ConditionVariable,
+						PSRWLOCK SRWLock, DWORD dwMilliseconds, ULONG Flags);
+VOID WINAPI WakeAllConditionVariable(PCONDITION_VARIABLE ConditionVariable);
+VOID WINAPI WakeConditionVariable(PCONDITION_VARIABLE ConditionVariable);
+VOID WINAPI AcquireSRWLockExclusive(PSRWLOCK SRWLock);
+VOID WINAPI AcquireSRWLockShared(PSRWLOCK SRWLock);
+BOOL TryAcquireSRWLockExclusive(PSRWLOCK SRWLock);
+VOID WINAPI InitializeSRWLock(PSRWLOCK SRWLock);
+VOID WINAPI ReleaseSRWLockExclusive(PSRWLOCK SRWLock);
+VOID WINAPI ReleaseSRWLockShared(PSRWLOCK SRWLock);
+
+/**
+ * Set active condvar of a thread before waiting in it.
+ *
+ * @param cv		active condition variable, NULL to unset
+ */
+void thread_set_active_condvar(CONDITION_VARIABLE *condvar);
+
+/**
+ * Set a thread specific value on the current thread.
+ *
+ * @param key		unique key specifying the TLS variable
+ * @param value		value to set
+ * @return			old value for key, if any
+ */
+void* thread_tls_put(void *key, void *value);
+
+/**
+ * Get a thread specific value from the current thread.
+ *
+ * @param key		unique key specifying the TLS variable
+ * @return			value for key, if any
+ */
+void* thread_tls_get(void *key);
+
+/**
+ * Remove a thread specific value from the current thread.
+ *
+ * @param key		unique key specifying the TLS variable
+ * @param value		value to set
+ * @return			old value for key, if any
+ */
+void* thread_tls_remove(void *key);
+
+/**
+ * Cleanup function for thread specific value.
+ *
+ * This is called whenever a thread exits to clean up thread specific data.
+ *
+ * This function is actually implemented in thread_value.c.
+ *
+ * @param value		value, as passed to thread_tls_put()
+ */
+void thread_tls_cleanup(void *value);
+
+#endif /** WINDOWS_THREAD_H_ @}*/

src/libstrongswan/Makefile.am
@@ -33,14 +33,18 @@ processing/jobs/callback_job.c processing/processor.c processing/scheduler.c \
 processing/watcher.c resolver/resolver_manager.c resolver/rr_set.c \
 selectors/traffic_selector.c settings/settings.c settings/settings_types.c \
 settings/settings_parser.y settings/settings_lexer.l \
-threading/thread.c threading/thread_value.c threading/mutex.c \
-threading/semaphore.c threading/rwlock.c threading/spinlock.c \
 utils/utils.c utils/chunk.c utils/debug.c utils/enum.c utils/identification.c \
 utils/lexparser.c utils/optionsfrom.c utils/capabilities.c utils/backtrace.c \
 utils/parser_helper.c utils/test.c utils/utils/strerror.c
 
 if !USE_WINDOWS
   libstrongswan_la_SOURCES += \
+    threading/thread.c \
+    threading/thread_value.c \
+    threading/mutex.c \
+    threading/rwlock.c \
+    threading/spinlock.c \
+    threading/semaphore.c \
     networking/streams/stream_unix.c \
     networking/streams/stream_service_unix.c
 endif
@@ -89,7 +93,8 @@ resolver/rr.h resolver/resolver_manager.h \
 plugins/plugin_loader.h plugins/plugin.h plugins/plugin_feature.h \
 processing/jobs/job.h processing/jobs/callback_job.h processing/processor.h \
 processing/scheduler.h processing/watcher.h selectors/traffic_selector.h \
-settings/settings.h threading/thread.h threading/thread_value.h \
+settings/settings.h threading/thread_value.h \
+threading/thread.h threading/windows/thread.h \
 threading/mutex.h threading/condvar.h threading/spinlock.h threading/semaphore.h \
 threading/rwlock.h threading/rwlock_condvar.h threading/lock_profiler.h \
 utils/utils.h utils/chunk.h utils/debug.h utils/enum.h utils/identification.h \
@@ -102,7 +107,7 @@ endif
 
 library.lo :	$(top_builddir)/config.status
 
-libstrongswan_la_LIBADD = $(PTHREADLIB) $(DLLIB) $(BTLIB) $(SOCKLIB) $(RTLIB) $(BFDLIB) $(UNWINDLIB)
+libstrongswan_la_LIBADD = $(DLLIB) $(BTLIB) $(SOCKLIB) $(RTLIB) $(BFDLIB) $(UNWINDLIB)
 
 AM_CPPFLAGS = \
 	-I$(top_srcdir)/src/libstrongswan \
@@ -122,7 +127,15 @@ AM_YFLAGS = -v -d
 if USE_WINDOWS
   libstrongswan_la_LIBADD += -lws2_32
   libstrongswan_la_SOURCES += \
+    threading/windows/thread.c \
+    threading/windows/thread_value.c \
+    threading/windows/mutex.c \
+    threading/windows/rwlock.c \
+    threading/windows/spinlock.c \
+    threading/windows/semaphore.c \
     utils/windows.c
+else
+  libstrongswan_la_LIBADD += $(PTHREADLIB)
 endif
 
 if USE_DBGHELP

src/libstrongswan/threading/windows/mutex.c
@@ -1,0 +1,197 @@
+/*
+ * Copyright (C) 2013 Martin Willi
+ * Copyright (C) 2013 revosec AG
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+
+#include <library.h>
+#include <utils/debug.h>
+#include <threading/mutex.h>
+#include <threading/condvar.h>
+
+#include "thread.h"
+
+typedef struct private_mutex_t private_mutex_t;
+typedef struct private_condvar_t private_condvar_t;
+
+/**
+ * private data of mutex
+ */
+struct private_mutex_t {
+
+	/**
+	 * public functions
+	 */
+	mutex_t public;
+
+	/**
+	 * wrapped critical section
+	 */
+	CRITICAL_SECTION cs;
+
+	/**
+	 * Recursive lock count
+	 */
+	u_int times;
+};
+
+/**
+ * private data of condvar
+ */
+struct private_condvar_t {
+
+	/**
+	 * public functions
+	 */
+	condvar_t public;
+
+	/**
+	 * wrapped condition variable
+	 */
+	CONDITION_VARIABLE cv;
+};
+
+
+METHOD(mutex_t, lock, void,
+	private_mutex_t *this)
+{
+	EnterCriticalSection(&this->cs);
+	this->times++;
+}
+
+METHOD(mutex_t, unlock, void,
+	private_mutex_t *this)
+{
+	this->times--;
+	LeaveCriticalSection(&this->cs);
+}
+
+METHOD(mutex_t, mutex_destroy, void,
+	private_mutex_t *this)
+{
+	DeleteCriticalSection(&this->cs);
+	free(this);
+}
+
+/*
+ * see header file
+ */
+mutex_t *mutex_create(mutex_type_t type)
+{
+	private_mutex_t *this;
+
+	INIT(this,
+		.public = {
+			.lock = _lock,
+			.unlock = _unlock,
+			.destroy = _mutex_destroy,
+		},
+	);
+
+	/* CriticalSections are recursive, we use it for all mutex types. */
+	InitializeCriticalSection(&this->cs);
+
+	return &this->public;
+}
+
+METHOD(condvar_t, timed_wait, bool,
+	private_condvar_t *this, mutex_t *pubmutex, u_int timeout)
+{
+	private_mutex_t *mutex = (private_mutex_t*)pubmutex;
+	u_int times;
+	bool ret;
+
+	thread_set_active_condvar(&this->cv);
+
+	/* while a CriticalSection is recursive, waiting in a condvar releases
+	 * only one mutex. So release (and reaquire) all locks except the last. */
+	times = mutex->times;
+	while (mutex->times-- > 1)
+	{
+		LeaveCriticalSection(&mutex->cs);
+	}
+
+	ret = SleepConditionVariableCS(&this->cv, &mutex->cs, timeout);
+
+	while (++mutex->times < times)
+	{
+		EnterCriticalSection(&mutex->cs);
+	}
+
+	thread_set_active_condvar(NULL);
+
+	return ret == 0;
+}
+
+METHOD(condvar_t, wait_, void,
+	private_condvar_t *this, mutex_t *mutex)
+{
+	timed_wait(this, mutex, INFINITE);
+}
+
+METHOD(condvar_t, timed_wait_abs, bool,
+	private_condvar_t *this, mutex_t *mutex, timeval_t tv)
+{
+	DWORD timeout;
+	timeval_t now, diff;
+
+	time_monotonic(&now);
+	if (timercmp(&now, &tv, >))
+	{
+		return TRUE;
+	}
+	timersub(&tv, &now, &diff);
+	timeout = diff.tv_sec * 1000 + diff.tv_usec / 1000;
+
+	return timed_wait(this, mutex, timeout);
+}
+
+METHOD(condvar_t, signal_, void,
+	private_condvar_t *this)
+{
+	WakeConditionVariable(&this->cv);
+}
+
+METHOD(condvar_t, broadcast, void,
+	private_condvar_t *this)
+{
+	WakeAllConditionVariable(&this->cv);
+}
+
+METHOD(condvar_t, condvar_destroy, void,
+	private_condvar_t *this)
+{
+	free(this);
+}
+
+/*
+ * see header file
+ */
+condvar_t *condvar_create(condvar_type_t type)
+{
+	private_condvar_t *this;
+
+	INIT(this,
+		.public = {
+			.wait = _wait_,
+			.timed_wait = _timed_wait,
+			.timed_wait_abs = _timed_wait_abs,
+			.signal = _signal_,
+			.broadcast = _broadcast,
+			.destroy = _condvar_destroy,
+		}
+	);
+
+	InitializeConditionVariable(&this->cv);
+
+	return &this->public;
+}

src/libstrongswan/threading/windows/rwlock.c
@@ -1,0 +1,221 @@
+/*
+ * Copyright (C) 2013 Martin Willi
+ * Copyright (C) 2013 revosec AG
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+
+#include <library.h>
+#include <utils/debug.h>
+#include <threading/rwlock.h>
+#include <threading/rwlock_condvar.h>
+#include <threading/thread_value.h>
+
+#include "thread.h"
+
+typedef struct private_rwlock_t private_rwlock_t;
+typedef struct private_rwlock_condvar_t private_rwlock_condvar_t;
+
+/**
+ * private data of rwlock
+ */
+struct private_rwlock_t {
+
+	/**
+	 * public functions
+	 */
+	rwlock_t public;
+
+	/**
+	 * wrapped rwlock
+	 */
+	SRWLOCK srw;
+
+	/**
+	 * Thread specific shared lock count
+	 */
+	thread_value_t *shared;
+};
+
+/**
+ * private data of condvar
+ */
+struct private_rwlock_condvar_t {
+
+	/**
+	 * public interface
+	 */
+	rwlock_condvar_t public;
+
+	/**
+	 * condition variable
+	 */
+	CONDITION_VARIABLE cv;
+};
+
+METHOD(rwlock_t, read_lock, void,
+	private_rwlock_t *this)
+{
+	uintptr_t count;
+
+	/* Recursive read locks are not supported. Use a thread specific
+	 * recursiveness counter. */
+
+	count = (uintptr_t)this->shared->get(this->shared);
+	if (count == 0)
+	{
+		AcquireSRWLockShared(&this->srw);
+	}
+	this->shared->set(this->shared, (void*)(count + 1));
+}
+
+METHOD(rwlock_t, write_lock, void,
+	private_rwlock_t *this)
+{
+	AcquireSRWLockExclusive(&this->srw);
+}
+
+METHOD(rwlock_t, try_write_lock, bool,
+	private_rwlock_t *this)
+{
+	/* TODO: causes random failures and segfaults. Bug? */
+	return FALSE;
+	return TryAcquireSRWLockExclusive(&this->srw);
+}
+
+METHOD(rwlock_t, unlock, void,
+	private_rwlock_t *this)
+{
+	uintptr_t count;
+
+	count = (uintptr_t)this->shared->get(this->shared);
+	switch (count)
+	{
+		case 0:
+			ReleaseSRWLockExclusive(&this->srw);
+			break;
+		case 1:
+			ReleaseSRWLockShared(&this->srw);
+			/* fall */
+		default:
+			this->shared->set(this->shared, (void*)(count - 1));
+			break;
+	}
+}
+
+METHOD(rwlock_t, destroy, void,
+	private_rwlock_t *this)
+{
+	this->shared->destroy(this->shared);
+	free(this);
+}
+
+/*
+ * see header file
+ */
+rwlock_t *rwlock_create(rwlock_type_t type)
+{
+	private_rwlock_t *this;
+
+	INIT(this,
+		.public = {
+			.read_lock = _read_lock,
+			.write_lock = _write_lock,
+			.try_write_lock = _try_write_lock,
+			.unlock = _unlock,
+			.destroy = _destroy,
+		},
+		.shared = thread_value_create(NULL),
+	);
+
+	InitializeSRWLock(&this->srw);
+
+	return &this->public;
+}
+
+METHOD(rwlock_condvar_t, timed_wait, bool,
+	private_rwlock_condvar_t *this, rwlock_t *pubrwlock, u_int timeout)
+{
+	private_rwlock_t *rwlock = (private_rwlock_t*)pubrwlock;
+	bool ret;
+
+	thread_set_active_condvar(&this->cv);
+
+	ret = SleepConditionVariableSRW(&this->cv, &rwlock->srw, timeout, 0);
+
+	thread_set_active_condvar(NULL);
+
+	return ret == 0;
+}
+
+METHOD(rwlock_condvar_t, wait_, void,
+	private_rwlock_condvar_t *this, rwlock_t *lock)
+{
+	timed_wait(this, lock, INFINITE);
+}
+
+METHOD(rwlock_condvar_t, timed_wait_abs, bool,
+	private_rwlock_condvar_t *this, rwlock_t *lock, timeval_t tv)
+{
+	DWORD timeout;
+	timeval_t now, diff;
+
+	time_monotonic(&now);
+	if (timercmp(&now, &tv, >))
+	{
+		return TRUE;
+	}
+	timersub(&tv, &now, &diff);
+	timeout = diff.tv_sec * 1000 + diff.tv_usec / 1000;
+
+	return timed_wait(this, lock, timeout);
+}
+
+METHOD(rwlock_condvar_t, signal_, void,
+	private_rwlock_condvar_t *this)
+{
+	WakeConditionVariable(&this->cv);
+}
+
+METHOD(rwlock_condvar_t, broadcast, void,
+	private_rwlock_condvar_t *this)
+{
+	WakeAllConditionVariable(&this->cv);
+}
+
+METHOD(rwlock_condvar_t, condvar_destroy, void,
+	private_rwlock_condvar_t *this)
+{
+	free(this);
+}
+
+/*
+ * see header file
+ */
+rwlock_condvar_t *rwlock_condvar_create()
+{
+	private_rwlock_condvar_t *this;
+
+	INIT(this,
+		.public = {
+			.wait = _wait_,
+			.timed_wait = _timed_wait,
+			.timed_wait_abs = _timed_wait_abs,
+			.signal = _signal_,
+			.broadcast = _broadcast,
+			.destroy = _condvar_destroy,
+		},
+	);
+
+	InitializeConditionVariable(&this->cv);
+
+	return &this->public;
+}

commit 188b7ff70bfbd4a94aa70b8abd44b4c6c67be58f

windows: Fix race when canceling threads in timed_wait()
If a thread is canceled that's in `timed_wait()` and has already called
`thread_set_active_condvar()` but has not yet started waiting on the
condvar, the wake could get lost (it is not queued on the condvar).
To avoid that, we signal the condvar until the woken thread resets it
again, either when exiting `timed_wait()` or when the APC is delivered
via the `SleepEx()` cancellation point in `thread_set_active_condvar()`.
Because that requires the target to reacquire the mutex/rwlock, the
thread that calls `cancel()` can't hold that same lock to avoid a
deadlock.  Note that `processor_t` technically does this, but since the
threads are not cancelable while waiting for jobs, it doesn't trigger
this issue.

An alternative option would have been to avoid waiting indefinitely in
`timed_wait()` (i.e. implement a kind of polling there).  But since
`cancel()` is called way less often, this seemed the better option.

Fixes: 0fa9c958114f ("windows: Provide a complete native Windows threading backend")

src/libstrongswan/threading/thread.h
@@ -51,6 +51,10 @@ struct thread_t {
 
 	/**
 	 * Cancel this thread.
+	 *
+	 * @warning On Windows, this blocks until the thread has left a condvar
+	 * wait, so it must not be called while holding the same mutex/rwlock the
+	 * thread might be waiting on in cancelable state.
 	 */
 	void (*cancel)(thread_t *this);
 

src/libstrongswan/threading/windows/thread.c
@@ -339,11 +339,35 @@ void thread_set_active_condvar(CONDITION_VARIABLE *condvar)
 }
 
 /**
+ * Wake the thread waiting on the condvar and return whether there
+ * currently is an active condvar.
+ */
+static bool wake_active_condvar(private_thread_t *this)
+{
+	CONDITION_VARIABLE *cv;
+
+	condvar_lock->lock(condvar_lock);
+	cv = this->condvar;
+	if (cv)
+	{
+		WakeAllConditionVariable(cv);
+	}
+	condvar_lock->unlock(condvar_lock);
+	return cv != NULL;
+}
+
+/**
  * APC to cancel a thread
  */
 static void WINAPI docancel(ULONG_PTR dwParam)
 {
 	private_thread_t *this = (private_thread_t*)dwParam;
+
+	/* if canceled in thread_set_active_condvar() before waiting on the condvar,
+	 * we clear this so cancel() doesn't wait for it */
+	condvar_lock->lock(condvar_lock);
+	this->condvar = NULL;
+	condvar_lock->unlock(condvar_lock);
 
 	end_thread(this);
 	ExitThread(0);
@@ -356,16 +380,27 @@ METHOD(thread_t, cancel, void,
 	if (atomic_get_bool(&this->cancelability) &&
 		cas_bool(&this->cancel_pending, FALSE, TRUE))
 	{
-		CONDITION_VARIABLE *cv;
-
-		condvar_lock->lock(condvar_lock);
-		cv = this->condvar;
 		QueueUserAPC(docancel, this->handle, (uintptr_t)this);
-		if (cv)
+
+		/* SleepConditionVariableCS() is not alertable, so the APC can only be
+		 * delivered after the thread returns from the wait. waking the condvar
+		 * normally achieves that, but the wake may get lost if it races with
+		 * the thread entering the wait (after setting the condvar).  so we keep
+		 * waking it until the thread cleared the pointer when leaving the wait
+		 * (docancel() does the same if the thread is canceled before actually
+		 * waiting).  however, because the target thread can only clear the
+		 * condvar by leaving the wait and reacquiring the mutex/rwlock,
+		 * cancel() can't be called while holding the same lock as that would
+		 * prevent that.  on the other hand, there wouldn't be a lost wake as
+		 * the target is definitely waiting on the condvar if we hold the lock.
+		 * unfortunately, we can't determine whether the calling thread holds
+		 * the same lock, so doing this must be avoided.  note that it's safe to
+		 * access `this` as calling cancel() on a detached thread doesn't make
+		 * sense as the thread object could get destroyed at any moment */
+		while (wake_active_condvar(this))
 		{
-			WakeAllConditionVariable(cv);
+			Sleep(1);
 		}
-		condvar_lock->unlock(condvar_lock);
 	}
 }
 