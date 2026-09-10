strongswan/strongswan

commit 69cbe2ca3fe4d6e6607eac0d2b00a5eb682fe4a0

trap-manager: Wait for install to finish before uninstalling
There was a race condition between install() and uninstall()
where one thread was in the process of installing a trap
entry, and had destroyed the child_sa, while the other
thread was uninstalling the same trap entry and ended up
trying to destroy the already destroyed child_sa, resulting
in a segmentation fault in the destroy_entry() function.

The uninstall() function needs to wait until all the threads
are done with the installing before proceeding to uninstall
a trap entry.

Closes strongswan/strongswan#131.

src/libcharon/sa/trap_manager.c
@@ -352,6 +352,10 @@ METHOD(trap_manager_t, uninstall, bool,
 	entry_t *entry, *found = NULL;
 
 	this->lock->write_lock(this->lock);
+	while (this->installing)
+	{
+		this->condvar->wait(this->condvar, this->lock);
+	}
 	enumerator = this->traps->create_enumerator(this->traps);
 	while (enumerator->enumerate(enumerator, &entry))
 	{

commit 78937d2190656f175599411c14be5ecbeb2c2c02

trap-manager: Ignore uninstalls during shutdown to avoid deadlock
Basically the same as the previous commit.

Fixes: 69cbe2ca3fe4 ("trap-manager: Wait for install to finish before uninstalling")

src/libcharon/sa/trap_manager.c
@@ -384,7 +384,7 @@ METHOD(trap_manager_t, install, bool,
 	this->lock->write_lock(this->lock);
 	/* do this at the end, so entries created temporarily are also destroyed */
 	this->installing--;
-	this->condvar->signal(this->condvar);
+	this->condvar->broadcast(this->condvar);
 	this->lock->unlock(this->lock);
 	return result;
 }
@@ -398,6 +398,11 @@ METHOD(trap_manager_t, uninstall, bool,
 	this->lock->write_lock(this->lock);
 	while (this->installing)
 	{
+		if (this->installing == INSTALL_DISABLED)
+		{	/* flush() has been called, policy is already uninstalled */
+			this->lock->unlock(this->lock);
+			return TRUE;
+		}
 		this->condvar->wait(this->condvar, this->lock);
 	}
 	enumerator = this->traps->create_enumerator(this->traps);
@@ -850,6 +855,11 @@ METHOD(trap_manager_t, flush, void,
 	private_trap_manager_t *this)
 {
 	this->lock->write_lock(this->lock);
+	if (this->installing == INSTALL_DISABLED)
+	{
+		this->lock->unlock(this->lock);
+		return;
+	}
 	while (this->installing)
 	{
 		this->condvar->wait(this->condvar, this->lock);