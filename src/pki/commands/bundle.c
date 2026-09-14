975b3beacea23fd2289bb43ac48a3a39665aedb6
Tobias Brunner <tobias@strongswan.org>
2026-09-14 16:22:32 +0200

    pki: Add option to create a PEM-encoded bundle of certificates
    Compared to just using `cat` this has the advantage of supporting
    DER-encoded certificates and only adding successfully parsed certificates
    to the output.

--- src/pki/commands/bundle.c
+++ src/pki/commands/bundle.c
@@ -18,6 +18,7 @@
 
 #include "pki.h"
 
+#include <asn1/asn1.h>
 #include <credentials/certificates/x509.h>
 #include <credentials/containers/pem.h>
 
@@ -92,17 +93,123 @@ static int export(pem_t *bundle, int index, char *outform)
 }
 
 /**
+ * Create a bundle file from a list of certificates
+ */
+static int create(array_t *certs)
+{
+	enumerator_t *enumerator;
+	certificate_t *cert;
+	chunk_t encoding;
+
+	enumerator = array_create_enumerator(certs);
+	while (enumerator->enumerate(enumerator, &cert))
+	{
+		if (cert->get_encoding(cert, CERT_PEM, &encoding))
+		{
+			if (fwrite(encoding.ptr, encoding.len, 1, stdout) != 1)
+			{
+				fprintf(stderr, "failed to write certificate to stdout\n");
+				free(encoding.ptr);
+				enumerator->destroy(enumerator);
+				return 1;
+			}
+			free(encoding.ptr);
+		}
+		else
+		{
+			fprintf(stderr, "unable to encode certificate in PEM format\n");
+			enumerator->destroy(enumerator);
+			return 1;
+		}
+	}
+	enumerator->destroy(enumerator);
+	return 0;
+}
+
+/**
+ * Add the parsed certificate/bundle to the list
+ */
+static bool add_certs(array_t **certs, char *path)
+{
+	certificate_t *cert;
+	enumerator_t *enumerator;
+	pem_t *pem = NULL;
+	chunk_t *mapped = NULL, chunk = chunk_empty;
+	bool success = FALSE;
+
+	if (streq(path, "-"))
+	{
+		set_file_mode(stdin, CERT_ASN1_DER);
+		if (!chunk_from_fd(0, &chunk))
+		{
+			fprintf(stderr, "reading input failed: %s\n", strerror(errno));
+			return FALSE;
+		}
+	}
+	else
+	{
+		mapped = chunk_map(path, FALSE);
+		if (!mapped)
+		{
+			fprintf(stderr, "reading '%s' failed: %s\n", path, strerror(errno));
+			return FALSE;
+		}
+		chunk = *mapped;
+	}
+
+	if (!is_asn1(chunk))
+	{
+		pem = lib->creds->create(lib->creds, CRED_CONTAINER, CONTAINER_PEM,
+								 BUILD_BLOB, chunk, BUILD_END);
+	}
+	if (pem)
+	{
+		enumerator = pem->create_cert_enumerator(pem);
+		while (enumerator->enumerate(enumerator, &cert))
+		{
+			array_insert_create(certs, ARRAY_TAIL, cert->get_ref(cert));
+		}
+		enumerator->destroy(enumerator);
+		pem->container.destroy(&pem->container);
+	}
+	else
+	{
+		cert = lib->creds->create(lib->creds, CRED_CERTIFICATE, CERT_X509,
+								  BUILD_BLOB, chunk, BUILD_END);
+		if (!cert)
+		{
+			goto out;
+		}
+		array_insert_create(certs, ARRAY_TAIL, cert);
+	}
+	success = TRUE;
+
+out:
+	if (mapped)
+	{
+		chunk_unmap(mapped);
+	}
+	else
+	{
+		free(chunk.ptr);
+	}
+	return success;
+}
+
+/**
  * Handle PEM certificate bundles
  */
 static int bundle()
 {
 	char *arg, *file = NULL, *outform = NULL;
 	pem_t *pem = NULL;
+	array_t *certs = NULL;
 	int res = 1, index = 0;
 	enum {
 		OP_NONE,
 		OP_LIST,
 		OP_EXPORT,
+		OP_CREATE,
 	} op = OP_NONE;
 
 	while (TRUE)
@@ -132,6 +239,20 @@ static int bundle()
 			case 'f':
 				outform = arg;
 				continue;
+			case 'C':
+				if (op != OP_NONE)
+				{
+					goto invalid;
+				}
+				op = OP_CREATE;
+				continue;
+			case 'c':
+				if (!add_certs(&certs, arg))
+				{
+					array_destroy_offset(certs, offsetof(certificate_t, destroy));
+					return command_usage("parsing certificate failed");
+				}
+				continue;
 			case EOF:
 				break;
 			default:
@@ -141,30 +262,33 @@ static int bundle()
 		break;
 	}
 
-	if (file)
+	if (op != OP_CREATE)
 	{
-		pem = lib->creds->create(lib->creds, CRED_CONTAINER, CONTAINER_PEM,
-								 BUILD_FROM_FILE, file, BUILD_END);
-	}
-	else
-	{
-		chunk_t chunk;
-
-		set_file_mode(stdin, CERT_ASN1_DER);
-		if (!chunk_from_fd(0, &chunk))
+		if (file)
 		{
-			fprintf(stderr, "reading input failed: %s\n", strerror(errno));
-			return 1;
+			pem = lib->creds->create(lib->creds, CRED_CONTAINER, CONTAINER_PEM,
+									 BUILD_FROM_FILE, file, BUILD_END);
 		}
-		pem = lib->creds->create(lib->creds, CRED_CONTAINER, CONTAINER_PEM,
-								 BUILD_BLOB, chunk, BUILD_END);
-		free(chunk.ptr);
-	}
+		else
+		{
+			chunk_t chunk;
 
-	if (!pem)
-	{
-		fprintf(stderr, "reading input failed!\n");
-		goto end;
+			set_file_mode(stdin, CERT_ASN1_DER);
+			if (!chunk_from_fd(0, &chunk))
+			{
+				fprintf(stderr, "reading input failed: %s\n", strerror(errno));
+				return 1;
+			}
+			pem = lib->creds->create(lib->creds, CRED_CONTAINER, CONTAINER_PEM,
+									 BUILD_BLOB, chunk, BUILD_END);
+			free(chunk.ptr);
+		}
+
+		if (!pem)
+		{
+			fprintf(stderr, "reading input failed!\n");
+			goto end;
+		}
 	}
 
 	switch (op)
@@ -174,6 +298,9 @@ static int bundle()
 			break;
 		case OP_EXPORT:
 			res = export(pem, index, outform);
+			break;
+		case OP_CREATE:
+			res = create(certs);
 			break;
 		default:
 			pem->container.destroy(&pem->container);
@@ -185,6 +312,7 @@ end:
 	{
 		pem->container.destroy(&pem->container);
 	}
+	array_destroy_offset(certs, offsetof(certificate_t, destroy));
 	return res;
 }
 
@@ -195,7 +323,7 @@ static void __attribute__ ((constructor))reg()
 {
 	command_register((command_t) {
 		bundle, 'b', "bundle", "PEM certificate bundle functions",
-		{"--export index|--list [--in file]",
+		{"--export index|--list [--in file]|--create [--cert -|file]+",
 		 "[--outform der|pem]"},
 		{
 			{"help",	'h', 0, "show usage information"},
@@ -203,6 +331,8 @@ static void __attribute__ ((constructor))reg()
 			{"list",	'l', 0, "list certificates"},
 			{"export",	'e', 1, "export the certificate with the given index"},
 			{"outform",	'f', 1, "encoding of exported certificate, default: der"},
+			{"create",	'C', 0, "create a bundle from multiple certificates"},
+			{"cert",	'c', 1, "certificate/bundle to place into a bundle"},
 		}
 	});
 }
